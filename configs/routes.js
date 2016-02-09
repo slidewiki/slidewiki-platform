5784909f37de2c382065ae867eb65895bbbf05cc
ali1k <hyperir@gmail.com>
2016-02-09 13:43:06 +0100

    added URI scheme for deck page

--- configs/routes.js
+++ configs/routes.js
@@ -19,8 +19,9 @@ export default {
         handler: require('../components/Home/About')
     },
     //-----------------------------------DeckPage routes------------------------------
+    // selector {stype: 'type of content e.g. slide, deck or question', sid: 'id of content type', sposition: 'if there are multiple item use the position', mode: 'interaction mode e.g. view or edit'}
     deck: {
-        path: '/deck/:id?',
+        path: '/deck/:id/:stype?/:sid?/:sposition?/:mode?',
         method: 'get',
         page: 'deck',
         handler: require('../components/Deck/Deck'),
