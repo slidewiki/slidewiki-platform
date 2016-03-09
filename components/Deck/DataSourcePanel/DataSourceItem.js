49309575c698dc8991726dd7f003238717b26d70
dpaun <dpaunovic@gmail.com>
2016-03-09 13:39:36 +0100

    custom scrolling using react-custom-scrollbars; deck sources aggregation

--- components/Deck/DataSourcePanel/DataSourceItem.js
+++ components/Deck/DataSourcePanel/DataSourceItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {NavLink} from 'fluxible-router';
 import classNames from 'classnames';
 
 class DataSourceItem extends React.Component {
@@ -11,37 +12,47 @@ class DataSourceItem extends React.Component {
         return (DataSourceItem.urlRegEx.test(text));
     }
 
+    //shorten url label if it's longer then maxLength
     shortenText(text) {
         const maxLength = 80, startLength = 25, endLength = 50;
         const textLength = text.length;
         return (textLength <  maxLength) ? text : text.substring(0, startLength - 1) + '...' + text.substring(textLength - endLength - 1, textLength - 1);
     }
 
+    //add http protocol to the url if it's missing
     addProtocolIfMissing(url) {
         return url.includes('://') ? url : 'http://' + url;
     }
 
-    insertTitle(title) {
-        let titleURLed = title.replace(DataSourceItem.urlRegEx, (url) => {
+    insertTitle(node) {
+        const title = node.title;
+
+        //mark beginning and end of each url found and then split the text into segments
+        const titleURLed = title.replace(DataSourceItem.urlRegEx, (url) => {
             return '###' + url + '###';
         });
-        let titleSegments = titleURLed.split('###');
+        const titleSegments = titleURLed.split('###');
         let y = [];
 
+        //for each text segment create a link if it's a url or display as is
         titleSegments.forEach((titleSegment, index) => {
             y.push(this.isTextURL(titleSegment) ? <a href={this.addProtocolIfMissing(titleSegment)} key={index}>{this.shortenText(titleSegment)}</a> : titleSegment);
         });
 
+        //append origin of the datasource if it's not the current deck/slide
+        const showOrigin = ((node.originType === node.nodeType) && (node.originId === node.nodeId)) ? false : true;
+        const appendOrigin = (showOrigin) ? <span>(originally from <NavLink href={'/deck/' + node.nodeId + '/' + node.originType + '/' + node.originId}> {node.originType + ' \'' + node.originTitle + ' \''}  </NavLink> ) </span> : '';
+
         return (
-            <span> {y} </span>
+            <span> {y} {appendOrigin}</span>
         );
     }
 
     render() {
-        let nodeTitle = this.props.node.title;
+        const nodeTitle = this.props.node.title;
 
         //change the icon based on the text
-        let iconClass = classNames({
+        const iconClass = classNames({
             'ui icon': true,
             'linkify': this.isTextURL(nodeTitle),
             'file': !this.isTextURL(nodeTitle)
@@ -51,7 +62,7 @@ class DataSourceItem extends React.Component {
             <div className="item" >
                 <i className={iconClass}></i>
                 <div className="content">
-                    {this.insertTitle(nodeTitle)}
+                    {this.insertTitle(this.props.node)}
                 </div>
             </div>
         );
--- services/datasource.js
+++ services/datasource.js
@@ -3,25 +3,96 @@ export default {
     // At least one of the CRUD methods is Required
     read: (req, resource, params, config, callback) => {
         let args = params.params? params.params : params;
-        if(resource === 'datasource.list'){
+        let selector = {'sid': args.sid, 'stype': args.stype};
+        if (resource === 'datasource.list'){
             /*********connect to microservices*************/
             //todo
             /*********received data from microservices*************/
-            let datasources = [
-                {'title': 'http://www.sti-innsbruck.at/results/movies/serviceweb30-the-future-internet/', 'id': 231},
-                {'title': 'http://www.sti-innsbruck.at/dip-movie', 'id': 241},
-                {'title': 'http://www.w3.org/TR/xslt', 'id': 31},
-                {'title': 'Gruber, “Toward principles for the design of ontologies used or knowledge sharing?” , Int. J. Hum.-Comput. Stud., vol. 43, no. 5-6,1995', 'id': 232},
-                {'title': '[Guarino, 98] Formal Ontology in Information Systems www.loa-cnr.it/Papers/FOIS98.pdf', 'id': 242},
-                {'title': 'Fensel, D.; Kerrigan, M.; Zaremba, M. (Eds): Implementing Semantic Web Services: The SESA Framework. Springer 2008. ', 'id': 32},
-                {'title': 'http://linkeddata.org/ (last accessed on 18.03.2009) ', 'id': 233},
-                {'title': 'http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/Statistics, http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/LinkStatistics  (last accessed on 04.02.2010)', 'id': 243},
-                {'title': 'Anja Jentzsch and Chris Bizer ', 'id': 33},
-                {'title': 'http://www.youtube.com/watch?v=6gmP4nk0EOE', 'id': 234},
-                {'title': 'A Framework for Web Science; T. Berners-Lee and W. Hall and J. A. Hendler and K. O\'Hara and N. Shadbolt and D. J. Weitzner Foundations and Trends® in Web Science 1 (2006)', 'id': 244 },
-                {'title': 'http://webscience.org/images/collide.jpg', 'id': 34}
-            ];
-            callback(null, {datasources: datasources});
+            let datasources = [];
+
+            switch (args.sid) {
+                case '56':
+                    datasources = [
+                        {'title': 'http://www.sti-innsbruck.at/results/movies/serviceweb30-the-future-internet/', 'id': 231, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '56', 'originTitle': 'Semantic Web'},
+                        {'title': 'http://www.sti-innsbruck.at/dip-movie', 'id': 241, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '66', 'originTitle': 'Introduction'},
+                        {'title': 'http://www.w3.org/TR/xslt', 'id': 31, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '67', 'originTitle': 'RDF Data Model'},
+                        {'title': 'Gruber, “Toward principles for the design of ontologies used or knowledge sharing?” , Int. J. Hum.-Comput. Stud., vol. 43, no. 5-6,1995', 'id': 232, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': '[Guarino, 98] Formal Ontology in Information Systems www.loa-cnr.it/Papers/FOIS98.pdf', 'id': 242, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': 'Fensel, D.; Kerrigan, M.; Zaremba, M. (Eds): Implementing Semantic Web Services: The SESA Framework. Springer 2008. ', 'id': 32, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': 'http://linkeddata.org/ (last accessed on 18.03.2009) ', 'id': 233, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/Statistics, http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/LinkStatistics  (last accessed on 04.02.2010)', 'id': 243, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'Anja Jentzsch and Chris Bizer ', 'id': 33, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'http://www.youtube.com/watch?v=6gmP4nk0EOE', 'id': 234, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'},
+                        {'title': 'A Framework for Web Science; T. Berners-Lee and W. Hall and J. A. Hendler and K. O\'Hara and N. Shadbolt and D. J. Weitzner Foundations and Trends® in Web Science 1 (2006)', 'id': 244, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'},
+                        {'title': 'http://webscience.org/images/collide.jpg', 'id': 34, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'},
+                        {'title': 'http://webcast.bibalex.org/Presentations/Bebo91108.ppt', 'id': 235, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '681', 'originTitle': 'Introduction'},
+                        {'title': 'RadarNetworks&NovaSpivack,2007-www.radarnetworks.com ', 'id': 245, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '685', 'originTitle': 'Syntax'},
+                        {'title': 'http://widgets-gadgets.com/2006_10_01_archive.html', 'id': 35, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '685', 'originTitle': 'Syntax'}
+                    ];
+
+                    break;
+                case '66':
+                    datasources = [
+                        {'title': 'http://www.sti-innsbruck.at/dip-movie', 'id': 241, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '66', 'originTitle': 'Introduction'}
+                    ];
+                    break;
+                case '67':
+                    datasources = [
+                        {'title': 'http://www.w3.org/TR/xslt', 'id': 31, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '67', 'originTitle': 'RDF Data Model'},
+                        {'title': 'Gruber, “Toward principles for the design of ontologies used or knowledge sharing?” , Int. J. Hum.-Comput. Stud., vol. 43, no. 5-6,1995', 'id': 232, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': '[Guarino, 98] Formal Ontology in Information Systems www.loa-cnr.it/Papers/FOIS98.pdf', 'id': 242, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': 'Fensel, D.; Kerrigan, M.; Zaremba, M. (Eds): Implementing Semantic Web Services: The SESA Framework. Springer 2008. ', 'id': 32, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': 'http://linkeddata.org/ (last accessed on 18.03.2009) ', 'id': 233, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/Statistics, http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/LinkStatistics  (last accessed on 04.02.2010)', 'id': 243, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'Anja Jentzsch and Chris Bizer ', 'id': 33, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'http://www.youtube.com/watch?v=6gmP4nk0EOE', 'id': 234, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'},
+                        {'title': 'A Framework for Web Science; T. Berners-Lee and W. Hall and J. A. Hendler and K. O\'Hara and N. Shadbolt and D. J. Weitzner Foundations and Trends® in Web Science 1 (2006)', 'id': 244, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'},
+                        {'title': 'http://webscience.org/images/collide.jpg', 'id': 34, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'}
+                    ];
+                    break;
+                case '671':
+                    datasources = [
+                        {'title': 'Gruber, “Toward principles for the design of ontologies used or knowledge sharing?” , Int. J. Hum.-Comput. Stud., vol. 43, no. 5-6,1995', 'id': 232, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': '[Guarino, 98] Formal Ontology in Information Systems www.loa-cnr.it/Papers/FOIS98.pdf', 'id': 242, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'},
+                        {'title': 'Fensel, D.; Kerrigan, M.; Zaremba, M. (Eds): Implementing Semantic Web Services: The SESA Framework. Springer 2008. ', 'id': 32, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '671', 'originTitle': 'Introduction'}
+                    ];
+                    break;
+                case '673':
+                    datasources = [
+                        {'title': 'http://linkeddata.org/ (last accessed on 18.03.2009) ', 'id': 233, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/Statistics, http://esw.w3.org/topic/TaskForces/CommunityProjects/LinkingOpenData/DataSets/LinkStatistics  (last accessed on 04.02.2010)', 'id': 243, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'},
+                        {'title': 'Anja Jentzsch and Chris Bizer ', 'id': 33, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '673', 'originTitle': 'Serialization'}
+                    ];
+                    break;
+                case '678':
+                    datasources = [
+                        {'title': 'http://www.youtube.com/watch?v=6gmP4nk0EOE', 'id': 234, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'},
+                        {'title': 'A Framework for Web Science; T. Berners-Lee and W. Hall and J. A. Hendler and K. O\'Hara and N. Shadbolt and D. J. Weitzner Foundations and Trends® in Web Science 1 (2006)', 'id': 244, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'},
+                        {'title': 'http://webscience.org/images/collide.jpg', 'id': 34, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '678', 'originTitle': 'Examples'}
+                    ];
+                    break;
+                case '68':
+                    datasources = [
+                        {'title': 'http://webcast.bibalex.org/Presentations/Bebo91108.ppt', 'id': 235, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '681', 'originTitle': 'Introduction'},
+                        {'title': 'RadarNetworks&NovaSpivack,2007-www.radarnetworks.com ', 'id': 245, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '685', 'originTitle': 'Syntax'},
+                        {'title': 'http://widgets-gadgets.com/2006_10_01_archive.html', 'id': 35, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '685', 'originTitle': 'Syntax'}
+                    ];
+                    break;
+                case '681':
+                    datasources = [
+                        {'title': 'http://webcast.bibalex.org/Presentations/Bebo91108.ppt', 'id': 235, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'slide', 'originId': '681', 'originTitle': 'Introduction'}
+                    ];
+                    break;
+                case '685':
+                    datasources = [
+                        {'title': 'RadarNetworks&NovaSpivack,2007-www.radarnetworks.com ', 'id': 245, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '685', 'originTitle': 'Syntax'},
+                        {'title': 'http://widgets-gadgets.com/2006_10_01_archive.html', 'id': 35, 'nodeType': args.stype, 'nodeId': args.sid, 'originType': 'deck', 'originId': '685', 'originTitle': 'Syntax'}
+                    ];
+                    break;
+                default:
+                    datasources = [];
+            }
+            callback(null, {datasources: datasources, selector: selector});
         }
     }
     // other methods
