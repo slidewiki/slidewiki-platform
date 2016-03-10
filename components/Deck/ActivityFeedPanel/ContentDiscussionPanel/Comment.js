fcbe6633d4f29597cff611dd87cbbb4b9a2d8b26
Vuk Mijovic <vmijovic@gmail.com>
2016-03-10 16:44:39 +0100

    SWIK-95/SWIK-136 ContentDiscussionPanel: Reformated code a litle and added posibility open reply box for individual comments

---
+++ components/Deck/ActivityFeedPanel/ContentDiscussionPanel/Comment.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import formatDate from '../DateFormatter';
+
+function breakLines(text) {
+    return text.split('\n').map((line, key) => {
+        return (
+            <span key={key}>
+                    {line}
+                <br />
+                </span>
+        );
+    });
+}
+
+class Comment extends React.Component {
+    state = {
+        replyBoxOpened: false
+    };
+    handleReply() {
+        this.setState({
+            replyBoxOpened: !this.state.replyBoxOpened
+        });
+    }
+    render() {
+        const comment = this.props.comment;
+        const replyBox = (
+            <form className="ui reply form">
+                <div className="field">
+                    <textarea style={{minHeight: '6em', height: '6em'}}></textarea>
+                </div>
+                <div className="ui primary submit labeled icon button">
+                    <i className="icon edit"></i> Add Reply
+                </div>
+            </form>
+        );
+        return (
+            <div key={this.props.key} className="comment">
+                <a className="avatar">
+                    <i className="ui icon user" />
+                </a>
+                <div className="content">
+                    <a className="author" href={'/user/' + comment.author.id}>{comment.author.username}</a>
+                    <div className="metadata">
+                        <span className="date">{formatDate(comment.date)}</span>
+                    </div>
+                    <div className="text">
+                        <strong>{comment.title}</strong><br/>
+                        {breakLines(comment.text)}
+                    </div>
+                    <div className="actions">
+                        <a className="reply" onClick={this.handleReply.bind(this)}>Reply</a>
+                    </div>
+                    { this.state.replyBoxOpened ? replyBox : '' }
+                </div>
+                {comment.replies?<div className="comments">{comment.replies.map((reply, index) => { return (<Comment key={index} comment={reply} />); })}</div> : ''}
+            </div>
+        );
+    }
+}
+
+export default Comment;
--- components/Deck/ActivityFeedPanel/ContentDiscussionPanel/ContentDiscussionPanel.js
+++ components/Deck/ActivityFeedPanel/ContentDiscussionPanel/ContentDiscussionPanel.js
@@ -2,42 +2,9 @@ import React from 'react';
 import {NavLink} from 'fluxible-router';
 import {connectToStores} from 'fluxible-addons-react';
 import ContentDiscussionStore from '../../../../stores/ContentDiscussionStore';
-import formatDate from '../DateFormatter';
+import Comment from './Comment';
 
 class ContentDiscussionPanel extends React.Component {
-    breakLines(text) {
-        return text.split('\n').map((line, key) => {
-            return (
-                <span key={key}>
-                    {line}
-                    <br />
-                </span>
-            );
-        });
-    }
-    formatComment(comment, index) {
-        return (
-            <div key={index} className="comment">
-                <a className="avatar">
-                    <i className="ui icon user" />
-                </a>
-                <div className="content">
-                    <a className="author" href={'/user/' + comment.author.id}>{comment.author.username}</a>
-                    <div className="metadata">
-                        <span className="date">{formatDate(comment.date)}</span>
-                    </div>
-                    <div className="text">
-                        <strong>{comment.title}</strong><br/>
-                        {this.breakLines(comment.text)}
-                    </div>
-                    <div className="actions">
-                        <a className="reply">Reply</a>
-                    </div>
-                </div>
-                {comment.replies?<div className="comments">{comment.replies.map((reply, index) => {return this.formatComment(reply, index);})}</div> : ''}
-            </div>
-        );
-    }
     render() {
         let oldWay = (
             <div ref="contentDiscussionPanel" className="ui segment">
@@ -56,9 +23,9 @@ class ContentDiscussionPanel extends React.Component {
                         <i className="icon edit"></i> Add Comment
                     </div>
                 </form>
-                <h3 class="ui dividing header">Comments</h3>
+                <h3 className="ui dividing header">Comments</h3>
                 <div style={{ maxHeight: '600px', overflowY: 'auto' }}>
-                    {this.props.ContentDiscussionStore.discussion.map(this.formatComment.bind(this))}
+                    {this.props.ContentDiscussionStore.discussion.map((comment, index) => { return (<Comment key={index} comment={comment} />); })}
                 </div>
             </div>
         );
