44271441593218a85f249ae2cc93ef36da95040c
Vuk Mijovic <vmijovic@gmail.com>
2016-03-17 22:18:16 +0100

    SWIK-95/SWIK-132 Infinite Scroll for Activity Feed: mock microservice now also tells whether there are more elements to be fetched; some refactoring; fixed a bug that caused execution of too many loadMoreActivities actions

--- components/Deck/ActivityFeedPanel/ActivityList.js
+++ components/Deck/ActivityFeedPanel/ActivityList.js
@@ -7,61 +7,76 @@ import {connectToStores} from 'fluxible-addons-react';
 
 class ActivityList extends React.Component {
     renderItem(index, key) {
+        return (
+            <div className="ui item" key={key} style={{ margin: '1em 0'}}>
+                <ActivityItem activity={this.props.ActivityFeedStore.activities[index]} />
+            </div>
+        );
+    }
+    onScroll() {
+        const range = this.refs.infiniteList.getVisibleRange();
         const activities = this.props.ActivityFeedStore.activities;
-        if (index + 3 > activities.length && index < 200) {
+        if (activities.length - range[1] < 5 && !this.loading && this.props.ActivityFeedStore.hasMore) {
+            this.loading = true;
             const payload = {
                 params: this.props.ActivityFeedStore.selector,
                 newActivities: { latestId: activities[activities.length-1].id, numNew: 30 }
             };
-            //Todo: please fix the bug: only fecth service on scroll
-            //this.context.executeAction(loadMoreActivities, payload);
+            this.context.executeAction(loadMoreActivities, payload);
         }
-        const node = activities[index];
-        return (
-            <div className="ui item" key={key} style={{ margin: '1em 0'}}>
-                <ActivityItem activity={node} />
-            </div>
-        );
+    }
+    componentDidMount() {
+        this.loading = false;
+        this.refs.activityList.addEventListener('scroll', this.onScroll.bind(this));
+    }
+    componentWillReceiveProps(nextProps) {
+        // TODO: same as in the ActivityFeedStore; check if there is more elegant way to tell the component that action loadMoreActivities (in the onScroll function) was executed
+        if (!nextProps.ActivityFeedStore.wasFetch) return;
+        this.loading = false;
     }
     render() {
-        let rows = [];
-        let currentRow = [];
-        this.props.ActivityFeedStore.activities.forEach((item) => {
-            if (currentRow.length % 3 === 0 && currentRow.length > 0) {
-                rows.push((
-                    <div className="row" key={rows.length}>
-                        {currentRow}
-                    </div>
-                ));
-                currentRow = [];
-            }
-            currentRow.push((
-                <div className="ui column" key={currentRow.length}>
-                    <ActivityItem activity={item} />
-                </div>
-            ));
-        });
-        if (currentRow.length > 0) {
-            rows.push((
-                <div className="row" key={rows.length}>
-                    {currentRow}
-                </div>
-            ));
-        }
-        let list = this.props.ActivityFeedStore.activities.map((node, index) => {
-            return (
-                <div className="ui item" key={index} style={{ margin: '1em 0'}}>
-                    <ActivityItem activity={node} />
-                </div>
-            );
-        });
+        ////////////////////////////////////////////////////////
+        //legacy code (commented below), can be removed probably
+        ////////////////////////////////////////////////////////
+
+        //let rows = [];
+        //let currentRow = [];
+        //this.props.ActivityFeedStore.activities.forEach((item) => {
+        //    if (currentRow.length % 3 === 0 && currentRow.length > 0) {
+        //        rows.push((
+        //            <div className="row" key={rows.length}>
+        //                {currentRow}
+        //            </div>
+        //        ));
+        //        currentRow = [];
+        //    }
+        //    currentRow.push((
+        //        <div className="ui column" key={currentRow.length}>
+        //            <ActivityItem activity={item} />
+        //        </div>
+        //    ));
+        //});
+        //if (currentRow.length > 0) {
+        //    rows.push((
+        //        <div className="row" key={rows.length}>
+        //            {currentRow}
+        //        </div>
+        //    ));
+        //}
+        //let list = this.props.ActivityFeedStore.activities.map((node, index) => {
+        //    return (
+        //        <div className="ui item" key={index} style={{ margin: '1em 0'}}>
+        //            <ActivityItem activity={node} />
+        //        </div>
+        //    );
+        //});
         const listStyles = {
             maxHeight: '400px',
             overflowY: 'auto'
         };
         return (
             <div ref="activityList" style={listStyles}>
-                <ReactList className="ui list"
+                <ReactList ref="infiniteList" className="ui list"
                     itemRenderer={this.renderItem.bind(this)}
                     length={this.props.ActivityFeedStore.activities.length}
                     type={'variable'}>
