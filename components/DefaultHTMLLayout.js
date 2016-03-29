54c36a80214c4a53eccc287e3afce9ecc91f58f6
ali1k <hyperir@gmail.com>
2016-03-29 13:19:05 +0200

    added semantic-ui as submodule

--- components/DefaultHTMLLayout.js
+++ components/DefaultHTMLLayout.js
@@ -9,14 +9,14 @@ class DefaultHTMLLayout extends React.Component {
                 <meta charSet="utf-8" />
                 <title>{this.props.context.getStore(ApplicationStore).getPageTitle()}</title>
                 <meta name="viewport" content="width=device-width, user-scalable=no" />
-                <link href="/bower_components/semantic/dist/semantic.min.css" rel="stylesheet" type="text/css" />
+                <link href="/custom_modules/custom-semantic-ui/dist/semantic.min.css" rel="stylesheet" type="text/css" />
                 <link href="/assets/css/custom.css" rel="stylesheet" type="text/css" />
             </head>
             <body>
                 <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
                 <script src="/bower_components/jquery/dist/jquery.min.js"></script>
                 <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-                <script src="/bower_components/semantic/dist/components/progress.min.js"></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/progress.min.js"></script>
                 <script src={'/public/js/' + this.props.clientFile}></script>
             </body>
             </html>
