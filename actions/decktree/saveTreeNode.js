516a176c00bf1d823ab2ea0deeae068dcd5048f0
ali1k <hyperir@gmail.com>
2016-03-31 22:45:43 +0200

    added navigate in case of new node in tree

--- actions/decktree/saveTreeNode.js
+++ actions/decktree/saveTreeNode.js
@@ -1,9 +1,16 @@
+import {navigateAction} from 'fluxible-router';
+
 export default function saveTreeNode(context, payload, done) {
     context.service.update('decktree.nodeTitle', payload, {timeout: 20 * 1000}, (err, res) => {
         if (err) {
             context.dispatch('SAVE_TREE_NODE_FAILURE', err);
         } else {
             context.dispatch('SAVE_TREE_NODE_SUCCESS', payload);
+            //update the URL
+            let newURL = '/deck/' + payload.selector.id + '/' + payload.selector.stype + '/' + payload.selector.sid + '/' + payload.selector.spath;
+            context.executeAction(navigateAction, {
+                url: newURL
+            });
         }
         done();
     });
