fe7ea019c7b51dde6c46d9419952479a41a8f85d
dpaun <dpaunovic@gmail.com>
2016-04-14 09:44:12 +0200

    SWIK-94 - corrected loadDataSources action name

--- actions/datasource/loadDataSources.js
+++ actions/datasource/loadDataSources.js
@@ -1,5 +1,5 @@
 import {shortTitle} from '../../configs/general';
-export default function loadDataSource(context, payload, done) {
+export default function loadDataSources(context, payload, done) {
     context.service.read('datasource.list', payload, {timeout: 20 * 1000}, (err, res) => {
         if (err) {
             context.dispatch('LOAD_DATASOURCES_FAILURE', err);
