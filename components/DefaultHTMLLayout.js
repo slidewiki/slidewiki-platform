835d1b39513b821a9da6684ea84a162de8678c33
ali1k <hyperir@gmail.com>
2016-04-26 15:03:53 +0200

    fixed the path for semantic-ui

--- components/DefaultHTMLLayout.js
+++ components/DefaultHTMLLayout.js
@@ -18,12 +18,12 @@ class DefaultHTMLLayout extends React.Component {
                 <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
                 <script src="/custom_modules/custom-semantic-ui/dist/components/progress.min.js"></script>
                 <script src="/custom_modules/custom-semantic-ui/dist/components/accordion.min.js"></script>
-                <script src="/bower_components/semantic/dist/components/transition.min.js"></script>
-                <script src="/bower_components/semantic/dist/components/popup.min.js"></script>
-                <script src="/bower_components/semantic/dist/components/dropdown.min.js"></script>
-                <script src="/bower_components/semantic/dist/components/checkbox.min.js"></script>
-                <script src="/bower_components/semantic/dist/components/dimmer.min.js"></script>
-                <script src="/bower_components/semantic/dist/components/modal.min.js"></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/transition.min.js"></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/popup.min.js"></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/dropdown.min.js"></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/checkbox.min.js"></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/dimmer.min.js"></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/modal.min.js"></script>
                 <script src={'/public/js/' + this.props.clientFile}></script>
             </body>
             </html>
