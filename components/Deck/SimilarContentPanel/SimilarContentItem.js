ad1ff98d3f0e877a26c8d2d295559eb5df1fde8c
svalero <svalero@dsic.upv.es>
2016-05-02 13:01:35 +0200

    Thumbains now stored into assets/images

--- components/Deck/SimilarContentPanel/SimilarContentItem.js
+++ components/Deck/SimilarContentPanel/SimilarContentItem.js
@@ -4,7 +4,12 @@ class SimilarContentItem extends React.Component {
     render() {
         return (
             <div className="item">
-                  <a href={'/slideview/' + this.props.data.id}>{this.props.data.title}</a>
+                  <a href={'/deck/' + this.props.data.id}>
+				    <div className="thumbnail"> 
+					    <img src={'/assets/images/thumbnails/' + this.props.data.id + '.jpg'} height="40" width="60"/>
+					</div>
+				     <div className="thumbnailtitle">{this.props.data.title}</div>
+				  </a>
             </div>
         );
     }
