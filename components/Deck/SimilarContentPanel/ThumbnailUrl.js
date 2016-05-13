9efbc0b35deb5b8a56ba05293f777184a38e0be8
svalero <svalero@dsic.upv.es>
2016-05-13 10:44:04 +0200

    SWIK-121: SimilarContent component prepared to call thumbnail componet

--- components/Deck/SimilarContentPanel/ThumbnailUrl.js
+++ components/Deck/SimilarContentPanel/ThumbnailUrl.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Thumbnail from 'react-thumbnail';
+
+//import Thumbnail from 'react-thumbnail';
 
 class ThumbnailUrl extends React.Component{
     render(){
         return(
-          <Thumbnail width={40} height={60} page="http://www.gti-ia.upv.es/" />
+      //    <Thumbnail width={40} height={60} page="http://www.gti-ia.upv.es/" />
+        <img src={'/assets/images/thumbnails/' + this.props.id + '.jpg'} alt={'/deck/' + this.props.id} height={this.props.height}  width={this.props.width}/>
       );
     }
 
