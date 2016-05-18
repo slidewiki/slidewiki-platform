a730877324135082928ddc0845b26f8afe557969
kadevgraaf <kadegraaf@gmail.com>
2016-05-18 12:05:07 +0200

    SWIK 127 & 227 - Create a prototype for SlideEditPanel + feasibility alloy editor - full fix for refresh problem
    AlloyEditor is now only initialized as soon as slideEditContent
    (this.props.content) is loaded via actions/loadSlideView.js and
    received by the store.
    Also, AlloyEditor is (hopefully) loaded once/one time in
    componentDidUpdate() by checking if instance of CKeditor exists
    typeof(CKEDITOR.instances[this.props.container]) === 'undefined’
    This check on existence CKeditor might need to be replaced by a more
    accurate check (does Alloy-editor exist? or via states & SlideEditStore)

--- components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideEditPanel.js
+++ components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideEditPanel.js
@@ -6,35 +6,6 @@ import SlideContentEditor from './SlideContentEditor';
 
 class SlideEditPanel extends React.Component {
     render() {
-        //need mechanism to ensure that slide content is known/rendering before Alloy Editor is called
-        this.props.SlideEditStore.content = `
-        <h1> Slide #test</h1>
-        <div>
-            <p style="font-size: 1.16em;">
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus.
-            </p>
-            <ul>
-                <li>item 1 from slide test</li>
-                <li>item 2 from slide test</li>
-                <li>item 3 from slide test</li>
-            </ul>
-            <p style="font-size: 1.2em;">
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p style="text-align:center">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <text x="20"  y="40"
-                          style="font-family: Arial;
-                                 font-size  : 25;
-                                 stroke     : #000000;
-                                 fill       : #6a79dc;
-                                "
-                          > SVG Image test</text>
-                </svg>
-            </p>
-        </div>
-        `;
         return (
             <div ref="slideEditPanel" className="ui bottom attached segment">
                 <SlideContentEditor content={this.props.SlideEditStore.content} selector={this.props.selector} />
