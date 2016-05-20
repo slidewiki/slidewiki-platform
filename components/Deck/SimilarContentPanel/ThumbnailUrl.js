1395ed7d814a64f858b8e529a8ad6f7c6969fdb9
svalero <svalero@dsic.upv.es>
2016-05-20 14:26:50 +0200

    SWIK-121: Similar content now is displayed as a card

--- components/Deck/SimilarContentPanel/ThumbnailUrl.js
+++ components/Deck/SimilarContentPanel/ThumbnailUrl.js
@@ -1,33 +1,9 @@
 import React from 'react';
-import Thumbnail from './Thumbnail/thumbnail';
-//import Thumbnail from 'react-thumbnail';
 
-const styles = {
-    containerFlex: {
-        display: 'flex',
-        margin: '0 auto',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'stretch',
-        flexWrap: 'wrap',
-        fontFamily: 'sans-serif',
-    },
-    itemFlex: {
-        background: 'lightgray',
-        padding: 5,
-        margin: 5,
-    },
-    itemThumbnail: {
-        margin: '0 auto',
-        border: '1px solid black'
-    }
-};
-
-//  <img src={'/assets/images/thumbnails/' + this.props.id + '.jpg'} width={this.props.width} height={this.props.height} alt={'/deck/' + this.props.id}/>
 class ThumbnailUrl extends React.Component{
     render(){
         return(
-          <Thumbnail style={ styles.itemThumbnail } width={250}  height={250}  page="git-ia.dsic.upv.es"  keepAspectRatio="height" scale={4} />
+          <img src={'/assets/images/thumbnails/' + this.props.id + '.jpg'} width={this.props.width} height={this.props.height} alt={'/deck/' + this.props.id}/>
         );
     }
 
