86993f1638b49093946e22fa49272b0bd0cd64a0
kadevgraaf <kadegraaf@gmail.com>
2016-05-26 17:39:37 +0200

    SWIK 127 & 227 - Create a prototype for SlideEditPanel - Inline editor config, bug fixes, optimisation lay-out edit-screen, code clean-up

--- components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideContentEditor.js
+++ components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideContentEditor.js
@@ -4,31 +4,17 @@ import React from 'react';
 //import CKeditorComponent from './CKeditorComponent';
 //import CKEDITOR from 'ckeditor';
 //import ckeditor from 'ckeditor';
+let ReactDOM = require('react-dom');
 
 class SlideContentEditor extends React.Component {
     constructor(props) {
         super(props);
         this.currentcontent;
-        //this.currentCKeditorid;
     }
     componentDidMount() {
-        //AlloyEditorComponent.editable(this.refs.deckEditPanel.id);
-        //AlloyEditorComponent.editable(this.refs.deckEditPanel);
-        //AlloyEditorComponent.editable(this.props.container);
-        //AlloyEditorComponent.editable(this.props.container);
-
-        //AlloyEditor.editable('myContentEditable');
-
-        //AlloyEditor.editable('1');
-        //AlloyEditor.editable(this.props.selector.sid);
-        //CKEDITOR.replace('CKeditor1');
-        //let myInstanceName = this.props.selector.sid;
-        //if (CKEDITOR.instances[this.props.selector.sid]) CKEDITOR.instances[this.props.selector.sid].destroy();
-        //CKEDITOR.replace('nonInline');
-
         //TODO/bug? = inline-toolbar does not resize properly when zooming in browser. Does work in example on CKeditor website..
-
-        //if (typeof(CKEDITOR.instances.nonInline) === 'undefined'){CKEDITOR.replace('nonInline');}
+        //TODO: needs sharedspace plugin for proper positioning of inline toolbars + http://ckeditor.com/addon/closebtn plugin for closing inline editor
+        //TODO: refresh of edit pages resets the toolbar configuration to default - needs fix
         if (typeof(CKEDITOR.instances.inlineHeader) === 'undefined'){CKEDITOR.inline('inlineHeader', {
             toolbarGroups: [
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
@@ -39,15 +25,15 @@ class SlideContentEditor extends React.Component {
 		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
 		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'clipboard', groups: [ 'undo', 'clipboard' ] },
 		{ name: 'styles', groups: [ 'styles' ] },
 		{ name: 'tools', groups: [ 'tools' ] },
 		{ name: 'others', groups: [ 'others' ] },
 		{ name: 'about', groups: [ 'about' ] }
             ],
-            removeButtons: 'Source,Save,NewPage,Preview,Print,Templates,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Button,Select,HiddenField,ImageButton,Subscript,Superscript,RemoveFormat,NumberedList,Outdent,BulletedList,Indent,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,Maximize,ShowBlocks,About'
+            floatSpacePreferRight: true,
+            uiColor: '#4183C4',
+            removeButtons: 'Undo, Clipboard, Source,Save,NewPage,Preview,Print,Templates,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Button,Select,HiddenField,ImageButton,Subscript,Superscript,RemoveFormat,NumberedList,Outdent,BulletedList,Indent,Blockquote,CreateDiv,Language,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,Maximize,ShowBlocks,About'
         });}
-        if (typeof(CKEDITOR.instances.inlineContent) === 'undefined'){CKEDITOR.inline('inlineContent', {customConfig: '../../../../../../assets/ckeditor_config.js'});}
         //TODO - remove more buttons speakernotes
         if (typeof(CKEDITOR.instances.inlineSpeakerNotes) === 'undefined'){CKEDITOR.inline('inlineSpeakerNotes', {
             toolbarGroups: [
@@ -65,44 +51,25 @@ class SlideContentEditor extends React.Component {
 		{ name: 'others', groups: [ 'others' ] },
 		{ name: 'about', groups: [ 'about' ] }
             ],
+            floatSpacePreferRight: true,
+            uiColor: '#4183C4',
             removeButtons: 'Source,Save,NewPage,Preview,Print,Templates,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Button,Select,HiddenField,ImageButton,Subscript,Superscript,RemoveFormat,NumberedList,Outdent,BulletedList,Indent,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,Maximize,ShowBlocks,About'
         });}
-        if (typeof(CKEDITOR.instances.nonInline) === 'undefined'){CKEDITOR.replace('nonInline', {customConfig: '../../../../../../assets/ckeditor_config.js'});}
-        //CKEDITOR.replace('nonInline');
-        //CKEDITOR.inline('inlineHeader');
-        //CKEDITOR.inline('inlineContent');
-        //CKEDITOR.inline('inlineSpeakerNotes');
+        if (typeof(CKEDITOR.instances.inlineContent) === 'undefined'){CKEDITOR.inline('inlineContent', {customConfig: '../../../../../../assets/ckeditor_config.js'});}
+        //if (typeof(CKEDITOR.instances.nonInline) === 'undefined'){CKEDITOR.replace('nonInline', {customConfig: '../../../../../../assets/ckeditor_config.js'});}
         this.currentcontent = this.props.content;
-        //CKEDITOR.replace(this.props.container);
-        //CKEDITOR.replace(this.props.selector.sid);
-        //CKEDITOR.replace(this.refs.CKeditor1), {language: 'fr', uiColor: '#9AB8F3'};
-        //CKEDITOR.appendTO()
-    }
-    componentWillReceiveProps(nextProps) {
-        // check editor exists before use
-        //todo: update editor content
-        //CKEDITOR.replace(this.props.selector.sid);
-        //if (CKEDITOR.instances[this.props.selector.sid])
-        //CKEDITOR.instances[this.props.selector.sid].destroy();
-        //CKEDITOR.replace(this.props.selector.sid);
-        //alert('test' + this.props.selector.sid);
-        //alert(this.props.selector.sid + typeof(CKEDITOR.instances[this.props.selector.sid]));
-        //alert(typeof(CKEDITOR.instances[this.props.selector.sid]));
+        ReactDOM.findDOMNode(this.refs.inlineContent).focus();
+        //this.forceUpdate();
     }
     componentDidUpdate() {
-        //if (typeof(CKEDITOR.instances[this.props.selector.sid]) !== 'undefined' && this.currentCKeditorid !== this.props.selector.sid)
-        //{
-        //    CKEDITOR.instances[this.props.selector.sid].destroy();
-        //    CKEDITOR.replace(this.props.selector.sid);
-        //    this.currentCKeditorid = this.props.selector.sid;
-        //    alert('CKEDITOR destroyed, and ID of editor instance (currentCKeditorid) updated');
-        //}
+        /*
         if (typeof(CKEDITOR.instances.nonInline) !== 'undefined' && this.currentcontent !== this.props.content)
-        {   /*If an instance of CKeditor exists,
+        {   */
+        /*If an instance of CKeditor exists,
             **and
             **the content of the slide has changed because of navigating to different slide (not because of WYSIWYG edit = is handleEditorChange() instead )
             ** TODO - probably a more fluent solution would be to use a CKeditor function for updating.
-            */
+            *//*
             CKEDITOR.instances.nonInline.destroy();
             CKEDITOR.replace('nonInline', {customConfig: '../../../../../../assets/ckeditor_config.js'});
             //if (typeof(CKEDITOR.instances.inlineHeader) !== 'undefined'){CKEDITOR.instances.inlineHeader.destroy();CKEDITOR.inline('inlineHeader');}
@@ -111,65 +78,52 @@ class SlideContentEditor extends React.Component {
             this.currentcontent = this.props.content;
             //alert('CKEDITOR destroyed, and content updated');
         }
-        //let myInstanceName = this.props.selector.sid;
-        //if (CKEDITOR.instances[this.props.selector.sid]) CKEDITOR.instances[this.props.selector.sid].destroy();
-        //CKEDITOR.replace(this.props.selector.sid);
-        //alert('test' + this.props.selector.sid);
+        */
     }
     componentWillUnmount() {
         //TODO
-        CKEDITOR.instances.nonInline.destroy();
+        //CKEDITOR.instances.nonInline.destroy();
         CKEDITOR.instances.inlineHeader.destroy();
         CKEDITOR.instances.inlineContent.destroy();
         CKEDITOR.instances.inlineSpeakerNotes.destroy();
-        //AlloyEditor.destroy();
-        //AlloyEditor.destroy(true);
-        //let myInstanceName = this.props.selector.sid;
-        //if (CKEDITOR.instances[this.props.selector.sid])
-        //if (typeof(CKEDITOR.instances[this.props.selector.sid]) !== 'undefined')
-        //{
-        //    alert('test'); CKEDITOR.instances[this.props.selector.sid].destroy();
-        //}
-        //alert('test' + this.props.selector.sid + typeof(CKEDITOR.instances[this.props.selector.sid]));
-        //CKEDITOR.instances[this.props.selector.sid].destroy(true);
-        //AlloyEditor.instances[this.props.container].destroy(true);
     }
     handleEditorChange(e) {
+        //http://docs.ckeditor.com/#!/guide/dev_savedata
         console.log(e.target.getContent());
     }
     render() {
         //TODO: offer option to switch between inline-editor (alloy) and permanent/full editor (CKeditor)
-        const compStyle = {
+        //TODO - remove use of id - Only use 'ref=' for React. Find CKeditor create function(s) that do not require id.
+
+        const headerStyle = {
+            minWidth: '100%',
+            maxHeight: 60,
+            minHeight: 60,
+            overflowY: 'auto'
+        };        const contentStyle = {
             minWidth: '100%',
             maxHeight: 500,
             minHeight: 500,
             overflowY: 'auto'
         };
-        //<textarea style={compStyle} ref={this.props.selector.sid} value={this.props.content} onChange={this.handleEditorChange} />
-        //<AlloyEditorComponent container="1" ref={this.props.selector.sid} id={this.props.selector.sid} alloyEditorConfig={{}} content="test"></AlloyEditorComponent>
-        //<AlloyEditorComponent  style={compStyle} container="1" ref={this.props.selector.sid} id={this.props.selector.sid} alloyEditorConfig={{}} content={this.props.content}></AlloyEditorComponent>
-        //<textarea name="CKeditor1" ref="CKeditor1" rows="10" cols="80" content={this.props.content}></textarea>
-        //<CKeditorComponent container="myContentEditable" id="myContentEditable" content={this.props.content}></AlloyEditorComponent>
-        /*
-        <textarea contenteditable='true' name='Inline' ref='Inline' id='Inline' onChange={this.handleEditorChange} value="
-                            <h1>Inline example test</h1>
-                            <p> some text to be edited</p>
-                            <br />
-        ">
-        </textarea>
-        //TODO - remove use of id - Only use 'ref=' for React. Find CKeditor create function(s) that do not require id.
-        */
+        const speakernotesStyle = {
+            minWidth: '100%',
+            maxHeight: 120,
+            minHeight: 120,
+            overflowY: 'auto'
+        };
+        //<textarea style={compStyle} name='nonInline' ref='nonInline' id='nonInline' value={this.props.content} rows="10" cols="80" onChange={this.handleEditorChange}></textarea>
         return (
             <div>
+            <br />
+                Edit slide title:<br />
+                <div style={headerStyle} contentEditable='true' name='inlineHeader' ref='inlineHeader' id='inlineHeader' dangerouslySetInnerHTML={{__html:'<h1>SLIDE ' + this.props.selector.sid + ' TITLE</h1>'}}></div>
                 <br />
-                    <div contentEditable='true' name='inlineHeader' ref='inlineHeader' id='inlineHeader' dangerouslySetInnerHTML={{__html:'<br /><br /><br /><h1>Inline example test - SLIDE ' + this.props.selector.sid + ' TITLE</h1><br /><br />'}}></div>
-                    <br />
-                    <div contentEditable='true' name='inlineContent' ref='inlineContent' id='inlineContent' dangerouslySetInnerHTML={{__html:'<b>CONTENT to be edited</b>' + this.props.content}}></div>
-                    <br />
-                    <br />
-                    <div contentEditable='true' name='inlineSpeakerNotes' ref='inlineSpeakerNotes' id='inlineSpeakerNotes' dangerouslySetInnerHTML={{__html:'<b>Speaker Notes</b>'}}></div>
+                Edit slide content:<br />
+                <div style={contentStyle} contentEditable='true' name='inlineContent' ref='inlineContent' id='inlineContent' dangerouslySetInnerHTML={{__html:this.props.content}}></div>
                 <br />
-                <textarea style={compStyle} name='nonInline' ref='nonInline' id='nonInline' value={this.props.content} rows="10" cols="80" onChange={this.handleEditorChange}></textarea>
+                Edit speaker notes:<br />
+                <div style={speakernotesStyle} contentEditable='true' name='inlineSpeakerNotes' ref='inlineSpeakerNotes' id='inlineSpeakerNotes' dangerouslySetInnerHTML={{__html:'<b>Speaker Notes</b>'}}></div>
             </div>
         );
     }
