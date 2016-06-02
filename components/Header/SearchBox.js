f4e6ad69e14a8fdd86bd2e64036402f9ddaa6b3a
kadevgraaf <kadegraaf@gmail.com>
2016-06-02 13:40:29 +0200

    SWIK-267-268-269-Search Prototype

--- components/Header/SearchBox.js
+++ components/Header/SearchBox.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import {connectToStores} from 'fluxible-addons-react';
+import {NavLink, navigateAction} from 'fluxible-router';
+import SearchResultsStore from '../../stores/SearchResultsStore';
+let ReactDOM = require('react-dom');
 
 class SearchBox extends React.Component {
+    handleRedirect(searchstring){
+        //console.log('test' + this.refs.searchstring.value);
+        //let searchstring = this.refs.searchstring.value;
+        //let searchstring = 'RDFisGood';
+        //console.log(searchstring.value);
+        //console.log(this.value);
+        this.context.executeAction(navigateAction, {
+            //url: '/searchresults/' + searchstring
+            url: '/searchresults/RDFisGood'
+        });
+        return false;
+    }
     render() {
         return (
             <div className="ui small icon input" ref="searchBox">
-                <input type="text" placeholder="Search..." />
-                <i className="search link icon"></i>
+                <input type="text" placeholder="Search..." ref="searchstring" />
+                <i className="search link icon" onClick={this.handleRedirect.bind(this)} onChange={this.handleRedirect.bind(this)}></i>
             </div>
         );
     }
 }
+SearchBox.contextTypes = {
+    executeAction: React.PropTypes.func.isRequired
+};
+SearchBox = connectToStores(SearchBox, [SearchResultsStore], (context, props) => {
+    return {
+        SearchResultsStore: context.getStore(SearchResultsStore).getState()
+    };
+});
 
 export default SearchBox;
