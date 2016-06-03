a30e3ebf853497dbb0fdcb514c9021f8a4f06ab2
svalero <svalero@dsic.upv.es>
2016-06-03 12:03:12 +0200

    SWIK-147thumbnail adding a service to get thumbnails from slides

--- services/similarcontent.js
+++ services/similarcontent.js
@@ -3,20 +3,16 @@ export default {
     // At least one of the CRUD methods is Required
     read: (req, resource, params, config, callback) => {
         let args = params.params? params.params : params;
-        let selector = {'sid': args.sid, 'stype': args.stype};
-        let payload = {
-            args : args,
-            selector : selector
-        };
+        let selector= {'sid': args.did, 'stype': args.stype};
 
         if(resource === 'similarcontent.list'){
             /*********connect to microservices*************/
             //todo
             /*********received data from microservices*************/
             let contents = [
-                {'id': '16', 'title': 'title deck related to ' + args.sid, 'author': 'soeren' , 'authorId':'2' , 'date':'15/05/2016' ,'liked': '20', 'downloaded': '5', 'imgSrc': ''},
-                {'id': '28', 'title': 'title deck related to ' + args.sid, 'author': 'soeren', 'authorId':'2' , 'date':'18/04/2016' ,'liked': '15', 'downloaded': '6','imgSrc' : ''},
-                {'id': '39', 'title': 'title deck related to ' + args.sid, 'author': 'ali1k', 'authorId':'1' , 'date':'03/05/2016' ,'liked': '10', 'downloaded': '4', 'imgSrc': ''}
+                {'id': '16', 'title': 'title deck related to ' + args.did, 'author': 'soeren' , 'authorId':'2' , 'date':'15/05/2016' ,'liked': '20', 'downloaded': '5'},
+                {'id': '28', 'title': 'title deck related to ' + args.did, 'author': 'soeren', 'authorId':'2' , 'date':'18/04/2016' ,'liked': '15', 'downloaded': '6'},
+                {'id': '39', 'title': 'title deck related to ' + args.did, 'author': 'ali1k', 'authorId':'1' , 'date':'03/05/2016' ,'liked': '10', 'downloaded': '4'}
             ];
             callback(null, {contents: contents, selector: selector});
         }
--- services/thumbnail.js
+++ services/thumbnail.js
@@ -5,28 +5,34 @@ export default {
     // At least one of the CRUD methods is Required
     read: (req, resource, params, config, callback) => {
         let args = params.params? params.params : params;
-        let selector= {'sid': args.sid};
+
+        let selector= {'sid': args.sid, 'stype': args.stype};
 
         if(resource === 'thumbnail.htmlcontent'){ //html code is provided
             let imgSrc;
             let webPage;
             let phInstance;
+            //TODO: get the htmlContent from slide service.
+            let hmtlcontent = '<html><body>TEST</body></html>';
 
             phantom.create().then((instance) => {
                 phInstance = instance;
                 return instance.createPage();
             }).then((page) => {
-                page.setContent(args.contentHTML,'/slide/' + args.sid);
+                page.setContent(hmtlcontent,'/slide/' + arg.sid);
+
                 webPage = page;
                 return page.renderBase64('PNG');
             }).then((src) => {
                 webPage.close();
                 phInstance.exit();
-                imgSrc= 'data:image/png;charset=utf-8;base64,'+ src;
-                callback(null, {contents: {'src': imgSrc}, selector: selector});
+                res.render('users',{title: 'Users',
+                imgSrc: 'data:image/png;charset=utf-8;base64,'+ src});
+                let contents = 'src:' + imgSrc;
+                callback(null, {contents: contents, selector: selector});
 
             }).catch((error) => {
-                callback(error, {contents: {'src': {}}, selector: selector});
+                callback(error, {contents: contents, selector: selector});
                 phInstance.exit();
             });
 
