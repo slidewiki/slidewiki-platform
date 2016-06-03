d90a2ff61c93c1388bda9ea8f109c4eb2098590e
pennyg <pennyg@imis.athena-innovation.gr>
2016-06-03 12:02:25 +0300

    [search results - 1st attempt]

--- components/Search/SearchResultsPanel/SearchBoxBadge.js
+++ components/Search/SearchResultsPanel/SearchBoxBadge.js
@@ -1,96 +1,38 @@
 import React from 'react';
-import { NavLink } from 'fluxible-router';
 import {connectToStores} from 'fluxible-addons-react';
+import {NavLink, navigateAction} from 'fluxible-router';
+// import SearchResultsStore from '../../stores/SearchResultsStore';
 import SearchResultsStore from '../../../stores/SearchResultsStore';
-import SearchResultsItem from './SearchResultsItem';
-import loadSearchResults from '../../../actions/search/loadSearchResults';
+let ReactDOM = require('react-dom');
 
 class SearchBoxBadge extends React.Component {
-    componentDidMount() {
-        this.context.executeAction(loadSearchResults, {
-            
-        });
-        this.enablePopup();
-    }
-    componentDidUpdate(){
-        this.enablePopup();
-    }
-    enablePopup() {
-        let searchBoxBadge = this.refs.searchBoxBadge;
-        const visible = true;
-        if (visible) {
-            $(searchBoxBadge).popup({
-                inline   : true,
-                hoverable: true,
-                on: 'hover',
-                position : 'bottom left',
-                delay: {
-                    show: 100,
-                    hide: 300
-                }
-            });
-        }
-    }
 
-    removePopupIfNeeded() {
-        let searchBoxBadge = this.refs.searchBoxBadge;
-        // const visible = (this.props.UserNotificationsStore.newNotificationsCount > 0);
-        const visible = true;
-        if (!visible) {
-            $(notificationsBadge).popup('destroy');
-        }
-    }
-
-    hidePopup() {
-        let searchBoxBadge = this.refs.searchBoxBadge;
-        $(searchBoxBadge).popup('hide');
-        return true;
-    }
-
+    // handleRedirect(searchstring){
+    //     this.context.executeAction(navigateAction, {
+    //         //url: '/searchresults/' + searchstring
+    //         url: '/searchresults/RDFisGood'
+    //         // url: '/searchresults'
+    //     });
+    //     return false;
+    // }
     render() {
-        const selector = this.props.selector;
-
-        let noResultsMessage = '';
-
-        const results = this.props.SearchResultsStore.results;
-        const list = results.map((node, index) => {
-            return (
-                <SearchResultsItem key={index} data={node} />
-            );
-        });
-
         return (
-            <div onMouseOver={this.removePopupIfNeeded.bind(this)}>
-                <div ref="searchBoxBadge" onClick={this.hidePopup.bind(this)}>
-                    <div className="ui small icon input" ref="searchBox">
-                        <input type="text" placeholder="Search..." />
-                        <NavLink className="item right" routeName="results" navParams={{uid:57}} activeClass="active">
-                            <i className="search link icon" ></i>
-                        </NavLink>
-                    </div>
-                </div>
-                <div id="popup" className="ui special flowing popup">
-                    <h5>Search results</h5>
-                    <div ref="resultsList">
-                        <div className="ui relaxed divided list">
-                            {list}
-                        </div>
-                        <div >
-                            {noResultsMessage}
-                        </div>
-                    </div>
-                </div>
+            <div className="ui small icon input" ref="searchBox">
+                <input type="text" placeholder="Search..." ref="searchstring" />
+                <NavLink className="item right" routeName="searchresults" activeClass="active">
+                    <i className="search link icon" ></i>
+                </NavLink>
             </div>
         );
     }
 }
+// SearchBoxBadge.contextTypes = {
+//     executeAction: React.PropTypes.func.isRequired
+// };
+// SearchBoxBadge = connectToStores(SearchBoxBadge, [SearchResultsStore], (context, props) => {
+//     return {
+//         SearchResultsStore: context.getStore(SearchResultsStore).getState()
+//     };
+// });
 
-SearchBoxBadge.contextTypes = {
-    executeAction: React.PropTypes.func.isRequired
-};
-SearchBoxBadge = connectToStores(SearchBoxBadge, [SearchResultsStore], (context, props) => {
-    return {
-        SearchResultsStore: context.getStore(SearchResultsStore).getState()
-    };
-});
 export default SearchBoxBadge;
