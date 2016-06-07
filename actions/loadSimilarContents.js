017037d879fc47631b5c38fb34f5a989b3df8385
svalero <svalero@dsic.upv.es>
2016-06-07 19:43:06 +0200

    SWIK147/adding thumbnail as a servvice

--- actions/loadSimilarContents.js
+++ actions/loadSimilarContents.js
@@ -1,9 +1,13 @@
 import {shortTitle} from '../configs/general';
+import getSlideThumbnail from './getSlideThumbnail';
+
 export default function loadSimilarContents(context, payload, done) {
     context.service.read('similarcontent.list', payload, {timeout: 20 * 1000}, (err, res) => {
         if (err) {
             context.dispatch('LOAD_SIMILAR_CONTENT_FAILURE', err);
         } else {
+            console.log('loadSimilarContents: '+ res.contents[0].id);
+            context.executeAction(getSlideThumbnail,payload,done);
             context.dispatch('LOAD_SIMILAR_CONTENT_SUCCESS', res);
         }
         let pageTitle = shortTitle + ' | Similar Content | ' + payload.params.stype + ' | ' + payload.params.sid;
