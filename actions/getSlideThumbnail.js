80c15add7fa70842239893678320e88166d15fe5
svalero <svalero@dsic.upv.es>
2016-06-07 19:43:06 +0200

    SWIK147/adding thumbnail as a servvice

--- actions/getSlideThumbnail.js
+++ actions/getSlideThumbnail.js
@@ -3,7 +3,9 @@ export default function getSlideThumbnail(context, payload, done) {
     context.service.read('thumbnail.htmlcontent', payload, {timeout: 20 * 1000}, (err, res) => {
         if (err) {
             context.dispatch('GET_SLIDE_THUMBNAIL_FAILURE', err);
+            console.log('Error en thumb');
         } else {
+            console.log('getSlideThumbnail.SRC = '+ res.contents.src);
             context.dispatch('GET_SLIDE_THUMBNAIL_SUCCESS', res);
         }
 
--- services/thumbnail.js
+++ services/thumbnail.js
@@ -5,19 +5,52 @@ export default {
     // At least one of the CRUD methods is Required
     read: (req, resource, params, config, callback) => {
         let args = params.params? params.params : params;
-        let selector= {'sid': args.sid, 'stype': args.stype};
-
+        let selector= {'sid': args.sid};
+        console.log('entra en el servici');
         if(resource === 'thumbnail.htmlcontent'){ //html code is provided
             let imgSrc;
             let webPage;
             let phInstance;
+            let contents;
+
             //TODO: get the htmlContent from slide service.
-            let hmtlcontent = '<html><body>TEST</body></html>';
+            let sampleContent = `
+            <h1>Deck #` + args.sid + `</h1>
+            This is a sample deck content. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+            <br/>
+            <br/>
+            <div class="ui cards segment center aligned">
+              <div class="card">
+                <div class="content">
+                  <div class="header">Slide 1 from ` + args.sid + `</div>
+                  <div class="description">
+                    Elliot Fu is a film-maker from New York.
+                  </div>
+                </div>
+                <div class="ui bottom attached button">
+                  <i class="eye icon"></i>
+                  See details
+                </div>
+              </div>
+              <div class="card">
+                <div class="content">
+                  <div class="header">Slide 2 from ` + args.sid + `</div>
+                  <div class="description">
+                    Veronika Ossi is a set designer living in New York who enjoys kittens, music, and partying.
+                  </div>
+                </div>
+                <div class="ui bottom attached button">
+                  <i class="eye icon"></i>
+                  See details
+                </div>
+              </div>
+            </div>
+            `;
             phantom.create().then((instance) => {
                 phInstance = instance;
                 return instance.createPage();
             }).then((page) => {
-                page.setContent(hmtlcontent,'/slide/' + arg.sid);
+                page.setContent(sampleContent,'/slide/' + arg.sid);
                 webPage = page;
                 return page.renderBase64('PNG');
             }).then((src) => {
@@ -25,7 +58,7 @@ export default {
                 phInstance.exit();
                 res.render('users',{title: 'Users',
                 imgSrc: 'data:image/png;charset=utf-8;base64,'+ src});
-                let contents = 'src:' + imgSrc;
+                contents = 'src:' + imgSrc;
                 callback(null, {contents: contents, selector: selector});
 
             }).catch((error) => {
