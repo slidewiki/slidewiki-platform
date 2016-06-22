a39c4f87bccb65871d25ee59eb5088f293ca1302
ali1k <hyperir@gmail.com>
2016-06-22 16:34:07 +0200

    test js compression

--- components/DefaultHTMLLayout.js
+++ components/DefaultHTMLLayout.js
@@ -15,30 +15,30 @@ class DefaultHTMLLayout extends React.Component {
             <body>
                 <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
                 {/* Following are added only to support IE browser */}
-                <script src="/bower_components/es5-shim/es5-shim.min.js"></script>
-                <script src="/bower_components/es5-shim/es5-sham.min.js"></script>
-                <script src="/bower_components/json3/lib/json3.min.js"></script>
-                <script src="/bower_components/es6-shim/es6-shim.min.js"></script>
-                <script src="/bower_components/es6-shim/es6-sham.min.js"></script>
+                <script src="/bower_components/es5-shim/es5-shim.min.js" defer></script>
+                <script src="/bower_components/es5-shim/es5-sham.min.js" defer></script>
+                <script src="/bower_components/json3/lib/json3.min.js" defer></script>
+                <script src="/bower_components/es6-shim/es6-shim.min.js" defer></script>
+                <script src="/bower_components/es6-shim/es6-sham.min.js" defer></script>
                 {/* Above are added only to support IE browser */}
-                <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-                <script src="/bower_components/jquery/dist/jquery.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/progress.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/accordion.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/transition.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/popup.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/dropdown.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/checkbox.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/dimmer.min.js"></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/modal.min.js"></script>
+                <script dangerouslySetInnerHTML={{__html: this.props.state}} defer></script>
+                <script src="/bower_components/jquery/dist/jquery.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/progress.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/accordion.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/transition.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/popup.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/dropdown.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/checkbox.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/dimmer.min.js" defer></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/modal.min.js" defer></script>
                 {/* All external vendors bundle*/}
-                <script src={'/public/js/vendor.bundle.js'}></script>
-                <script src="/bower_components/ckeditor/ckeditor.js"></script>
+                <script src="/public/js/vendor.bundle.js" defer></script>
+                <script src="/bower_components/ckeditor/ckeditor.js" defer></script>
                 <script type="javascript">
                     CKEDITOR.disableAutoInline = true; //need to disable auto-initate to config inline toolbars
                 </script>
                 {/* Main app bundle */}
-                <script src={'/public/js/' + this.props.clientFile}></script>
+                <script src={'/public/js/' + this.props.clientFile} defer></script>
             </body>
             </html>
         );
