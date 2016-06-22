77629db4dec26d52cb6930b3bcb5db9a9da8d86c
ali1k <hyperir@gmail.com>
2016-06-22 16:39:51 +0200

    test 2 async load of js files

--- components/DefaultHTMLLayout.js
+++ components/DefaultHTMLLayout.js
@@ -15,25 +15,25 @@ class DefaultHTMLLayout extends React.Component {
             <body>
                 <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
                 {/* Following are added only to support IE browser */}
-                <script src="/bower_components/es5-shim/es5-shim.min.js" defer></script>
-                <script src="/bower_components/es5-shim/es5-sham.min.js" defer></script>
-                <script src="/bower_components/json3/lib/json3.min.js" defer></script>
-                <script src="/bower_components/es6-shim/es6-shim.min.js" defer></script>
-                <script src="/bower_components/es6-shim/es6-sham.min.js" defer></script>
+                <script src="/bower_components/es5-shim/es5-shim.min.js" async></script>
+                <script src="/bower_components/es5-shim/es5-sham.min.js" async></script>
+                <script src="/bower_components/json3/lib/json3.min.js" async></script>
+                <script src="/bower_components/es6-shim/es6-shim.min.js" async></script>
+                <script src="/bower_components/es6-shim/es6-sham.min.js" async></script>
                 {/* Above are added only to support IE browser */}
-                <script dangerouslySetInnerHTML={{__html: this.props.state}} defer></script>
-                <script src="/bower_components/jquery/dist/jquery.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/progress.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/accordion.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/transition.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/popup.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/dropdown.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/checkbox.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/dimmer.min.js" defer></script>
-                <script src="/custom_modules/custom-semantic-ui/dist/components/modal.min.js" defer></script>
+                <script dangerouslySetInnerHTML={{__html: this.props.state}} async></script>
+                <script src="/bower_components/jquery/dist/jquery.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/progress.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/accordion.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/transition.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/popup.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/dropdown.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/checkbox.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/dimmer.min.js" async></script>
+                <script src="/custom_modules/custom-semantic-ui/dist/components/modal.min.js" async></script>
                 {/* All external vendors bundle*/}
-                <script src="/public/js/vendor.bundle.js" defer></script>
-                <script src="/bower_components/ckeditor/ckeditor.js" defer></script>
+                <script src="/public/js/vendor.bundle.js" async></script>
+                <script src="/bower_components/ckeditor/ckeditor.js" async></script>
                 <script type="javascript">
                     CKEDITOR.disableAutoInline = true; //need to disable auto-initate to config inline toolbars
                 </script>
