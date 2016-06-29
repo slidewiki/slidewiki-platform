53782b6909e38195fe915f6215232926e833e49d
svalero <svalero@dsic.upv.es>
2016-06-29 15:06:12 +0200

    SWIK243-login aspect changed

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -25,13 +25,41 @@ class Login extends React.Component {
                 <br/>
                 <div className="fields">
                  <button className="ui primary button" type="submit"  >Login</button>
-              
+
                 </div>
                 </form>
               </div>
             </div>
 
           </div>
+
+          <div className="ui four column row">
+              <div className="seven wide column">
+                  <div className="ui blue padded center aligned segment">
+                      <h1 className="ui dividing header">Login</h1>
+                      <form className="ui form signin">
+
+                          <div className="ui icon input field">
+                              <label>E-mail</label>
+                              <input type="email" id="email1" name="email1" ref="email1" placeholder="Email" autoFocus tabIndex="0"  aria-required="true"/><i className="mail icon"></i>
+                          </div>
+                          <br/>
+                          <div className="ui icon input field">
+                              <label>Password</label>
+                              <input type="password" id="password1" name="password1" ref="password1" placeholder="Password" tabIndex="0"  aria-required="true"/><i className="lock icon"></i>
+                          </div><br/>
+                          <div className="ui error message"></div>
+                          <button type="submit" className="ui blue labeled submit icon button">
+                              <i className="icon sign in"></i> Login
+                          </button>
+                      </form>
+                      <br/>
+                      <div className="ui floated right ">
+                          <a href="">I can not access my account</a>
+                      </div>
+                  </div>
+              </div>
+            </div>
         </div>
         );
     }
