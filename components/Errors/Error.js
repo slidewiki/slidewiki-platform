0acc929511211ca85d0377129067fb00a27a2da2
Vinay Modi <info@vinaymodi.com>
2016-07-01 10:44:14 +0200

    Minor correction and adding TODO

--- components/Errors/Error.js
+++ components/Errors/Error.js
@@ -4,12 +4,12 @@ import DeckPageStore from '../../../stores/DeckPageStore';
 
 class ErrorComponent extends React.Component {
     let deckIdError = this.props.DeckPageStore.error ? this.props.DeckPageStore.error.msg : 0;
+    # TODO: use of deckIdError to show appropriate error message in render.
 
     render() {
         return (
             <div>
                 <p>Error!. TODO:Add appropriate CSS and message.</p>
-                </div>
             </div>
         );
     }
