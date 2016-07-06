58230b9b17af906eb39acec4d286a423205fa3b1
pennyg <pennyg@imis.athena-innovation.gr>
2016-07-06 16:41:09 +0300

    SWIK-267-268-269_Search_Prototype - missing file

---
+++ components/Search/SearchResultsPanel/SearchPanel.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {connectToStores} from 'fluxible-addons-react';
+import classNames from 'classnames/bind';
+import SearchResultsStore from '../../../stores/SearchResultsStore';
+import AdvancedSearch from '../AdvancedSearch/AdvancedSearch';
+import SearchResultsPanel from './SearchResultsPanel';
+
+class SearchPanel extends React.Component {
+    render() {
+
+        let advSearchDiv='';
+        if(this.props.SearchResultsStore.searchstatus === 'advsearch' || this.props.SearchResultsStore.searchstatus === 'advsearchresults'){
+            advSearchDiv = <AdvancedSearch />;
+        }
+
+        let searchResultsDiv='';
+        if(this.props.SearchResultsStore.searchstatus === 'results' || this.props.SearchResultsStore.searchstatus === 'advsearchresults'){
+            searchResultsDiv = <SearchResultsPanel />;
+        }
+
+
+        return (
+            <div ref="searchPanel">
+
+                <div className='advancedSearch' style={{margin: '1em'}}>
+                    {advSearchDiv}
+                </div>
+                <div className='searchResults' style={{margin: '1em'}}>
+                    {searchResultsDiv}
+                </div>
+
+            </div>
+        );
+    }
+}
+
+SearchPanel.contextTypes = {
+    executeAction: React.PropTypes.func.isRequired
+};
+SearchPanel = connectToStores(SearchPanel, [SearchResultsStore], (context, props) => {
+    return {
+        SearchResultsStore: context.getStore(SearchResultsStore).getState()
+    };
+});
+
+export default SearchPanel;
