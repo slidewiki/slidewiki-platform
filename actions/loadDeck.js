6c492abd503a397c043e51a6db5ef7c1b03e33e5
Vinay Modi <info@vinaymodi.com>
2016-07-11 17:05:00 +0200

    Working example of generic error component has been added.

--- actions/loadDeck.js
+++ actions/loadDeck.js
@@ -10,30 +10,30 @@ import loadActivities from './activityfeed/loadActivities';
 import loadSimilarContents from './loadSimilarContents';
 
 export default function loadDeck(context, payload, done) {
-    let http400 = "400 Bad request.";
+    let http400 = '400 Bad request.';
     if (!(Number.parseInt(payload.params.id) >= 0)) {
-        context.dispatch('DECK_ID_TYPE_ERROR', http400, "");
+        context.dispatch('DECK_ID_TYPE_ERROR', http400, '');
     }
 
     if (!(['deck', 'slide', 'question'].indexOf(payload.params.stype) > -1 || payload.params.stype === undefined)) {
         console.log('Content type incorrect. Loading deck failed.');
-        context.dispatch('DECK_ID_TYPE_ERROR', http400, "");
+        context.dispatch('DECK_ID_TYPE_ERROR', http400, '');
     }
 
 
     if (!(/^[0-9a-zA-Z]+$/.test(payload.params.sid) || payload.params.sid === undefined)) {
         console.log('Slide id incorrect. Loading deck failed.');
-        context.dispatch('DECK_ID_TYPE_ERROR', http400, "");
+        context.dispatch('DECK_ID_TYPE_ERROR', http400, '');
     }
 
     if (!(payload.params.spath || payload.params.spath === undefined)) {
         console.log('Incorrect path. Loading deck failed.');
-        context.dispatch('DECK_ID_TYPE_ERROR', http400, "");
+        context.dispatch('DECK_ID_TYPE_ERROR', http400, '');
     }
 
     if (!(payload.params.mode || payload.params.mode === undefined)) {
         console.log('Incorrect mode. Loading deck failed.');
-        context.dispatch('DECK_ID_TYPE_ERROR', http400, "");
+        context.dispatch('DECK_ID_TYPE_ERROR', http400, '');
     }
 
     //we should store the current content state in order to avoid duplicate load of actions
