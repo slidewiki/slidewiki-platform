4b9905a3d46371c1b87f4028ac2c1b5cff1f4b3c
svalero <svalero@dsic.upv.es>
2016-07-18 13:22:39 +0200

    SWIK134. adding a mising semicolom at importing setion of Presentation.js component

--- components/Deck/Presentation/Presentation.js
+++ components/Deck/Presentation/Presentation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import {NavLink} from 'fluxible-router';
 import {connectToStores} from 'fluxible-addons-react';
 //import PresentationSlideList from './PresentationSlideList';
