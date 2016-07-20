5a7ab6ad092ccb0cf22c20b3299754e2faebd05b
Vinay Modi <info@vinaymodi.com>
2016-07-20 13:01:06 +0200

    Handling Resource/URL Not Found 404 error case

---
+++ actions/loadRouteNotFound.js
@@ -0,0 +1,12 @@
+import async from 'async';
+import {shortTitle} from '../configs/general';
+import ErrorStore from '../stores/ErrorStore';
+import {ErrorsList} from '../components/Error/util/ErrorDescriptionUtil';
+let fumble = require('fumble');
+
+export default function loadRouteNotFound(context, payload, done) {
+    console.log('Action loadRouteNotFound');
+    let error = fumble.http.notFound();
+    context.dispatch('RESOURCE_NOT_FOUND_ERROR', ErrorsList.RESOURCE_NOT_FOUND_ERROR);
+    done(); // In place of done, you can also write: throw error
+}
