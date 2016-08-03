9bd242422a336253b2ce6928103b8e58c3d66e49
Stavros Maroulis <stavrosmrl@gmail.com>
2016-08-03 13:25:07 +0300

    SWIK-334 - Create prototype for revisions panel

--- components/Deck/ActivityFeedPanel/ContentHistoryPanel/ContentHistoryPanel.js
+++ components/Deck/ActivityFeedPanel/ContentHistoryPanel/ContentHistoryPanel.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {NavLink} from 'fluxible-router';
 import {connectToStores} from 'fluxible-addons-react';
 import ContentHistoryStore from '../../../../stores/ContentHistoryStore';
+import ContentHistoryList from './ContentHistoryList';
 
 class ContentHistoryPanel extends React.Component {
     render() {
         return (
             <div ref="contentHistoryPanel" className="ui">
-                History related to {this.props.ContentHistoryStore.selector.stype} #{this.props.ContentHistoryStore.selector.sid}.
-                <br/>
-                <NavLink href={'/history/' + this.props.ContentHistoryStore.selector.stype + '/' + this.props.ContentHistoryStore.selector.sid}>{'/history/' + this.props.ContentHistoryStore.selector.stype + '/' + this.props.ContentHistoryStore.selector.sid}</NavLink>
+                {/*History related to {this.props.ContentHistoryStore.selector.stype}
+                #{this.props.ContentHistoryStore.selector.sid}.
+                <br/>*/}
+                {/*<NavLink href={'/history/' + this.props.ContentHistoryStore.selector.stype + '/' + this.props.ContentHistoryStore.selector.sid}>{'/history/' + this.props.ContentHistoryStore.selector.stype + '/' + this.props.ContentHistoryStore.selector.sid}</NavLink>*/}
+                <div>
+                    <ContentHistoryList items={this.props.ContentHistoryStore.history} selector={this.props.ContentHistoryStore.selector}/>
+                </div>
             </div>
         );
     }
