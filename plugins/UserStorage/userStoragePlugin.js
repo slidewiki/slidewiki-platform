b339aeb024c2fea8063e6e35733bb21cd482e482
Kurt Junghanns <tboonx@googlemail.com>
2016-08-24 15:06:04 +0200

    [SWIK-455_save_user_data] Added basis for new plugin

---
+++ plugins/UserStorage/userStoragePlugin.js
@@ -0,0 +1,77 @@
+import cookie from 'cookie';
+
+module.exports = function userStoragePlugin(options) {
+    /**
+     * @class userStorage
+     */
+    return {
+        name: 'UserStoragePlugin',
+        // Called after context creation to dynamically create a context plugin
+        plugContext: function plugContext(contextOptions) {
+            // `options` is the same as what is passed into `Fluxible.createContext(options)`
+            let user = null;
+            if (contextOptions.req) {
+                user = contextOptions.req.user;
+            }
+
+            //from cookie plugin
+            let req = contextOptions.req;
+            let res = contextOptions.res;
+
+            let cookies = req ? req.cookies : cookie.parse(document.cookie);
+
+            // Returns a context plugin
+            return {
+                /**
+                 * Provides full access to the user in the action context
+                 * @param {Object} actionContext
+                 */
+                plugActionContext: function plugActionContext(actionContext) {
+                    actionContext.getUser = function () {
+                        return user;
+                    };
+                    actionContext.setCookie = function (name, value, options) {
+                        var cookieStr = cookie.serialize(name, value, options);
+                        if (res) {
+                            res.setHeader('Set-Cookie', cookieStr);
+                        } else {
+                            document.cookie = cookieStr;
+                        }
+                        cookies[name] = value;
+                    };
+                    actionContext.getCookie = function (name) {
+                        return cookies[name];
+                    };
+                },
+                /**
+                 * Provides access to user
+                 * @param {Object} componentContext
+                 */
+                plugComponentContext: function plugComponentContext(componentContext) {
+                    componentContext.getUser = function () {
+                        return user;
+                    };
+                },
+                /**
+                 * Provides access to user
+                 * @param {Object} storeContext
+                 */
+                plugStoreContext: function plugStoreContext(storeContext) {
+                    storeContext.getUser = function () {
+                        return user;
+                    };
+                },
+                dehydrate: function () {
+                    return {
+                        user: user
+                    };
+                },
+                // Called on client to rehydrate the context plugin settings
+                rehydrate: function (state) {
+                    user = state.user;
+                }
+            };
+        },
+
+    };
+};
