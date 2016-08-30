7860826d96adbd0d4e1fe5e4a8fb34a638d70b0b
Kurt Junghanns <tboonx@googlemail.com>
2016-08-30 10:58:15 +0200

    [SWIK-455_save_user_data] Fixed cookie handling

--- actions/user/userSignOut.js
+++ actions/user/userSignOut.js
@@ -1,6 +1,6 @@
 
 export default function userSignOut(context, payload, done) {
-    context.setUser({}); //clear user (is cookie) via userStoragePlugin
+    context.deleteUser(); //clear user (is cookie) via userStoragePlugin
     context.dispatch('USER_SIGNOUT', payload);
     done();
 }
--- plugins/UserStorage/userStoragePlugin.js
+++ plugins/UserStorage/userStoragePlugin.js
@@ -1,4 +1,4 @@
-import cookie from 'cookie';
+import cookie from 'js-cookie';
 
 module.exports = function userStoragePlugin(options) {
     /**
@@ -10,10 +10,7 @@ module.exports = function userStoragePlugin(options) {
         // Called after context creation to dynamically create a context plugin
         plugContext: function plugContext(contextOptions) {
             // `options` is the same as what is passed into `Fluxible.createContext(options)`
-            let user = null;
-            if (contextOptions.req) {
-                user = contextOptions.req.user;
-            }
+            let user = undefined;
             const user_cookieName = 'user_json_storage';
 
             //original from fluxible-plugin-cookie
@@ -21,9 +18,7 @@ module.exports = function userStoragePlugin(options) {
             let res = contextOptions.res;
 
             //get cookies from cookie-parser plugin
-            let cookies = req ? req.cookies : cookie.parse(document.cookie);
-
-            console.log('UserStoragePlugin plugContext:', cookies, res ? 'result is defined - server-side' : 'res is undefined - client-side');
+            let cookies = req ? req.cookies : cookie.get();
 
             const secondsCookieShouldBeValid = 60*60*24*14 ;  //2 weeks
             let createExpire = () => {
@@ -37,7 +32,9 @@ module.exports = function userStoragePlugin(options) {
 
             //get user from cookies if undefined
             if (user === undefined && cookies !== undefined && cookies[user_cookieName] !== undefined)
-                user = JSON.parse(cookies[user_cookieName]);
+                user = cookie.getJSON(user_cookieName);
+
+            console.log('UserStoragePlugin plugContext:', cookies, res ? 'result is defined - server-side' : 'res is undefined - client-side', user);
 
             // Returns a context plugin
             return {
@@ -47,10 +44,13 @@ module.exports = function userStoragePlugin(options) {
                  */
                 plugActionContext: function plugActionContext(actionContext) {
                     actionContext.getUser = function () {
+                        console.log('userStoragePlugin actionContext getUser()');
+
                         let result = user;
 
                         if (result === undefined || result === null || result === {}) {
-                            result = JSON.parse(actionContext.getCookie(user_cookieName));
+                            result = cookie.getJSON(user_cookieName);
+                            console.log('userStoragePlugin actionContext getUser: got user from cookies');
                         }
 
                         return result;
@@ -61,28 +61,23 @@ module.exports = function userStoragePlugin(options) {
 
                         user = newUser;
 
-                        actionContext.setCookie(user_cookieName, JSON.stringify(newUser), {
+                        cookie.set(user_cookieName, newUser,{
                             expires: createExpire(),
                             maxAge: secondsCookieShouldBeValid
                         });
                     };
                     actionContext.deleteUser = function() {
-                        actionContext.setUser({});
+                        user = {};
+
+                        cookie.remove(user_cookieName);
                     };
                     actionContext.setCookie = function (name, value, options) {
                         console.log('userStoragePlugin actionContext setCookie:', name, value, options);
-                        const cookieStr = cookie.serialize(name, value, options);
-                        if (res) {
-                            console.log('userStoragePlugin actionContext setCookie: on server-side');
-                            res.setHeader('Set-Cookie', cookieStr);
-                        } else {
-                            console.log('userStoragePlugin actionContext setCookie: on client-side');
-                            document.cookie = cookieStr;
-                        }
+                        const cookieStr = cookie.set(name, value, options);
                         cookies[name] = value;
                     };
                     actionContext.getCookie = function (name) {
-                        return cookies[name];
+                        return cookies[name] ? cookies[name] : cookie.get(name);
                     };
                 },
                 /**
