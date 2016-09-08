cc68e183c13dc71121c13e58a89b46938c663911
Vinay Modi <info@vinaymodi.com>
2016-09-08 17:04:11 +0200

    For #SWIK-409, deck view implementation. Fetch deck data, slides data and user data. Avoid nested promise (that is anti-pattern).

--- services/deck.js
+++ services/deck.js
@@ -7,14 +7,80 @@ export default {
     read: (req, resource, params, config, callback) => {
         let args = params.params? params.params : params;
         if (resource === 'deck.content') {
-            rp.get({uri: Microservices.deck.uri + '/deck/' + args.sid + '/slides'})
-                .then((res) => {
-                    console.log('From deck.js service:', res);
-                    callback(null, {content: res});})
+            /* Create promise for deck data success */
+            let deckRes = rp.get({uri: Microservices.deck.uri + '/deck/' + args.sid});
+            /* Create promise for slides data success */
+            let slidesRes = rp.get({uri: Microservices.deck.uri + '/deck/' + args.sid + '/slides'});
+            /* Catch errors from deck data response */
+            let deckPromise = deckRes.catch((err) => {
+                callback({msg: 'Error in retrieving deck meta data ' + Microservices.deck.uri + ',', content: err}, {});
+            });
+            /* Catch erros from slides data response */
+            let slidesPromise = slidesRes.catch((err) => {
+                callback({msg: 'Error in retrieving slides data from ' + Microservices.deck.uri + ' service! Please try again later...', content: err}, {});
+            });
+            /* Create user data promise which is dependent on deck data promise */
+            let userRes = deckPromise.then((deckData) => {
+                //TODO Replace user id '4' with JSON.parse(data[0]).user when deckservice and userservice data is in sync
+                return rp.get({uri: Microservices.user.uri + '/user/' + '15'});//(JSON.parse(deckData).user).toString()});//JSON.parse(deckData).user});
+            });
+            /* Catch errors from the user data response */
+            let userPromise = userRes.catch((err) => {
+                callback({msg: 'Error in retrieving user data from ' + Microservices.user.uri, content: err}, {});
+            });
+
+            /* Create promise which resolves when all the three promises are resolved or fails when any one of the three promises fails */
+            Promise.all([deckPromise, slidesPromise, userPromise]).then((data) => {
+                console.log(data);
+                callback(null, {deckMetaData: JSON.parse(data[0]), slidesData: JSON.parse(data[1]), userData: JSON.parse(data[2])});
+            }).catch((err) => {
+                console.log(err);
+                callback({msg: 'Error in resolving promiese', content: err}, {});
+            });
+
+        /*
+
+            let deckRes = rp.get({uri: Microservices.deck.uri + '/deck/' + args.sid});
+            let slidesRes = deckRes.then(() => {
+                console.log('Inside slidesRes promise');
+                return rp.get({uri: Microservices.deck.uri + '/deck/' + args.sid + '/slides'});
+            }).catch((err) => {
+                callback({msg: 'Error in retrieving deck meta data ' + Microservices.deck.uri + ',', content: err}, {});
+            });
+            return slidesRes.then(() => {
+                console.log('After slidesRes resolved');
+                //let data = {deckMetaData: JSON.parse(deckRes.value()), slidesData: JSON.parse(slidesRes.values())};
+                console.log(deckRes);
+                callback(null, {deckMetaData: JSON.parse(deckRes), slidesData: JSON.parse(slidesRes)});
+            }).catch((err) => {
+                callback({msg: 'Error in retrieving slides data from ' + Microservices.deck.uri + ' service! Please try again later...', content: err}, {});
+            });
+        */
+
+            /*
+            // Get deck meta data
+            rp.get({uri: Microservices.deck.uri + '/deck/' + args.sid})
+                .then((deckRes) => {
+                    console.log('Deck meta data:', deckRes);
+
+                    // Retrieving slides for the given deck
+                    rp.get({uri: Microservices.deck.uri + '/deck/' + args.sid + '/slides'})
+                        .then((slidesRes) => {
+                            console.log('Slides data:', slidesRes);
+                            callback(null, {deckMetaData: JSON.parse(deckRes), slidesData: JSON.parse(slidesRes)});
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            callback({msg: 'Error in retrieving slides data from ' + Microservices.deck.uri + ' service! Please try again later...', content: err}, {});
+                        });
+
+                })
                 .catch((err) => {
                     console.log(err);
-                    callback({msg: 'Error in retrieving data from ' + Microservices.deck.uri + ' service! Please try again later...', content: err}, {});});
-                    //callback(null, {content: {}, selector: selector, 'page': params.page, 'mode': args.mode});
+                    callback({msg: 'Error in retrieving deck meta data ' + Microservices.deck.uri + ',', content: err}, {});
+                });
+                */
+                //callback(null, {content: {}, selector: selector, 'page': params.page, 'mode': args.mode});
         }
             /*********connect to microservices*************/
             //todo
