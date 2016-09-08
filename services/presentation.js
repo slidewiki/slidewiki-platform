8ed64a2761b4713ca72cab5462a3c2174c664ec2
svalero <svalero@dsic.upv.es>
2016-09-08 13:52:12 +0200

    SWIK134. Remove mock data generators from presentation service

--- services/presentation.js
+++ services/presentation.js
@@ -33,38 +33,3 @@ function get_sample_theme(){
     // Just hardcode this for now.  Change to themes[index] if we really want it random.
     return themes[1];
 }
-
-function get_sample_text(id){
-    return `
-    <h1> Slide #` + id + `</h1>
-    <div>
-        <p style='font-size: 1.16em;'>
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus.
-        </p>
-        <ul>
-            <li>item 1 from slide ` + id + `</li>
-            <li>item 2 from slide ` + id + `</li>
-            <li>item 3 from slide ` + id + `</li>
-        </ul>
-        <p style='font-size: 1.2em;'>
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-        </p>
-        <p style='text-align:center'>
-            <svg xmlns='http://www.w3.org/2000/svg'
-                 xmlns:xlink='http://www.w3.org/1999/xlink'>
-                <text x='20'  y='40'
-                      style='font-family: Arial;
-                             font-size  : 25;
-                             stroke     : #000000;
-                             fill       : #` +((1<<24)*Math.random()|0).toString(16) + `;
-                            '
-                      > SVG Image ` + id + `</text>
-            </svg>
-        </p>
-    </div>
-    `;
-}
-
-function get_sample_notes(id){
-    return 'These are some really important notes for Slide ' + id;
-}
