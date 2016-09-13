66e355f82afe3a09ee9fb61ef7b5583e95dd72fa
Vinay Modi <info@vinaymodi.com>
2016-09-13 12:42:16 +0200

    For #SWIK-409, using reactjs map for dynamically building html. Changing variable names and changing CSS class to match the mockup.

--- components/Deck/ContentPanel/DeckModes/DeckViewPanel/SlideThumbnail.js
+++ components/Deck/ContentPanel/DeckModes/DeckViewPanel/SlideThumbnail.js
@@ -6,38 +6,29 @@ import SlideThumbnailStore from '../../../../../stores/SlideThumbnailStore';
 class SlideThumbnail extends React.Component {
     render() {
         const slides = [];
-        /* We do not want to show all slides; 8 slides at max fits well in the available mockup. */
-        const maxSlidesToShow = this.props.slidesData.children.length > 8 ? 8 : this.props.slidesData.children.length;
-        for (let slideNumber = 0; slideNumber < maxSlidesToShow; slideNumber++) {
-            slides.push(
-                <div className="column">
-                    <div className="ui segment">
-                        <div className="content" tabIndex="0">
-                            <a className="header" dangerouslySetInnerHTML={{__html:this.props.slidesData.children[slideNumber].title}} />
-                            <div className="description" dangerouslySetInnerHTML={{__html:this.props.slidesData.children[slideNumber].content}} />
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-
+        /* We do not want to show all slides; */
+        const maxSlideThumbnails = this.props.maxSlideThumbnails === undefined ? this.props.slidesData.children.length : this.props.maxSlideThumbnails;
         return (
-            <div className="ui four column grid container">{slides}</div>
-        /*
-        <div className="column">
-            <div ref="slideViewPanel" className="ui segment">
-                <div className="content" tabIndex="0">
-                    <a href="http://google.com" className="ui small image" tabIndex="-1">
-                        <img src="assets/images/wireframe/image.png" alt="thumbnail of slide" />
-                    </a>
-                    <a className="header" dangerouslySetInnerHTML={{__html:this.props.slidesData.children[0].title}} />
-                    <div className="description">Slide 4 of 30</div>
-                    <div dangerouslySetInnerHTML={{__html:this.props.SlideThumbnailStore.title}} />
-                    <div dangerouslySetInnerHTML={{__html:this.props.SlideThumbnailStore.content}} />
-                 </div>
+            <div className="ui four column grid container">
+                {this.props.slidesData.children.map((slide, index) => {
+                    if (index < maxSlideThumbnails) {
+                        return  <div className="column">
+                                    <div className="ui card">
+                                        <div className="content" tabIndex="0">
+                                            <a href="http://localhost:3000/" className="ui small image" tabIndex="-1">
+                                                <img src="/assets/images/logo_full.png" alt="thumbnail of slide" />
+                                            </a>
+                                            <a className="header" dangerouslySetInnerHTML={{__html:slide.title}} />
+                                            <div className="description">Slide {index + 1} of {this.props.slidesData.children.length}</div>
+                                        </div>
+                                    </div>
+                                </div>;
+                    }
+                    else {
+                        return '';
+                    }
+                })}
             </div>
-        </div>
-        */
         );
     }
 }
