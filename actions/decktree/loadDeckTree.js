32a8592f4ea5cbe8037d3c245d67638775367793
Stavros Maroulis <stavrosmrl@gmail.com>
2016-09-21 17:34:33 +0300

    Add flag to force deck tree fetching in loadDeckTree action

--- actions/decktree/loadDeckTree.js
+++ actions/decktree/loadDeckTree.js
@@ -1,41 +1,48 @@
 import {shortTitle} from '../../configs/general';
 import DeckTreeStore from '../../stores/DeckTreeStore';
-import { deckIdTypeError, deckContentPathError, serviceUnavailable } from '../loadErrors';
+import {deckIdTypeError, deckContentPathError, serviceUnavailable} from '../loadErrors';
 
 export default function loadDeckTree(context, payload, done) {
     if (!(/^[0-9-]+$/.test(payload.params.id) && Number.parseInt(payload.params.id) >= 0)) {
-        context.executeAction(deckIdTypeError, payload).catch((err) => {done(err);});
+        context.executeAction(deckIdTypeError, payload).catch((err) => {
+            done(err);
+        });
         return;
     }
 
-    if (!(payload.params.spath && (/^[0-9a-z:;-]+$/.test(payload.params.spath)) ||
-        payload.params.spath === undefined ||
-        payload.params.spath === '')) {
-        context.executeAction(deckContentPathError, payload).catch((err) => {done(err);});
+    if (!(payload.params.spath && (/^[0-9a-z:;-]+$/.test(payload.params.spath)) || payload.params.spath === undefined || payload.params.spath === '')) {
+        context.executeAction(deckContentPathError, payload).catch((err) => {
+            done(err);
+        });
         return;
     }
 
     let currentSelector = context.getStore(DeckTreeStore).getSelector();
+
     let runFetchTree = 1;
-    if(currentSelector.id === payload.params.id){
+
+    //runFetchTree flag may be passed through the navigate action to force deck tree fetch
+    if (!payload.navigate.runFetchTree && currentSelector.id === payload.params.id) {
         runFetchTree = 0;
     }
     let pageTitle = shortTitle + ' | Deck Tree | ' + payload.params.id;
     context.dispatch('UPDATE_PAGE_TITLE', {
         pageTitle: pageTitle
     });
-    if(runFetchTree){
+    if (runFetchTree) {
         //we need to load the whole tree for the first time
         context.service.read('decktree.nodes', payload, {}, (err, res) => {
             if (err) {
-                context.executeAction(serviceUnavailable, payload).catch((error) => {done(error);});
+                context.executeAction(serviceUnavailable, payload).catch((error) => {
+                    done(error);
+                });
                 return;
             } else {
                 context.dispatch('LOAD_DECK_TREE_SUCCESS', res);
             }
             done();
         });
-    }else{
+    } else {
         //when we only select the node in tree, there is no need to call the external service
         context.dispatch('SELECT_TREE_NODE_SUCCESS', payload.params);
         done();
