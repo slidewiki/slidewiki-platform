9ed118ba2dbbb65fecede62f92c393ed66b1722c
ali1k <hyperir@gmail.com>
2016-09-27 15:09:18 +0200

    fixed the html validation error

--- components/Home/About.js
+++ components/Home/About.js
@@ -8,9 +8,7 @@ class About extends React.Component {
                     <div className="column">
                         <div className="ui content">
                             <h2 className="ui header">About SlideWiki</h2>
-                            <p>
-
-                                <p>SlideWiki aims to exploit the wisdom, creativity and productivity of the crowd for the creation of qualitative, rich,
+                            <p>SlideWiki aims to exploit the wisdom, creativity and productivity of the crowd for the creation of qualitative, rich,
                                 engaging educational content. With SlideWiki users can create and collaborate on slides, diagrams, assessments and arrange this
                                 content in richly-structured course presentations.</p>
                                 <p>SlideWiki empowers communities of educators to author, share and re-use sophisticated educational content in a truly collaborative way.
@@ -22,21 +20,22 @@ class About extends React.Component {
                                 <p>With SlideWiki we aim to make educational content dramatically more accessible, interactive, engaging and qualitative.</p>
                                 <p>Slidewiki is an open-source platform, and all its content can be reused under CC-BY license. SlideWiki development, large-scale trials
                                 and underlying reseach is funded from Framework Programme for Research and Innovation Horizon 2020 under grant agreement no 688095.
-                                For more details, see <a href="http://slidewiki.eu">SlideWiki EU project website</a></p>
-                                <h3> The final-release functionality of the SlideWiki platform will include, but not limited to: </h3>
-                                <ul>
-                                    <li>WYSIWYG slide authoring</li>
-                                    <li>Logical slide and deck representation</li>
-                                    <li>LaTeX/MathML integration</li>
-                                    <li>Multilingual decks / semi-automatic translation / synchronization of content between translated versions</li>
-                                    <li>Import/export of the content into a number of formats (PDF, SCORM, ePUB, PowerPoint, OpenOffice and others)</li>
-                                    <li>Dynamic CSS themability and transitions</li>
-                                    <li>Support of social networking activities</li>
-                                    <li>Full revisioning and branching of slides and decks</li>
-                                    <li>E-Learning with self-assessment questionnaires</li>
-                                    <li>Content recommendation and personalization</li>
-                                </ul>
+                                For more details, see <a href="http://slidewiki.eu">SlideWiki EU project website</a>
+
                             </p>
+                            <h3> The final-release functionality of the SlideWiki platform will include, but not limited to: </h3>
+                            <ul>
+                                <li>WYSIWYG slide authoring</li>
+                                <li>Logical slide and deck representation</li>
+                                <li>LaTeX/MathML integration</li>
+                                <li>Multilingual decks / semi-automatic translation / synchronization of content between translated versions</li>
+                                <li>Import/export of the content into a number of formats (PDF, SCORM, ePUB, PowerPoint, OpenOffice and others)</li>
+                                <li>Dynamic CSS themability and transitions</li>
+                                <li>Support of social networking activities</li>
+                                <li>Full revisioning and branching of slides and decks</li>
+                                <li>E-Learning with self-assessment questionnaires</li>
+                                <li>Content recommendation and personalization</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
