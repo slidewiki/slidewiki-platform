29bb0c84c50b4793b5f1d83c964d45c5a91910e5
kadevgraaf <kadegraaf@gmail.com>
2016-09-27 17:19:59 +0200

    Update deck link on home page

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -30,7 +30,7 @@ class Home extends React.Component {
                             <div>
                                 Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
                                 <div className="ui divided list animated ">
-                                    <a className="item" href="/deck/442">Try Sample Deck Imported from SlideWiki.org &raquo;</a>
+                                    <a className="item" href="/deck/478">Try Sample Deck Imported from SlideWiki.org &raquo;</a>
                                     <a className="item" href="/deck/372">See documentation deck&raquo;</a>
                                 </div>
                             </div>
