470f806492f064d0dd89756d38ef6fb23f5d53da
abijames <a.james@soton.ac.uk>
2016-10-04 10:07:32 +0100

    update layout home page

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,32 +1,45 @@
 import React from 'react';
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div ref="home">
-                <div className="ui grid">
+render() {
+return (
+<div ref="home">
+    <div className="ui grid">
+        <div className="row">
+            <div className="column padding-reset">
+                <div className="ui huge message centered grid">
                     <div className="row">
-                        <div className="column padding-reset">
-                            <div className="ui huge message centered grid">
-                                <div className="row">
-                                  <img className="logo" src="/assets/images/logo_full.png" />
-                                </div>
-                                <div className="row">
-                                  <p>SlideWiki revolutionises how educational material is authored, shared and used.</p>
-                                </div>
+                        <img className="logo" src="/assets/images/logo_full.png" />
+                    </div>
+                    <div className="row">
+                        <p>SlideWiki revolutionises how educational material is authored, shared and used.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="ui hidden divider"></div>
+    <div className="ui container stackable two columm grid">
+        <div className="six wide column">
+            <h2 className="ui header">Features</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+        </div>
+        <div className="ten wide column">
+            <div className="ui segment" style={heightStyle}>
+                <div className="ui two column grid container">
+                    <div className="column">
+                        <div className="content">
+                            <h3 className="ui header">{deckTitle}</h3>
+                            <div className="meta">Creator: {deckCreator}</div>
+                            <div className="meta">Date: {deckDate}</div>
+                            <div className="description">
+                                <p></p>
+                                <p>{deckDescription}</p>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="ui hidden divider"></div>
-                <div className="ui container grid stackable">
-                    <div className="three column row">
-                        <div className="column">
-                          <h2 className="ui header">Features</h2>
-                          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                        </div>
-                        <div className="column">
-                            <h2 className="ui header">Presentations</h2>
+                    <div className="column">
+                      <h2 className="ui header">Presentations</h2>
                             <div>
                                 Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
                                 <div className="ui divided list animated ">
@@ -34,17 +47,16 @@ class Home extends React.Component {
                                     <a className="item" href="/deck/5">Deck created from scratch &raquo;</a>
                                     <a className="item" href="/deck/372">TODO:See documentation deck&raquo;</a>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="column">
-                          <h2 className="ui header">User Activities</h2>
-                          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                        </div>
+</div>
+
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>    
+    </div>
+</div>
+);
+}
 }
 
 export default Home;
