44b7d35012f964a437f9696be3d3a9d31b6b225a
dpaun <dpaunovic@gmail.com>
2016-10-21 09:25:37 +0200

    SWIK-283 Moved reCaptcha key to general config

--- configs/general.js
+++ configs/general.js
@@ -6,5 +6,7 @@ export default {
     //salt for password hashing
     hashSalt: ['6cee6c6a420e0573d1a4ad8ecb44f2113d010a0c3aadd3c1251b9aa1406ba6a3'],
     //API Key
-    resetPasswordAPIKey: '2cbc621f86e97189239ee8c4c80b10b3a935b8a9f5db3def7b6a3ae7c4b75cb5'
+    resetPasswordAPIKey: '2cbc621f86e97189239ee8c4c80b10b3a935b8a9f5db3def7b6a3ae7c4b75cb5',
+    //Public reCAPTCHA key
+    publicRecaptchaKey: '6LdNLyYTAAAAAINDsVZRKG_E3l3Dvpp5sKboR1ET'
 };
