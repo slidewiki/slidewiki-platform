b5b8fff24ff07b8699923bde2aa58bafc686111e
Roy Meissner <meissner@informatik.uni-leipzig.de>
2016-10-25 15:01:11 +0200

    Added missing file

---
+++ actions/user/userprofile/chooseAction.js
@@ -0,0 +1,29 @@
+import async from 'async';
+import fetchUser from './fetchUser';
+import { fetchUserDecks } from './fetchUserDecks';
+import notFoundError from '../../error/notFoundError';
+
+export default function chooseAction(context, payload, done) {
+    async.series([
+        (callback) => {
+            context.executeAction(fetchUser, payload, callback);
+        },
+        (callback) => {
+            switch (payload.params.category) {
+                case 'settings':
+                    context.dispatch('USER_CATEGORY', {category: payload.params.category, item: payload.params.item});
+                    callback();
+                    break;
+                case undefined:
+                    context.executeAction(fetchUserDecks, {params: {username: payload.params.username}}, callback);
+                    break;
+                default:
+                    context.executeAction(notFoundError, {}, callback);
+            }
+        }
+    ],
+    (err, result) => {
+        if(err) console.log(err);
+        done();
+    });
+}
