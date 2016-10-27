032c72107da184c986d77c3be6722947cf453db6
Roy Meissner <meissner@informatik.uni-leipzig.de>
2016-10-27 15:53:55 +0200

    Added null check and revised the code

--- components/User/UserProfile/PopularDecks.js
+++ components/User/UserProfile/PopularDecks.js
@@ -2,40 +2,35 @@ import React from 'react';
 import DeckCard from './DeckCard';
 import { connectToStores } from 'fluxible-addons-react';
 import UserProfileStore from '../../../stores/UserProfileStore';
+import { isEmpty } from './../../../common';
 
 class PublicUserDecks extends React.Component {
 
     render() {
-        let content = this.props.decks;
-        switch(this.props.sort){
-            case '1':
-                content = content.sort((a,b) => new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime());
-                break;
-            case '2':
-                content = content.sort((a,b) => new Date(b.updated).getTime() - new Date(a.updated).getTime());
-                break;
-            case '0':
-            default:
-                content = content.sort((a,b) => (b.title.toUpperCase() < a.title.toUpperCase()) ? 1 : -1 );
-        }
         let size = 0;
-        if (content !== undefined && content.length !== 0 && this.props.size === 0)
-            size = content.length;
-        else if (content !== undefined && content.length !== 0 && this.props.size !== 0)
-            size = content.length < 3 ? content.length : this.props.size;
-        let divs = '';
-        if(size > 0) {
-            divs = (<div className="ui three doubling cards">
+        let content = this.props.decks;
+        if(!isEmpty(content)){
+            switch(this.props.sort){
+                case '1':
+                    content = content.sort((a,b) => new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime());
+                    break;
+                case '2':
+                    content = content.sort((a,b) => new Date(b.updated).getTime() - new Date(a.updated).getTime());
+                    break;
+                case '0':
+                default:
+                    content = content.sort((a,b) => (b.title.toUpperCase() < a.title.toUpperCase()) ? 1 : -1 );
+            }
+            if (this.props.size === 0)
+                size = content.length;
+            else
+                size = content.length < 3 ? content.length : this.props.size;
+            return (<div className="ui three doubling cards">
                 {[...Array(size).keys()].map( (i) => <DeckCard userid={this.props.UserProfileStore.userid} key={i} cardContent={content[i]}/>)}
-            </div>);
+                    </div>);
         } else {
-            divs = <h3>No decks available</h3>;
+            return <h3>No decks available</h3>;
         }
-        return (
-            <div>
-                {divs}
-            </div>
-        );
     }
 }
 
