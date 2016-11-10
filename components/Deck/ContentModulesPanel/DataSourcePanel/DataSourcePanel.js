42726d4782074b606f8da3c900e79b225f9ce5e7
dpaun <dpaunovic@gmail.com>
2016-11-10 12:22:10 +0100

    SWIK-469 Added 'show more' link, removed custom scrollbar

--- components/Deck/ContentModulesPanel/DataSourcePanel/DataSourcePanel.js
+++ components/Deck/ContentModulesPanel/DataSourcePanel/DataSourcePanel.js
@@ -6,15 +6,23 @@ import DataSourceList from './DataSourceList';
 import EditDataSource from './EditDataSource';
 import ShadowScrollbars from './Scrollbars/ShadowScrollbars';
 import newDataSource from '../../../../actions/datasource/newDataSource';
+import showMoreDataSources from '../../../../actions/datasource/showMoreDataSources';
 
 class DataSourcePanel extends React.Component {
     handleNewDataSource() {
-        this.context.executeAction(newDataSource, {
-        });
+        this.context.executeAction(newDataSource);
+    }
+
+    handleShowMore(e) {
+        e.preventDefault();
+        this.context.executeAction(showMoreDataSources);
     }
 
     render() {
         const dataSources = this.props.DataSourceStore.dataSources;
+        const arrayOfDataSourcesIsLarge = dataSources.length > 10;
+        const showAllDataSources = this.props.DataSourceStore.showAllDataSources;
+        const displayDataSources = (arrayOfDataSourcesIsLarge && !showAllDataSources) ? dataSources.slice(0, 9) : dataSources;
         const dataSource = this.props.DataSourceStore.dataSource;
         const selector = this.props.DataSourceStore.selector;
         const userId = this.props.UserProfileStore.userid;
@@ -23,19 +31,22 @@ class DataSourcePanel extends React.Component {
 
         let newDataSourceButton = (editable) ?
             <button tabIndex="0" onClick={this.handleNewDataSource.bind(this)} className="ui blue labeled icon button">
-                <i className="icon edit"></i> Add Data Source
+                <i className="icon plus"></i> Add source
             </button>
             : '';
 
         let sourcesHeader = <h3 className="ui dividing header">Sources</h3>;
 
+        let showMoreLink = (!showAllDataSources && arrayOfDataSourcesIsLarge) ? <div><br/><a href="#" onClick={this.handleShowMore.bind(this)} >Show more ...</a></div> : '';
         let sourcesList = (dataSources.length === 0)
             ?
             <div>There are currently no sources for this {this.props.DataSourceStore.selector.stype}.</div>
             :
-            <ShadowScrollbars style={{height:300}} >
-                <DataSourceList items={dataSources} editable ={editable} selector={selector}/>
-            </ShadowScrollbars>;
+            <div>
+                <DataSourceList items={displayDataSources} editable ={editable} selector={selector}/>
+                {showMoreLink}
+            </div>
+            ;
 
         let editForm = <EditDataSource dataSource={dataSource}/>;
 
