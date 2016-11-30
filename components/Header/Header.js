39c42ac3e60e207a2b97b9ea993b75a7783eeb53
Roy Meissner <meissner@informatik.uni-leipzig.de>
2016-11-30 16:56:29 +0100

    Added Media Queries and first version of a sidebar

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,29 +4,60 @@ import SearchBox  from '../Search/AutocompleteComponents/HeaderSearchBox';
 //import UserNotificationsBadge from '../User/UserNotificationsPanel/UserNotificationsBadge';
 import LoginModal from '../Login/LoginModal.js';
 import AddDeckPanel from '../AddDeck/AddDeckPanel.js';
+let MediaQuery = require ('react-responsive');
 
 class Header extends React.Component {
+
+    toggleSidebar() {
+        $(this.refs.menubar)
+          .sidebar('setting', 'transition', 'overlay')
+          .sidebar('setting', 'mobileTransition', 'overlay')
+          .sidebar('toggle');
+    }
+
     render() {
 
         return (
-            <div className="ui inverted blue menu" ref="header">
-                <div className="ui container">
+            <div>
+              <MediaQuery query='(min-device-width: 768px)'>
+                <div className="ui inverted blue menu" ref="header">
+                    <div className="ui container">
+                        <a className="item" href='/'>
+                            <img className="logo" src="/assets/images/slidewiki-square-notext-glow.svg" alt=""/>
+                            &nbsp; SlideWiki
+                        </a>
+                        <div className="item">
+                            <SearchBox className="item"/>
+                        </div>
+                        <div className="ui right inverted blue menu">
+                            <NavLink className="item" routeName="addDeck" activeClass="active">
+                                <AddDeckPanel />
+                            </NavLink>
+                            {/*<UserNotificationsBadge className="ui item"/>*/}
+                            <div className="item"><LoginModal/></div>
+                        </div>
+
+                    </div>
+                </div>
+              </MediaQuery>
+              <MediaQuery query='(max-device-width: 767px)'>
+                <div className="ui inverted blue menu" ref="header">
+                  <button className="ui icon button item" onClick={this.toggleSidebar.bind(this)}><i className="content icon"/></button>
+                </div>
+                <div className="ui left dimmed sidebar vertical menu" ref="menubar">
                     <a className="item" href='/'>
-                        <img className="logo" src="/assets/images/slidewiki-square-notext-glow.svg" alt=""/>
-                        &nbsp; SlideWiki
+                        <i className="home icon"/> Homepage
                     </a>
                     <div className="item">
                         <SearchBox className="item"/>
                     </div>
-                    <div className="right menu">
-                        <NavLink className="ui item" routeName="addDeck" activeClass="active">
-                            <AddDeckPanel />
-                        </NavLink>
-                        {/*<UserNotificationsBadge className="ui item"/>*/}
-                        <LoginModal className="ui item"/>
-                    </div>
-
+                    <NavLink className="item" routeName="addDeck">
+                        <AddDeckPanel />
+                    </NavLink>
+                    {/*<UserNotificationsBadge className="ui item"/>*/}
+                    <div className="item"><LoginModal/></div>
                 </div>
+              </MediaQuery>
             </div>
         );
     }
