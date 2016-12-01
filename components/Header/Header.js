0433711ccacb73b1920948c0465501133f57419d
svalero <svalero@dsic.upv.es>
2016-12-01 16:27:27 +0100

    SWIK-811 Duplicated header menu. DONT MERGE

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,11 +4,13 @@ import SearchBox  from '../Search/AutocompleteComponents/HeaderSearchBox';
 //import UserNotificationsBadge from '../User/UserNotificationsPanel/UserNotificationsBadge';
 import LoginModal from '../Login/LoginModal.js';
 import AddDeckPanel from '../AddDeck/AddDeckPanel.js';
+import {Input, Menu, Image, Container} from 'semantic-ui-react';
 
 class Header extends React.Component {
     render() {
 
         return (
+          <div>
             <div className="ui inverted blue menu" ref="header">
                 <div className="ui container">
                     <a className="item" href='/'>
@@ -28,6 +30,30 @@ class Header extends React.Component {
 
                 </div>
             </div>
+
+            <Menu inverted="true"  color="blue" secondary>
+               <Container>
+                 <Menu.Item href="/" >
+                    <Image src="/assets/images/slidewiki-square-notext-glow.svg" size="mini" alt=""/>
+                    &nbsp; SlideWiki
+                 </Menu.Item>
+                 <Menu.Item>
+                    <SearchBox className="item"/>
+                 </Menu.Item>
+                 <Menu.Menu position="right">
+                    <Menu.Item>
+                      <NavLink routeName="addDeck" activeClass="active">
+                          <AddDeckPanel />
+                      </NavLink>
+                    </Menu.Item>
+                    <Menu.Item>
+
+                        <LoginModal />
+                    </Menu.Item>
+                 </Menu.Menu>
+              </Container>
+            </Menu>
+          </div>
         );
     }
 }
