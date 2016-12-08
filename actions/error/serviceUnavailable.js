46018bf2955ec3d7a9303187989293bcbf33198c
Vinay Modi <info@vinaymodi.com>
2016-12-08 18:35:02 +0100

    service unavailable issue fix. Added store and made provision in Deck.js component

--- actions/error/serviceUnavailable.js
+++ actions/error/serviceUnavailable.js
@@ -1,11 +1,14 @@
 import ErrorStore from '../../stores/ErrorStore';
 import { ErrorsList } from '../../components/Error/util/ErrorDescriptionUtil';
 const fumble = require('fumble');
+import { logger, breadcrumb} from '../../configs/log';
 
 export default function serviceUnavailable(context, payload, done) {
     const error = fumble.http.serviceUnavailable();
     ErrorsList.SERVICE_UNAVAILABLE.statusCode = error.statusCode;
     ErrorsList.SERVICE_UNAVAILABLE.statusText = error.message;
+    ErrorsList.SERVICE_UNAVAILABLE.breadcrumb = breadcrumb(context.stack);
+    logger.error({reqId: payload.navigate.reqId, err:ErrorsList.SERVICE_UNAVAILABLE});
     context.dispatch('SERVICE_UNAVAILABLE', ErrorsList.SERVICE_UNAVAILABLE);
     done(error);
 }
