c72a3db0eeb11c42634e7b7dc7901e742c3131cd
Stavros Maroulis <stavrosmrl@gmail.com>
2017-01-19 12:08:26 +0200

    Hide revert button for anonymous users

--- components/Deck/ContentModulesPanel/ContentHistoryPanel/ContentHistoryPanel.js
+++ components/Deck/ContentModulesPanel/ContentHistoryPanel/ContentHistoryPanel.js
@@ -2,22 +2,24 @@ import React from 'react';
 import {connectToStores} from 'fluxible-addons-react';
 import ContentHistoryStore from '../../../../stores/ContentHistoryStore';
 import ContentHistoryList from './ContentHistoryList';
+import UserProfileStore from '../../../../stores/UserProfileStore';
 
 class ContentHistoryPanel extends React.Component {
     render() {
         return (
             <div ref="contentHistoryPanel" className="ui">
                 <div>
-                    <ContentHistoryList revisions={this.props.ContentHistoryStore.history} selector={this.props.ContentHistoryStore.selector}/>
+                    <ContentHistoryList revisions={this.props.ContentHistoryStore.history} userid={this.props.UserProfileStore.userid} selector={this.props.ContentHistoryStore.selector}/>
                 </div>
             </div>
         );
     }
 }
 
-ContentHistoryPanel = connectToStores(ContentHistoryPanel, [ContentHistoryStore], (context, props) => {
+ContentHistoryPanel = connectToStores(ContentHistoryPanel, [ContentHistoryStore, UserProfileStore], (context, props) => {
     return {
-        ContentHistoryStore: context.getStore(ContentHistoryStore).getState()
+        ContentHistoryStore: context.getStore(ContentHistoryStore).getState(),
+        UserProfileStore: context.getStore(UserProfileStore).getState()
     };
 });
 export default ContentHistoryPanel;
