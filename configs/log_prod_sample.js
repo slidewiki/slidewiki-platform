b70e08ceadc69cba9905985898a2ef63f40e2692
Vinay Modi <info@vinaymodi.com>
2017-01-23 17:34:45 +0100

    Set log levels as per RFC5424 and provide production log configuration

--- configs/log_prod_sample.js
+++ configs/log_prod_sample.js
@@ -8,21 +8,12 @@ if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir);
 }
 
-// winston.setLevels(winston.config.npm.levels);
 winston.setLevels(winston.config.syslog.levels);
 winston.addColors(winston.config.npm.colors);
 
 if (!obj.log) {
     obj.log = new (winston.Logger)({
         transports: [
-            // Console transport
-            new (winston.transports.Console)({
-                timestamp: true,
-                colorize: true,
-                level: 'debug',
-                handleExceptions: true,
-                humanReadableUnhandledException: true
-            }),
             // file transport
             new (require('winston-daily-rotate-file'))({
                 label: 'slidewiki-platform',
@@ -30,7 +21,7 @@ if (!obj.log) {
                 timestamp: true,
                 datePattern: 'yyyy-MM-dd',
                 prepend: true,
-                level: 'debug',
+                level: 'warning',
                 handleExceptions: true,
                 humanReadableUnhandledException: true,
                 maxsize: 1024 * 1024 * 10, // in bytes, 10 MB
