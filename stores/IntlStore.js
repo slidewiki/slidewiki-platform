319cf797c1b1be3cf8d345fa1574bec59c4a540d
DaryaTarasowa <darya.tarasowa@gmail.com>
2017-02-07 18:30:01 +0100

    initial architecture

---
+++ stores/IntlStore.js
@@ -0,0 +1,60 @@
+import { BaseStore } from 'fluxible/addons';
+import co from '../common';
+
+class IntlStore extends BaseStore {
+
+    constructor(dispatcher) {
+        super(dispatcher);
+        this.messages = {};
+        this.locales = [];
+        this.currentLocale = null;
+    }
+
+    handleLoad({ messages, locales }) {
+        this.messages = messages;
+        this.locales = locales;
+        this.currentLocale = locales[0];
+        this.emitChange();
+    }
+
+    getMessages() {
+        return this.messages;
+    }
+
+    getMessage(path) {
+        return co.getIntlMessage(this.messages, path);
+    }
+
+    getLocales() {
+        return this.locales;
+    }
+
+    getCurrentLocale() {
+        return this.currentLocale;
+    }
+
+    dehydrate() {
+        return {
+            currentLocale: this.currentLocale,
+            locales: this.locales,
+            messages: this.messages
+        };
+    }
+
+    rehydrate({ messages, locales, currentLocale }) {
+        this.messages = messages;
+        this.locales = locales;
+        this.currentLocale = currentLocale;
+    }
+}
+
+// This action is dispatched only on the server
+
+IntlStore.storeName = 'IntlStore';
+if (!process.env.BROWSER) {
+    IntlStore.handlers = {'LOAD_INTL_SERVER' : 'handleLoad'};
+}else{
+    IntlStore.handlers = {};
+}
+
+export default IntlStore;
