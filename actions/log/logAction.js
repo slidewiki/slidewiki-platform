a9621098da20a08e3ff17594b38f03705c687928
Vinay Modi <info@vinaymodi.com>
2017-02-17 15:03:19 +0100

    Removed logAction.js. Not needed

--- actions/log/logAction.js
+++
@@ -1,6 +0,0 @@
-
-export default function loadCustomlog(context, payload, done) {
-    context.service.update('log.error', {reqId: payload.navigate.reqId, navStack: context.stack}, (err, res) => {
-        done();
-    });
-}
