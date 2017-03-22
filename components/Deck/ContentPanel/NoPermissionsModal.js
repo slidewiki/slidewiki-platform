1142ab16d18affc1cd7bb15e33da871386ba6f77
Stavros Maroulis <stavrosmrl@gmail.com>
2017-03-22 12:09:13 +0200

    Use react-semantic-ui for the no permissions modal

--- components/Deck/ContentPanel/NoPermissionsModal.js
+++ components/Deck/ContentPanel/NoPermissionsModal.js
@@ -1,60 +1,52 @@
 import React from 'react';
 import {connectToStores} from 'fluxible-addons-react';
 import PermissionsStore from '../../../stores/PermissionsStore';
-import classNames from 'classnames';
 import forkDeck from '../../../actions/decktree/forkDeck';
-import Portal from 'react-portal';
 import ContentUtil from './util/ContentUtil';
 import {navigateAction} from 'fluxible-router';
+import {Button, Icon, Modal, Header} from 'semantic-ui-react';
+import FocusTrap from 'focus-trap-react';
 
 class NoPermissionsModal extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { modalHeight: 0 };
     }
 
-    componentDidMount() {
-        let modalHeight = window.$('#noPermissionsModal').outerHeight();
-        this.setState({modalHeight: modalHeight});
-    }
-
-    handleForkClick(e) {
+    handleFork() {
         this.context.executeAction(forkDeck, {deckId: this.props.selector.id});
     }
 
-    handleCloseClick(e) {
+    handleClose() {
         this.context.executeAction(navigateAction, {
             url: ContentUtil.makeNodeURL(this.props.selector, 'view')
         });
     }
 
     render() {
-        let modalStyle = {
-            'margin-top': - this.state.modalHeight / 2
-        };
-
+        let isModalShown = this.props.PermissionsStore.isNoPermissionsModalShown;
         return (
-            <Portal isOpened={this.props.PermissionsStore.isNoPermissionsModalShown}>
-                <div className="ui dimmer modals visible active page transition">
-                    <div className="ui modal small transition visible active" style={modalStyle} id="noPermissionsModal">
-                        <i className="close icon" tabIndex="0" onClick={this.handleCloseClick.bind(this)}></i>
-                        <div className="header"><i className="warning sign icon"></i> No Edit Rights</div>
-                        <div className="content">
-                            <p>
-                                You can only view this deck. To make changes, you may ask the owner to grant you edit rights or fork the deck.</p>
-                        </div>
-                        <div className="actions">
-                            <button className="ui button disabled"><i className="edit icon"/>Request edit access</button>
-                            <button className="ui button" tabIndex="0" onClick={this.handleForkClick.bind(this)}><i className="fork icon"/>Fork
-                                this deck
-                            </button>
-                            <button className="ui button" tabIndex="0" onClick={this.handleCloseClick.bind(this)}><i className="remove icon"/>Close
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </Portal>
+            <Modal dimmer="blurring" size='small' open={isModalShown}
+                   onClose={this.handleClose.bind(this)}>
+                <Header icon='warning sign' content='No Edit Rights'/>
+                <Modal.Content>
+                    <p>You can only view this deck. To make changes, you may ask the owner to grant you edit rights
+                        or fork the deck.</p>
+                </Modal.Content>
+                <Modal.Actions>
+                    <FocusTrap focusTrapOptions={{clickOutsideDeactivates: true}} active={isModalShown}>
+                        <Button disabled>
+                            <Icon name='edit'/> Request edit access
+                        </Button>
+                        <Button onClick={this.handleFork.bind(this)} tabIndex="0">
+                            <Icon name='fork'/> Fork this deck
+                        </Button>
+                        <Button onClick={this.handleClose.bind(this)} tabIndex="0">
+                            <Icon name='close'/> Close
+                        </Button>
+                    </FocusTrap>
+                </Modal.Actions>
+            </Modal>
         );
     }
 }
