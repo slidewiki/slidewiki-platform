9c3ae70d466c8ab124dd308d05c8c173007b9740
Todor Tsankov <tsankov.todya@gmail.com>
2017-03-23 11:38:41 +0100

    handle complex object nesting

--- components/Deck/ContentModulesPanel/ContentDiffviewPanel/ContentDiffviewPanel.js
+++ components/Deck/ContentModulesPanel/ContentDiffviewPanel/ContentDiffviewPanel.js
@@ -24,7 +24,7 @@ export default class ContentDiffviewPanel extends Component {
         let src = '<div name="inlineContent" id="inlineContent" style="min-width: 100%; min-height: 450px; overflow: auto;"><h1 id="h1-1">STatic text part 1</h1><p id="p-1">THEN text</p><p id="p-2">Static</p><p id="p-3"><span id="sp-1" style="color:#e74c3c;">Color in red</span></p><p id="p-4">To delete</p><h2 id="h2-1">Title 1</h2></div>';
         src = src.replace(/&nbsp;/g, ' ');
 
-        let src2 = '<div name="inlineContent" id="inlineContent" style="min-width: 100%; min-height: 450px; overflow: auto;"><h1 id="h1-1">STatic text part 1</h1><p id="p-1">NOW text</p><p id="p-2">Static</p><p id="p-3"><span id="sp-1" style="color:#f1c40f;">Color in yellow</span></p><h2 id="h2-1">Title 1</h2><h1 id="h1-2">NEWEST TITLE</h1></div>';
+        let src2 = '<div name="inlineContent" id="inlineContent" style="min-width: 100%; min-height: 450px; overflow: auto;"><h1 id="h1-1">STatic text part 1</h1><p id="p-1">NOW text</p><p id="p-2">Static</p><p id="p-3"><span id="sp-1" style="color:#f1c40f;">Color in yellow</span></p><h2 id="h2-1">Title 1</h2><p id="p-5">New paragraph</p><h1 id="h1-2">NEWEST TITLE</h1></div>';
         src2 = src2.replace(/&nbsp;/g, ' ');
 
 
@@ -35,10 +35,9 @@ export default class ContentDiffviewPanel extends Component {
 
         //DIFF 2 vTrees
         const diff_results = diff(vTree, vTree2);
-        const elements = Object.keys(diff_results).map((e) => {
-            return diff_results[e];
-        });
-        // console.log(elements);
+
+        //Reduce complex Object into flat Array
+        const elements = Object.keys(diff_results).reduce((arr, key) => arr.concat(diff_results[key]), []);
 
         //LOG
         // console.group();
