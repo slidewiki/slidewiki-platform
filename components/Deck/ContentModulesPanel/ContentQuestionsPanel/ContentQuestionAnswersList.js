61904ec4b48b6bb13b5d1e2d31e835a902bf385f
Ainuddin Faizan <andyfaizan@gmail.com>
2017-03-28 20:51:37 +0200

    Connected correct answers column to state

--- components/Deck/ContentModulesPanel/ContentQuestionsPanel/ContentQuestionAnswersList.js
+++ components/Deck/ContentModulesPanel/ContentQuestionsPanel/ContentQuestionAnswersList.js
@@ -5,14 +5,52 @@ class ContentQuestionAnswersList extends React.Component {
     render() {
         let list = this.props.items.map((node, index) => {
             return (
-                <ContentQuestionAnswersItem answer={node} key={index}/>
+                <ContentQuestionAnswersItem answer={node} name={'answer' + index} key={index}/>
             );
         });
+
+        let correctAnswers = this.props.items.filter((item) => item.correct).map((node, index) => {
+            return (
+              <div key={index}>
+                <a className="header">
+                  {node.answer}
+                </a>
+                <div className="description">
+                  <p>
+                    {node.explanation}
+                  </p>
+                </div>
+              </div>
+            );
+        });
+
+        let answers = (
+          <div className="ui two column stackable grid">
+                <div className="column">
+                  <div className="ui grouped fields">
+                    <fieldset>
+                      {list}
+                    </fieldset>
+                  </div>
+                </div>
+                <div className="column">
+                  <button className="ui compact button primary">
+                    <i className=" help circle icon" />
+                    Show answer
+                  </button>
+                  <div className="ui item">
+                    <div className="content">
+                      {correctAnswers}
+                    </div>
+                  </div>
+                </div>
+              </div>
+        );
 
         return (
             <div ref="contentquestionanswersList">
                 <div className="ui relaxed list">
-                    {list}
+                    {answers}
                 </div>
              </div>
         );
