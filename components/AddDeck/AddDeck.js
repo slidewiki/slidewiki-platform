b3a4cb42660a6416874c6d6c982d95a4b7d367aa
Kurt Junghanns <tboonx@googlemail.com>
2017-03-29 12:43:46 +0200

    [Add-padding-to-user-settings] Removed redundant not working error view

--- components/AddDeck/AddDeck.js
+++ components/AddDeck/AddDeck.js
@@ -255,12 +255,6 @@ class AddDeck extends React.Component {
             <option value="CC0" >Creative Commons CC0 Public Domain</option>
         </select>;
 
-        let errorView = '';
-        if (this.props.AddDeckStore.error !== null)
-            errorView = <Error error={this.props.AddDeckStore.error} />;
-        else
-            errorView = '';
-
         let hint_title = this.props.AddDeckStore.wrongFields.title ? 'Please enter a title.' : undefined;
         let hint_language = this.props.AddDeckStore.wrongFields.language ? 'Please select a language.' : undefined;
         let hint_license = this.props.AddDeckStore.wrongFields.license ? 'Please select a license.' : undefined;
@@ -347,7 +341,6 @@ class AddDeck extends React.Component {
 
                   </form>
               </div>
-              {errorView}
             </div>
         );
     }
