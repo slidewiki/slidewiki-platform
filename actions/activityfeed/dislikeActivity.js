58c447ec83b8407e7b87a563d91dadea39016ef8
Nikola Tomasevic <Nikola Tomasevic>
2017-04-04 16:22:53 +0200

    SWIK-216: dislikeActivity - initial implementation

---
+++ actions/activityfeed/dislikeActivity.js
@@ -0,0 +1,16 @@
+const log = require('../log/clog');
+import serviceUnavailable from '../error/serviceUnavailable';
+
+export default function dislikeActivity(context, payload, done) {
+    log.info(context);
+    context.service.create('like.dislikeActivity', payload, {}, {timeout: 20 * 1000}, (err, res) => {
+        if (err) {
+            log.error(context, {filepath: __filename, err: err});
+            context.executeAction(serviceUnavailable, payload, done);
+            //context.dispatch('DISLIKE_ACTIVITY_FAILURE', err);
+        } else {
+            context.dispatch('DISLIKE_ACTIVITY_SUCCESS', res);
+        }
+        done();
+    });
+}
