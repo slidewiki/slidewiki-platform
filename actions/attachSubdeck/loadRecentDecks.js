4e8bf5981d4fe2d99d3aeca8a221210e865ec3ee
svalero <svalero@dsic.upv.es>
2017-04-05 15:52:25 +0200

    SWIK-1046-AddingSubdecks new action to load recent decks to the the from SlideWiki tab

---
+++ actions/attachSubdeck/loadRecentDecks.js
@@ -0,0 +1,27 @@
+import log from '../log/clog';
+import notFoundError from '../error/notFoundError';
+import methodNotAllowedError  from '../error/methodNotAllowedError';
+
+export default function loadRecentDecks(context,payload,done){
+    log.info(context);
+    context.service.read('deck.recent', payload, {timeout: 20 * 1000}, (err, res) => {
+        if (err) {
+            if (err.statusCode === 404) {
+                context.executeAction(notFoundError, {}, done);
+                context.dispatch('ATTACHSUBDECK_LOAD_RECENTDECKS', []);
+                return;
+            } else if (err.statusCode === 401) {
+                context.executeAction(methodNotAllowedError, {}, done);
+                return;
+            } else{
+                log.error(context, {filepath: __filename, err: err});
+                context.dispatch('ATTACHSUBDECK_LOAD_RECENTDECKS', []);
+            }
+        } else { //Normal action
+
+            context.dispatch('ATTACHSUBDECK_LOAD_RECENTDECKS', res);
+        }
+        done();
+    });
+
+}
