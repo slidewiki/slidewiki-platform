e8621e2842da9c957d94e22ac05d4c5de6062ca8
Huw Fryer <huwfryer@gmail.com>
2017-04-11 14:26:48 +0100

    SWIK-1123 move CSS require to constructor

--- components/Deck/Presentation/Presentation.js
+++ components/Deck/Presentation/Presentation.js
@@ -29,7 +29,18 @@ class Presentation extends React.Component{
         this.startingSlide = this.props.PresentationStore.selector.sid;
         this.deck = this.props.PresentationStore.selector.id;
         this.revealDiv = null;
-
+        // Load the theme stylesheet
+        let styleName = 'white';
+        if(this.props.PresentationStore.theme && typeof this.props.PresentationStore.theme !== 'undefined'){
+            styleName = this.props.PresentationStore.theme;
+        }
+        //console.log('styleName', styleName);
+        if (styleName === '' || typeof styleName === 'undefined' || styleName === 'undefined' || styleName === 'default')
+        {
+            //if none of above yield a theme:
+            styleName = 'white';
+        }
+        require('../../../custom_modules/reveal.js/css/theme/' + styleName + '.css');
     }
 
     componentDidMount(){
