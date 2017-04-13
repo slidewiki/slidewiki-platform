efb660ccce5ad5b80f9d46ee8604d55b2383e936
Kurt Junghanns <tboonx@googlemail.com>
2017-04-13 16:11:04 +0200

    [SWIK-1112---tags-currently-not-saved-in-tag-tab-on-experimental] Corrected action names. added clog.

--- actions/tags/cancelEditTag.js
+++ actions/tags/cancelEditTag.js
@@ -1,4 +1,8 @@
-export default function cancelEditDataSource(context, payload, done) {
+const log = require('../log/clog');
+
+//TODO delete if not needed
+export default function cancelEditTag(context, payload, done) {
+    log.info(context);
     context.dispatch('CANCEL_EDIT_DATASOURCE');
     done();
 }
