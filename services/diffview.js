d107ee7631b52e5169e8bee9adf45c6526e1e9b9
Todor Tsankov <tsankov.todya@gmail.com>
2017-04-21 13:01:30 +0200

    resolve merge conflict

---
+++ services/diffview.js
@@ -0,0 +1,10 @@
+import {Microservices} from '../configs/microservices';
+import rp from 'request-promise';
+import _ from 'lodash';
+
+export default {
+    name: 'diffview',
+    read: (req, resource, params, config, callback) => {
+      
+    }
+};
