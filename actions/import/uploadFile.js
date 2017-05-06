871456880ca39ee4313391de0a070e5c453c1457
dpaun <dpaunovic@gmail.com>
2017-05-06 16:50:39 +0200

    SWIK-568 Corrected a typo

--- actions/import/uploadFile.js
+++ actions/import/uploadFile.js
@@ -7,28 +7,30 @@ export default function uploadFile(context, payload, done) {
     log.info(context);
     context.dispatch('UPLOAD_STARTED', null);
 
-    //use timer in order to make a working progress bar
-    // context.myStuff = {
-    //     uploadFinished: false
-    // };
+    // use timer in order to make a working progress bar
+    context.myStuff = {
+        uploadFinished: false
+    };
     const timeout = 60;
-    // const remainingProgress = 89;
-    // const updatePeriod = 3; //3 seconds
-    // const progressPerThreeSeconds = (timeout / remainingProgress) * updatePeriod + 1;
-    // const timer = () => {
-    //     setTimeout(() => {
-    //         if (!context.myStuff.uploadFinished) {
-    //             context.dispatch('UPLOAD_MORE_PROGRESS', progressPerThreeSeconds);
-    //             timer();
-    //         }
-    //     }, updatePeriod * 1000);
-    // };
-    // timer();
+    const remainingProgress = 55;
+    let currentProgress = 0;
+    const updatePeriod = payload.base64.length / 20 / 1000 / 1024 ; // progress bar speed depends on file length
+    const progressStep = 1;
+    const timer = () => {
+        setTimeout(() => {
+            if (!context.myStuff.uploadFinished && currentProgress < remainingProgress) {
+                currentProgress += progressStep;
+                context.dispatch('UPLOAD_MORE_PROGRESS', progressStep);
+                timer();
+            }
+        }, updatePeriod * 1000);
+    };
+    timer();
 
     context.service.create('import', payload, {timeout: timeout * 1000}, {timeout: timeout * 1000}, (err, res) => {
         //console.log('action got response from server', err);
 
-        // context.myStuff.uploadFinished = true;
+        context.myStuff.uploadFinished = true;
         if (err) {
             log.error(context, {filepath: __filename, err: err});
             context.executeAction(serviceUnavailable, payload, done);
