4dc8ce3bfc1d1fe2117c72ffdf0a89e03a42bd79
dpaun <dpaunovic@gmail.com>
2017-05-15 22:21:27 +0200

    SWIK-1326 Handling userid===0

--- components/Deck/ActivityFeedPanel/ActivityItem.js
+++ components/Deck/ActivityFeedPanel/ActivityItem.js
@@ -55,6 +55,9 @@ class ActivityItem extends React.Component {
         const viewPath = ((node.content_kind === 'slide') ? '/deck/' + this.props.selector.id + '/slide/' : '/deck/') + node.content_id;
         const nodeRef = (node.content_kind === this.props.selector.stype && node.content_id.split('-')[0] === this.props.selector.sid.split('-')[0]) ? (<span> {'this ' + node.content_kind} </span>) :  (<span>{node.content_kind + ' '}<a href={this.getPath(this.props.activity)} onClick={this.handleRefClick.bind(this)}>{node.content_name}</a></span>);
 
+        if (node.user_id === '0') {
+            node.user_id = undefined;
+        }
         switch (node.activity_type) {
             case 'translate':
                 IconNode = (<i className="ui big translate icon"></i>);
