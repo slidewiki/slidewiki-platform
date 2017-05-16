1353bb340391ddc95fcfda72401c99ef020c163d
Todor Tsankov <tsankov.todya@gmail.com>
2017-05-16 16:11:33 +0200

    add DiffView service

--- services/diffview.js
+++ services/diffview.js
@@ -1,10 +1,32 @@
 import {Microservices} from '../configs/microservices';
 import rp from 'request-promise';
-import _ from 'lodash';
+const log = require('../configs/log').log;
 
 export default {
     name: 'diffview',
     read: (req, resource, params, config, callback) => {
-      
+
+        req.reqId = req.reqId ? req.reqId : -1;
+        log.info({Id: req.reqId, Service: __filename.split('/').pop(), Resource: resource, Operation: 'read', Method: req.method });
+        let args = params.params ? params.params : params;
+        let selector = {'stype': args.stype, 'sid': args.sid, 'diffid': args.did};
+
+        if (resource === 'diffview.slide') {
+
+            rp.get({uri: Microservices.deck.uri + '/' + selector.stype + '/' + selector.sid.split('-')[0]}).then((res) => {
+
+                // console.log(res);
+
+                callback(null, {
+                    history: res,
+                    selector: selector
+                });
+
+            }).catch((err) => {
+                console.log(err);
+                callback(err);
+            });
+        }
+
     }
 };
