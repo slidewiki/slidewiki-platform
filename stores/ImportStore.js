92a873dc381a3ddacd985140aaaf2cf1ed587a0e
dpaun <dpaunovic@gmail.com>
2017-05-17 16:01:50 +0200

    SWIK-568 removed console.log

--- stores/ImportStore.js
+++ stores/ImportStore.js
@@ -62,7 +62,7 @@ class ImportStore extends BaseStore {
     }
 
     storeFile(payload) {
-        console.log('ImportStore: storeFile()', payload);
+        // console.log('ImportStore: storeFile()', payload);
         this.file = payload.file;
         this.base64 = payload.base64;
         this.filename = this.file.name;
@@ -83,7 +83,7 @@ class ImportStore extends BaseStore {
         this.emitChange();
     }
     uploadSuccess(headers) {
-        console.log('ImportStore: uploadSuccess()', headers);
+        // console.log('ImportStore: uploadSuccess()', headers);
         this.isUploaded = true;
         // this.uploadProgress = 100;
         this.uploadProgress = 65;
@@ -97,7 +97,7 @@ class ImportStore extends BaseStore {
         this.emitChange();
     }
     uploadStarted() {
-        console.log('ImportStore: uploadStarted()');
+        // console.log('ImportStore: uploadStarted()');
         this.uploadProgress = 10;
         this.error = null;
         this.noOfSlides = 0;
