5ecd628fb0f597811a69a7c8e4cdf02f46497f50
Stavros Maroulis <stavrosmrl@gmail.com>
2017-06-05 10:57:50 +0300

    Add message for empty slide history

--- components/Deck/ContentModulesPanel/ContentHistoryPanel/SlideHistoryPanel.js
+++ components/Deck/ContentModulesPanel/ContentHistoryPanel/SlideHistoryPanel.js
@@ -9,11 +9,11 @@ import ContentChangeItem from './ContentChangeItem';
 class SlideHistoryPanel extends React.Component {
 
     render() {
-        const changes = this.props.SlideHistoryStore.changes ? this.props.SlideHistoryStore.changes.map((change, index) => {
+        const changes = this.props.SlideHistoryStore.changes && this.props.SlideHistoryStore.changes.length ? this.props.SlideHistoryStore.changes.map((change, index) => {
             return (
             <ContentChangeItem selector={this.props.selector} permissions={this.props.PermissionsStore.permissions} change={change} key={index}/>
             );
-        }) : '';
+        }) : 'There are no changes for this slide.';
 
         return (
         <div ref="slideHistoryPanel" className="ui">
