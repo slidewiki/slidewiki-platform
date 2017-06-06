a3c73fe8530af23da8231a77838f70b19a351994
DaryaTarasowa <darya.tarasowa@gmail.com>
2017-06-06 13:41:39 +0200

    minor

--- services/deck.js
+++ services/deck.js
@@ -203,9 +203,9 @@ export default {
                 uri: Microservices.deck.uri + '/deck/' + params.deckId + '/translate',
                 json: true,
                 body: toSend
-            }).then((deck) => {
-                console.log('DECK:' + deck);
-                callback(false, deck);
+            }).then((data) => {
+                //console.log('DECK:' + JSON.stringify(data.root_deck));
+                callback(false, data);
             })
             .catch((err) => callback(err));
         }
