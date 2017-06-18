fae856f436a119f21ce85abede03993eb068d522
kadevgraaf <kadegraaf@gmail.com>
2017-06-18 16:17:02 +0100

    destroy/unmount SlideContentEditor component on navigating to a different slide

--- components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideEditPanel.js
+++ components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideEditPanel.js
@@ -8,28 +8,57 @@ import restoreDeckPageLayout from '../../../../../actions/deckpagelayout/restore
 
 
 class SlideEditPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        //this.currentID;
+        this.currentContent;
+        this.editorcontent = '';
+        this.destroy;
+    }
     componentWillUnmount(){
         //show deckTree again
-        context.executeAction(restoreDeckPageLayout,{});
-
+        //context.executeAction(restoreDeckPageLayout,{});
     }
-
+    componentDidMount(){
+        //this.currentID = this.props.selector.sid;
+        this.currentContent = this.props.SlideEditStore.content;
+    }
+    componentDidUpdate(){
+        //if (this.currentID !== this.props.selector.sid)
+        //{
+        //    console.log('slide id changed - destroy/unmount SlideContentEditor component');
+        //    this.editorcontent = ''; //destroy/unmount SlideContentEditor component
+        //    this.destroy = true;
+            if(this.currentContent !== this.props.SlideEditStore.content)
+            {
+                //console.log('content changed');
+                this.editorcontent = ''; //destroy/unmount SlideContentEditor component
+                this.destroy = true;
+                //this.currentID = this.props.selector.sid;
+                this.currentContent = this.props.SlideEditStore.content;
+            }
+        //}
+        else {
+            this.destroy = false;
+        }
+    }
     render() {
         //handle the notifications --> in process.env.BROWSER
         let self = this;
         //-------------------------------------------------------
-        let editorcontent = '';
         // Only load WYSIWYG-Editor when the content has been loaded via loadSlideEdit.js
-        if (this.props.SlideEditStore.content !== ''){
-            editorcontent = <SlideContentEditor title={this.props.SlideEditStore.title}
-                                                content={this.props.SlideEditStore.content}
-                                                id={this.props.SlideEditStore.id}
-                                                speakernotes={this.props.SlideEditStore.speakernotes}
-                                                selector={this.props.selector} />;
+        if (this.props.SlideEditStore.content !== '' && !this.destroy){
+            this.editorcontent = <SlideContentEditor title={this.props.SlideEditStore.title}
+                                content={this.props.SlideEditStore.content}
+                                id={this.props.SlideEditStore.id}
+                                speakernotes={this.props.SlideEditStore.speakernotes}
+                                selector={this.props.selector} />;
         }
+
         return (
             <div ref="slideEditPanel" className="ui bottom attached segment">
-                {editorcontent}
+            {(this.props.SlideEditStore.content === undefined) ? <div className="ui active dimmer"><div className="ui text loader">Loading</div></div> : ''}
+            {this.editorcontent}
             </div>
         );
     }
