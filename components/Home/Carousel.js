be2ec2b3a44266fd8ee20d5b7cf8649213551300
abijames <a.james@soton.ac.uk>
2017-06-20 12:22:09 +0100

    Fix typo in header, update logo. Fix keyboard access in carousel

--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -32,31 +32,31 @@ class Carousel extends React.Component {
             <div className="ui fluid container two column grid" style={{minHeight: '200px'}}>
 
                 <div className="twelve wide column center aligned">
-                    <div className="glide">
+                    <div className="glide" tabIndex="-1">
                       <div className="glide__arrows">
-                          <button className="glide__arrow prev ui basic icon button" data-glide-dir="<"><i className="ui big icon chevron left"></i></button>
-                          <button className="glide__arrow next ui basic icon button" data-glide-dir=">"><i className="ui big icon chevron right"></i></button>
+                          <button className="glide__arrow prev ui basic icon button" data-glide-dir="<" tabIndex="-1"><i className="ui big icon chevron left"></i></button>
+                          <button className="glide__arrow next ui basic icon button" data-glide-dir=">" tabIndex="-1"><i className="ui big icon chevron right"></i></button>
                       </div>
 
                       <div className="glide__wrapper">
                           <ul className="glide__track">
-                              <li className="glide__slide">
-                                <a href="http://slidewiki.org" className="ui medium image">
+                              <li className="glide__slide" >
+                                <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                   <img src="/assets/images/carousel/slide1.png" alt="multilingual" />
                                 </a>
                               </li>
-                              <li className="glide__slide">
-                                <a href="http://slidewiki.org" className="ui medium image">
+                              <li className="glide__slide" >
+                                <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                   <img src="/assets/images/carousel/slide2.png" alt="feature2" />
                                 </a>
                               </li>
-                              <li className="glide__slide">
-                                <a href="http://slidewiki.org" className="ui medium image">
+                              <li className="glide__slide" >
+                                <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                   <img src="/assets/images/carousel/slide3.png" alt="feature3" />
                                 </a>
                               </li>
-                              <li className="glide__slide">
-                                <a href="http://slidewiki.org" className="ui medium image">
+                              <li className="glide__slide" >
+                                <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                   <img src="/assets/images/carousel/slide4.png" alt="feature4" />
                                 </a>
                               </li>
@@ -65,24 +65,25 @@ class Carousel extends React.Component {
 
                       <div className="glide__bullets"></div>
                     </div>
+                    <button onClick={this.togglePause.bind(this)} className="ui small icon button right floated">{this.state.paused ? <i className="ui play small icon"></i> : <i className="ui pause small icon"></i>}</button>
                 </div>
 
                 <div className="four wide column">
                   <div className="ui fluid vertical divided menu">
-                    <a className="item gslide-header gh1" data-glide-trigger='.glide' data-glide-dir='=1'>
+                    <a className="item gslide-header gh1" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=1'>
                       Feature 1
                     </a>
-                    <a className="item gslide-header gh2" data-glide-trigger='.glide' data-glide-dir='=2'>
+                    <a className="item gslide-header gh2" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=2'>
                       Feature 2
                     </a>
-                    <a className="item gslide-header gh3" data-glide-trigger='.glide' data-glide-dir='=3'>
+                    <a className="item gslide-header gh3" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=3'>
                       Feature 3
                     </a>
-                    <a className="item gslide-header gh4" data-glide-trigger='.glide' data-glide-dir='=4'>
+                    <a className="item gslide-header gh4" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=4'>
                       Feature 4
                     </a>
                   </div>
-                  <button onClick={this.togglePause.bind(this)} className="ui small icon button fluid">{this.state.paused ? <i className="ui play small icon"></i> : <i className="ui pause small icon"></i>}</button>
+                  
                 </div>
             </div>
 
