46db728c01c0485fb41fb2def5dbbdfcbe90c15e
abijames <a.james@soton.ac.uk>
2017-07-08 18:11:36 +0100

    UI improvements and accessibiltiy labels

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -22,73 +22,75 @@ class Home extends React.Component {
             }
         });
         return (
-        <div ref="home">
-            <div className="ui blue inverted segment">
-                <h2>SlideWiki...Create, Share and Enjoy Presentations</h2>
-                <Carousel />
-            </div>
-            <div className="ui fluid container stackable three columm grid">
-              <div className="six wide column">
-                <div className="six wide column">
-                  <div className="ui segments">
-                    <div className="ui segment top attached">
-                      <h3>Getting Involved</h3>
+            <div ref="home">
+                <div className="ui blue inverted segment" style={{borderRadius: '0px'}}>
+                    <h2>SlideWiki...Create, Share and Enjoy Presentations</h2>
+                    <Carousel />
+                </div>
+                <div className="ui fluid container">
+                    <div className="ui padded stackable grid ">
+                        <div className="one wide column"></div>
+                        <div className="four wide column">
+                            <div className="four wide column">
+                                <div className="ui segments">
+                                    <div className="ui segment top attached">
+                                        <h3>Getting Involved</h3>
+                                    </div>
+                                    <div className="ui segment">
+                                        <div className="ui divide list">
+                                            <div className="item">
+                                                <div className="content">
+                                                    <a className="header" href="">Sign up to get started</a>
+                                                    <div className="description">
+                                                        <p>Create an account to start creating and sharing your decks</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="item">
+                                                <div className="content">
+                                                    <a className="header" href="">SlideWiki foundation</a>
+                                                    <div className="description">
+                                                        <p>Find out more about SlideWiki</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="item">
+                                                <div className="content">
+                                                    <a className="header" href="">Contribute to SlideWiki's development</a>
+                                                    <div className="description">
+                                                        <p>Get involved with supporting and improving SlideWiki</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="five wide column">
+                            <div className="ui segments">
+                                <div className="ui segment top attached">
+                                    <h3><FormattedMessage id='home.featured' defaultMessage='Featured decks'/></h3>
+                                </div>
+                                <DeckList scope="featured"/>
+                            </div>
+                        </div>
+                        <div className="five wide column" style={{paddingRight: '2rem'}}>
+                            <div className="ui segments">
+                                <div className="ui segment top attached">
+                                    <h3><FormattedMessage id='home.recent' defaultMessage='Recent decks'/>
+                                    </h3>
+                                </div>
+                                <DeckList scope="recent"/>
+                                <a className="ui bottom attached fuild button" href='/recent'><FormattedMessage id='home.view_all' defaultMessage='View all'/>
+                                </a>
+                            </div>
+                            <div className="one wide column"></div>
+                        </div>
                     </div>
-                    <div className="ui segment">
-                      <div className="ui divide list">
-                          <div className="item">
-                            <div className="content">
-                              <a className="header" href="">Sign up to get started</a>
-                              <div className="description">
-                                <p>Create an account to start creating and sharing your decks</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="item">
-                            <div className="content">
-                              <a className="header" href="">SlideWiki foundation</a>
-                              <div className="description">
-                                <p>Find out more about SlideWiki</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="item">
-                            <div className="content">
-                              <a className="header" href="">Contribute to SlideWiki's development</a>
-                              <div className="description">
-                                <p>Get involved with supporting and improving SlideWiki</p>
-                              </div>
-                            </div>
-                          </div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              </div>
-
-              <div className="five wide column">
-                <div className="ui segments">
-                  <div className="ui segment top attached">
-                    <h3><FormattedMessage id='home.featured' defaultMessage='Featured decks'/></h3>
-                  </div>
-                  <DeckList scope="featured"/>
-                </div>
-              </div>
-
-
-              <div className="five wide column">
-                <div className="ui segments">
-                  <div className="ui segment top attached">
-                    <h3><FormattedMessage id='home.recent' defaultMessage='Recent decks'/></h3>
-                  </div>
-                  <DeckList scope="recent"/>
-                  <a className="ui bottom attached fuild button" href='/recent'><FormattedMessage id='home.view_all' defaultMessage='View all'/></a>
-
-                </div>
-              </div>
             </div>
-
-        </div>
         );
     }
 }
