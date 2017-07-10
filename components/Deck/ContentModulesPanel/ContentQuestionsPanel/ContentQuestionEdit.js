07809692e99f265323202531e88136f1af8c68a4
Vinay Modi <vinay.modi@gmail.com>
2017-07-10 16:48:05 +0200

    Handle the case when not all 4 answer choices are available. Fix textarea value warning

--- components/Deck/ContentModulesPanel/ContentQuestionsPanel/ContentQuestionEdit.js
+++ components/Deck/ContentModulesPanel/ContentQuestionsPanel/ContentQuestionEdit.js
@@ -10,19 +10,18 @@ class ContentQuestionEdit extends React.Component {
         const title = this.props.question.title;
         const difficulty = this.props.question.difficulty;
         const answers = this.props.question.answers;
+        const numAnswers = answers.length;
         const answerChoiceWidth = {
             width: '680px',
         };
-
+        console.log(answers);
         return (
             <div className="ui bottom attached" data-reactid="637">
                 <div className="ui padded segment">
                     <form className="ui form">
                         <div className="two fields">
                             <div className="required field"><label htmlFor="question">Question</label>
-                                <textarea rows="3"  name="question" id="question" aria-required="true">
-                                    {title}
-                                </textarea>
+                                <textarea rows="3"  name="question" id="question" aria-required="true" value={title} />
                             </div>
                             <div className="ui grouped fields">
                                 <fieldset>
@@ -74,35 +73,35 @@ class ContentQuestionEdit extends React.Component {
                                 </div>
                                 <div className="inline field">
                                     <div className="ui checkbox">
-                                        {answers[1].correct ?
+                                        {numAnswers > 1 && answers[1].correct ?
                                             <input  type="checkbox" name="example2" id="answer2" tabIndex="0" className="hidden" checked="checked"/> :
                                             <input  type="checkbox" name="example2" id="answer2" tabIndex="0" className="hidden" />
                                         }
                                         <label htmlFor="answer2"></label>
                                     </div>
-                                    <input style={answerChoiceWidth} type="text" name="response2" id="response2" value={answers[1].answer}/>
+                                    <input style={answerChoiceWidth} type="text" name="response2" id="response2" value={numAnswers > 1 ? answers[1].answer: ''}/>
                                     <label htmlFor="response2"></label>
                                 </div>
                                 <div className="inline field">
                                     <div className="ui checkbox">
-                                        {answers[2].correct ?
+                                        {numAnswers > 2 && answers[2].correct ?
                                             <input type="checkbox" name="example3" id="answer3" tabIndex="0" className="hidden" checked="checked"/> :
                                             <input type="checkbox" name="example3" id="answer3" tabIndex="0" className="hidden" />
                                         }
                                         <label htmlFor="answer3"></label>
                                     </div>
-                                    <input type="text" style={answerChoiceWidth} name="response4" id="response4" value={answers[2].answer}/>
+                                    <input type="text" style={answerChoiceWidth} name="response4" id="response4" value={numAnswers > 2 ? answers[2].answer: ''}/>
                                     <label htmlFor="response3"></label>
                                 </div>
                                 <div className="inline field">
                                     <div className="ui checkbox">
-                                        {answers[3].correct ?
+                                        {numAnswers > 3 && answers[3].correct ?
                                             <input  type="checkbox" name="example4" id="answer4" tabIndex="0" className="hidden" checked="checked"/> :
                                             <input  type="checkbox" name="example4" id="answer4" tabIndex="0" className="hidden" />
                                         }
                                         <label htmlFor="answer4"></label>
                                     </div>
-                                    <input type="text" style={answerChoiceWidth} name="response4" id="response4" value={answers[3].answer}/>
+                                    <input type="text" style={answerChoiceWidth} name="response4" id="response4" value={numAnswers > 3 ? answers[3].answer: ''}/>
                                     <label htmlFor="response4"></label>
                                 </div>
                             </fieldset>
