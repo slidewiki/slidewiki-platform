44ad365109f444e1b6b03f2bffa9089029b33bfd
Ali <hyperir@gmail.com>
2017-07-11 16:11:13 +0200

    fix the issue with play button, decreased the size of images

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -24,10 +24,10 @@ class Footer extends React.Component {
                                 <NavLink className="item" routeName="dataprotection" href="/dataprotection">Data Protection Conditions</NavLink>
                             </div>
                         </div>
-                        <div className="five wide column">
+                        <div className="seven wide column">
                             <div className="ui inverted  list">
                                 <div className="item">Copyright &copy; 2017 &middot; All Rights Reserved</div>
-                                <div className="item">The SlideWiki project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No 688095 </div>
+                                <div className="item" style={{textAlign: 'justify'}}>The SlideWiki project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No 688095 </div>
                                 <div className="item">Build GIT_COMMIT&#64;GIT_BRANCH</div>
                             </div>
                         </div>
--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -34,7 +34,7 @@ class Carousel extends React.Component {
     render() {
         return (
             <div ref="carousel">
-                <div className="ui fluid container grid " style={{minHeight: '200px'}}>       
+                <div className="ui fluid container grid " style={{minHeight: '200px'}}>
                         <div className="twelve wide column center aligned">
                             <div className="glide" tabIndex="-1">
                                 <div className="glide__arrows hide-element">
@@ -45,22 +45,22 @@ class Carousel extends React.Component {
                                 <div className="glide__wrapper hide-element">
                                     <ul className="glide__track">
                                         <li className="glide__slide" >
-                                            <a href="http://slidewiki.org" className="ui big image" tabIndex="-1">
+                                            <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                                 <img src="/assets/images/carousel/SW-deck.png" alt="Create slides with SlideWiki." />
                                             </a>
                                         </li>
                                         <li className="glide__slide" >
-                                            <a href="http://slidewiki.org" className="ui big image" tabIndex="-1">
+                                            <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                                 <img src="/assets/images/carousel/OER-Logo.png" alt="Repurpose & Reuse Educational Content through open educational resources." />
                                             </a>
                                         </li>
                                         <li className="glide__slide" >
-                                            <a href="http://slidewiki.org" className="ui big image" tabIndex="-1">
+                                            <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                                 <img src="/assets/images/carousel/collaborative-hands.png" alt="Collaborative Content Authoring." />
                                             </a>
                                         </li>
                                         <li className="glide__slide" >
-                                            <a href="http://slidewiki.org" className="ui big image" tabIndex="-1">
+                                            <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
                                                 <img src="/assets/images/carousel/globe-smartphone.png" alt="Supporting Knowledge Communities." />
                                             </a>
                                         </li>
@@ -69,7 +69,10 @@ class Carousel extends React.Component {
 
                                 <div className="glide__bullets"></div>
                             </div>
-                            <div onClick={this.togglePause.bind(this)} className="ui lightGrey right labeled icon button" role="button" tabIndex="0" >{this.state.paused ? <i className="ui play icon">Play</i> : <i className="ui pause icon">Pause</i>}</div>
+                            <div onClick={this.togglePause.bind(this)} className="ui lightGrey right labeled icon button" role="button" tabIndex="0" >
+                                {this.state.paused ? <i className="play icon"></i> : <i className="pause icon"></i>}
+                                {this.state.paused ? 'Play' : 'Pause'}
+                            </div>
 
                         </div>
 
@@ -94,7 +97,7 @@ class Carousel extends React.Component {
                         <div className="one wide column"></div>
                     </div>
                 </div>
-       
+
         );
     }
 }
