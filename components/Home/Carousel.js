6607223149d8f9c2ca41ec2ec603682b90f16e53
abijames <a.james@soton.ac.uk>
2017-07-12 13:25:40 +0100

    styling adn accessibility of carousel

--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -32,19 +32,22 @@ class Carousel extends React.Component {
         this.setState({paused: !this.state.paused});
     }
     render() {
+        const PauseStyle = {
+            backgroundColor: '#FFFFFF',
+            opacity: '1.0',
+            boxShadow: '0 0 0 1px rgba(34,36,38,.15) inset',
+            borderColor: '#1E78BB'
+        };
         return (
             <div ref="carousel">
-                <div className="ui  container grid " style={{minHeight: '200px'}}>
+                <div className="ui container grid " style={{minHeight: '200px'}}>
                     <div className="one wide column"></div>
                     <div className="fourteen wide column center aligned">
-                        <div className="ui segment" style={{borderRadius: '0px'}}>
+                        <div className="ui segment" >
                             <div className="ui two column fluid stackable grid">
-                                <div className="twelve wide column">
+                                <div className="eleven wide column">
                                     <div className="glide" tabIndex="-1">
-                                        <div className="glide__arrows hide-element">
-                                            <button className="glide__arrow prev ui basic icon button" data-glide-dir="<" tabIndex="-1"><i className="ui big icon chevron left"></i></button>
-                                            <button className="glide__arrow next ui basic icon button" data-glide-dir=">" tabIndex="-1"><i className="ui big icon chevron right"></i></button>
-                                        </div>
+
 
                                         <div className="glide__wrapper hide-element">
                                             <ul className="glide__track">
@@ -71,32 +74,46 @@ class Carousel extends React.Component {
                                             </ul>
                                         </div>
 
-                                        <div className="glide__bullets"></div>
+
                                     </div>
-                                    <div onClick={this.togglePause.bind(this)} className="ui lightGrey right labeled icon button" role="button" tabIndex="0" >
-                                        {this.state.paused ? <i className="play icon"></i> : <i className="pause icon"></i>}
-                                        {this.state.paused ? 'Play' : 'Pause'}
+                                    <div onClick={this.togglePause.bind(this)} className="ui icon button" style={PauseStyle} role="button" tabIndex="0" aria-label= {this.state.paused ? 'Play' 
+                                        : 'Pause'}>
+                                        {this.state.paused ? <i className="play blue icon"></i> : <i className="pause blue icon"></i>}
                                     </div>
                                 </div>
-                                <div className="left aligned four wide column">
+                                <div className="left aligned five wide column">
                                     <h2>Discover SlideWiki</h2>
-                                    <div className="ui relaxed divided list">
-                                        <div classname="item">
+                                    <div className="ui divided compact link items">      
+                                        <NavLink className="item gslide-header gh1"  data-glide-trigger='.glide' href="/features" data-glide-dir='=1'>
                                             <div className="content">
-                                                <div className="header"> <a className="item gslide-header gh1" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=1'>
+                                                <div className="tiny header">
                                                     Create Online Slide Decks
-                                                    </a></div>
+                                                </div>
                                                 <div className="description">description</div>
-                                            </div></div>
-                                        <a className="item gslide-header gh2" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=2'>
-                                            Repurpose and Reuse Educational Content
-                                        </a>
-                                        <a className="item gslide-header gh3" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=3'>
-                                            Collaborative Content Authoring
-                                        </a>
-                                        <a className="item gslide-header gh4" tabIndex="0" data-glide-trigger='.glide' data-glide-dir='=4'>
-                                            Supporting Knowledge Communities
-                                        </a>
+                                            </div>
+                                        </NavLink>
+                                        <NavLink className="item gslide-header gh2" data-glide-trigger='.glide' data-glide-dir='=2' href="/features" >
+                                            <div className="content">
+                                                <div className="tiny header">
+                                                    Repurpose and Reuse Educational Content
+                                                </div>
+                                                <div className="description">description</div>
+                                            </div>
+                                        </NavLink>
+                                        <NavLink className="item gslide-header gh3"  data-glide-trigger='.glide' href="/features" href="/features" data-glide-dir='=3'>
+                                            <div className="content">
+                                                <div className="tiny header">Collaborative Content Authoring
+                                                </div>
+                                                <div className="description">description</div>
+                                            </div>
+                                        </NavLink>
+                                        <NavLink className="item gslide-header gh4"  href="/features" data-glide-trigger='.glide' data-glide-dir='=4'>
+                                            <div className="content">
+                                                <div className="tiny header">Supporting Knowledge Communities
+                                                </div>
+                                                <div className="description">description</div>
+                                            </div>
+                                        </NavLink>
                                     </div>
                                 </div>
                             </div>
