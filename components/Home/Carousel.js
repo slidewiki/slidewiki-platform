b0b5d840c3f227bbeb35080670260129309a3792
abijames <a.james@soton.ac.uk>
2017-07-16 19:10:31 +0100

    Increased size of images. Added content to carousel menu.

--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -62,23 +62,23 @@ class Carousel extends React.Component {
                                         <div className="glide__wrapper hide-element">
                                             <ul className="glide__track" style={{minHeight: '300px'}}>
                                                 <li className="glide__slide">
-                                                    <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
-                                                        <img src="/assets/images/carousel/slidewiki-300x106px.png" alt="Create slides with SlideWiki." />
+                                                    <a href="http://slidewiki.org" className="ui large image" tabIndex="-1">
+                                                        <img src="/assets/images/carousel/slidewiki 500x200px.png" alt="Create slides with SlideWiki." />
                                                     </a>
                                                 </li>
                                                 <li className="glide__slide" >
-                                                    <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
-                                                        <img src="/assets/images/carousel/OER_Logo_300x92px.png" alt="Repurpose & Reuse Educational Content through open educational resources." />
+                                                    <a href="http://slidewiki.org" className="ui large image" tabIndex="-1">
+                                                        <img src="/assets/images/carousel/OER-Logo.png" alt="Repurpose & Reuse Educational Content through open educational resources." />
                                                     </a>
                                                 </li>
                                                 <li className="glide__slide" >
-                                                    <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
-                                                        <img src="/assets/images/carousel/handshake_300x145px.png" alt="Collaborative Content Authoring." />
+                                                    <a href="http://slidewiki.org" className="ui large image" tabIndex="-1">
+                                                        <img src="/assets/images/carousel/hands-1926704_640.png" alt="Collaborative Content Authoring." />
                                                     </a>
                                                 </li>
                                                 <li className="glide__slide" >
-                                                    <a href="http://slidewiki.org" className="ui medium image" tabIndex="-1">
-                                                        <img src="/assets/images/carousel/globe_300x134px.png" alt="Supporting Knowledge Communities." />
+                                                    <a href="http://slidewiki.org" className="ui large  image" tabIndex="-1">
+                                                        <img src="/assets/images/carousel/gglobe-smartphone-600px.png" alt="Supporting Knowledge Communities." />
                                                     </a>
                                                 </li>
                                             </ul>
@@ -92,7 +92,6 @@ class Carousel extends React.Component {
 
                                     </div>
 
-
                                 </div>
 
                                 <div className="left aligned five wide column">
@@ -103,7 +102,8 @@ class Carousel extends React.Component {
                                                 <div className="ui small header">
                                                     Create Online Slide Decks
                                                 </div>
-                                                <div className="description">description</div>
+                                                <div className="description">Use our  slide creator or import your existing slides to form online HTML slide decks./div>
+                                                </div>
                                             </div>
                                         </NavLink>
                                         <NavLink className="item gslide-header gh2" data-glide-trigger='.glide' data-glide-dir='=2' href="/features" >
@@ -111,26 +111,26 @@ class Carousel extends React.Component {
                                                 <div className="ui small header">
                                                     Reuseable Educational Content
                                                 </div>
-                                                <div className="description">description</div>
+                                                <div className="description">Discover a wide range of open educational slides and courses.</div>
                                             </div>
                                         </NavLink>
                                         <NavLink className="item gslide-header gh3"  data-glide-trigger='.glide' href="/features" href="/features" data-glide-dir='=3'>
                                             <div className="content">
                                                 <div className="ui small header">Collaborative Content Authoring
                                                 </div>
-                                                <div className="description">description</div>
+                                                <div className="description">Create slides online together with peers and colleagues through our collaborative editing features.</div>
                                             </div>
                                         </NavLink>
                                         <NavLink className="item gslide-header gh4"  href="/features" data-glide-trigger='.glide' data-glide-dir='=4'>
                                             <div className="content">
                                                 <div className="ui small header">Supporting Knowledge Communities
                                                 </div>
-                                                <div className="description">description</div>
+                                                <div className="description">Using our search and tags features discover content and authors who share your interests. </div>
                                             </div>
                                         </NavLink>
                                     </div>
                                 </div>
-                                  </div>
+                            </div>
 
                         </div>
                     </div>
