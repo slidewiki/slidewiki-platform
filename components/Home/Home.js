52a75df16d0d1543921cc3058b5686e00eb8d5d7
abijames <a.james@soton.ac.uk>
2017-07-16 20:11:02 +0100

    correct lint error

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -32,80 +32,80 @@ class Home extends React.Component {
                     <div className="ui padded stackable grid ">
                         <div className="one wide column"></div>
                         <div className="four wide column">
-                            <div className="four wide column">
-                                <div className="ui segments">
-                                    <div className="ui segment top attached">
-                                        <h3>Get Started</h3>
-                                    </div>
-                                    <div className="ui segment">
-                                        <div className="ui divide list">
+
+                            <div className="ui segments">
+                                <div className="ui segment top attached">
+                                    <h3>Get Started</h3>
+                                </div>
+                                <div className="ui segment">
+                                    <div className="ui divide list">
+                                        <div className="item">
+                                            <div className="content">
+                                                <NavLink className="ui header" routeName="license" href="/signup">Sign up for a SlideWiki account</NavLink>
+                                                <div className="description">
+                                                    <p>Create an account to start creating and sharing your decks</p>
+                                                </div>
+                                            </div>
+                                            <div classame="ui hidden divider"></div>
                                             <div className="item">
                                                 <div className="content">
-                                                    <NavLink className="ui header" routeName="license" href="/signup">Sign up for a SlideWiki account</NavLink>
+                                                    <a className="ui header" href="/features">Learn about  SlideWiki</a>
                                                     <div className="description">
-                                                        <p>Create an account to start creating and sharing your decks</p>
+                                                        <p>Find out more about SlideWiki's features and how it can be used to create, share and adapt slides, decks and open educational resources.</p>
                                                     </div>
                                                 </div>
-                                                <div classame="ui hidden divider"></div>
-                                                <div className="item">
-                                                    <div className="content">
-                                                        <a className="ui header" href="/features">Learn about  SlideWiki</a>
-                                                        <div className="description">
-                                                            <p>Find out more about SlideWiki's features and how it can be used to create, share and adapt slides, decks and open educational resources.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div classame="ui hidden divider"></div>
-                                                <div className="item">
-                                                    <div className="content">
-                                                        <a className="ui header" href="https://slidewiki.eu">Find out about the SlideWiki project</a>
-                                                        <div className="description">
-                                                            <p>SlideWiki is an open source development project, funded from the European Union's Horizon 2020 research and innovation programme. The project involves 17 partners to develop, test and trial SlideWiki. To find out more or get involved visit the <a href="https://slidewiki.eu">project webiste</a>.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div classame="ui hidden divider"></div>
                                             </div>
+                                            <div classame="ui hidden divider"></div>
+                                            <div className="item">
+                                                <div className="content">
+                                                    <a className="ui header" href="https://slidewiki.eu">Find out about the SlideWiki project</a>
+                                                    <div className="description">
+                                                        <p>SlideWiki is an open source development project, funded from the European Union's Horizon 2020 research and innovation programme. The project involves 17 partners to develop, test and trial SlideWiki. To find out more or get involved visit the <a href="https://slidewiki.eu">project webiste</a>.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div classame="ui hidden divider"></div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <div className="five wide column">
-                                <div className="ui segments">
-                                    <div className="ui segment top attached">
-                                        <h3><FormattedMessage id='home.featured' defaultMessage='Featured decks'/></h3>
-                                    </div>
-                                    <DeckList scope="featured"/>
+                        <div className="five wide column">
+                            <div className="ui segments">
+                                <div className="ui segment top attached">
+                                    <h3><FormattedMessage id='home.featured' defaultMessage='Featured decks'/></h3>
                                 </div>
-                            </div>
-                            <div className="five wide column">
-                                <div className="ui segments">
-                                    <div className="ui segment top attached">
-                                        <h3><FormattedMessage id='home.recent' defaultMessage='Recent decks'/>
-                                        </h3>
-                                    </div>
-                                    <DeckList scope="recent"/>
-                                    <a className="ui bottom attached fuild button" href='/recent'><FormattedMessage id='home.view_all' defaultMessage='View all'/>
-                                    </a>
-                                </div>
-                                <div className="one wide column"></div>
+                                <DeckList scope="featured"/>
                             </div>
                         </div>
+                        <div className="five wide column">
+                            <div className="ui segments">
+                                <div className="ui segment top attached">
+                                    <h3><FormattedMessage id='home.recent' defaultMessage='Recent decks'/>
+                                    </h3>
+                                </div>
+                                <DeckList scope="recent"/>
+                                <a className="ui bottom attached fuild button" href='/recent'><FormattedMessage id='home.view_all' defaultMessage='View all'/>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="one wide column"></div>
                     </div>
                 </div>
-                );
-                }
-                }
+            </div>
+        );
+    }
+}
 
-                Home.contextTypes = {
-                    intl: React.PropTypes.object.isRequired,
-                        getUser: React.PropTypes.func
-                };
-
-
-                export default Home;
+Home.contextTypes = {
+    intl: React.PropTypes.object.isRequired,
+    getUser: React.PropTypes.func
+};
 
 
+export default Home;
 
-                //export default Home;
+
+
+//export default Home;
