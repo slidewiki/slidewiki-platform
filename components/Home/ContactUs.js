6aa3082f9db39230347fe61691d0b7aa38f572dc
svalero <svalero@dsic.upv.es>
2017-07-27 13:35:04 +0200

    SWIK-15-42-ContactForm the contact form loads the data of the logged user

--- components/Home/ContactUs.js
+++ components/Home/ContactUs.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {connectToStores} from 'fluxible-addons-react';
-import { Container, Divider,Header,Segment,Form,Label,Dropdown,Input} from 'semantic-ui-react';
+import { Container, Divider,Header,Segment,Form,Label,Dropdown,Input,TextArea} from 'semantic-ui-react';
 import UserProfileStore from '../../stores/UserProfileStore';
+import ReCAPTCHA from 'react-google-recaptcha';
+import {publicRecaptchaKey} from '../../configs/general';
+import fetchUser from '../../actions/user/userprofile/fetchUser';
+import {FormattedMessage, defineMessages} from 'react-intl';
 
 class ContactUs extends React.Component {
     constructor(props){
@@ -13,6 +17,7 @@ class ContactUs extends React.Component {
             type : '',
             firstName:this.props.UserProfileStore.user.fname,
             lastName:this.props.UserProfileStore.user.lname,
+            email: this.props.UserProfileStore.user.email
 
         };
         console.log(this.props.UserProfileStore.username);
@@ -20,48 +25,55 @@ class ContactUs extends React.Component {
 
 
 
-    }
 
+    }
+    componentDidMount(){
+        this.context.executeAction(fetchUser,{ params: {username: this.props.UserProfileStore.username, id:this.props.UserProfileStore.userid}});
+    }
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            firstName :nextProps.UserProfileStore.user.fname,
+            lastName:nextProps.UserProfileStore.user.lname,
+            email: nextProps.UserProfileStore.user.email
+        });
+
+    }
+    /*
+    onChange={(e, {value}) => {this.handleTypeChange(value);}
     handleTypeChange(value){
         this.setState({
             type:value
         });
-    }
-    handleNameChange(value){
+    }*/
+    onRecaptchaChange(response) {
         this.setState({
-            name:value
+            'grecaptcharesponse': response
         });
-
+        console.log(response);
     }
-    handleLastChange(value){
-        this.setState({
-            name:value
-        });
 
-    }
     render() {
-        let typeOptions = [
+        const typeOptions = [
           {value:'Suggestion' , text:'Suggestion'},
-          {value:'Error' , text:'Error'},
+          {value:'Support' , text:'Support Issue'},
+          {value:'Account' , text:'Account Issue'},
           {value:'Other' , text:'Other'},
 
         ];
-        let labelStyle = {
+        const labelStyle = {
             width:20+'px'
         };
-        //Short way. It creates symple labels. If they don't like ribbons, we can change to this     <Form.Input id='NameContact' label='Name:' placeholder='Your name' />
+
+        const recaptchaStyle = {display: 'inline-block'};
+
+        //Short way. It creates symple labels. If they don't like ribbons, we can change to this:
+        //     <Form.Input id='NameContact' label='Name:' placeholder='Your name' />
         return (
             <Container text>
                 <Divider hidden />
 
                 <Header as="h2">Contact Us</Header>
-                <p>Let us know what you think of SlideWiki so we can continue to
-                 make it better. If you find a problem or bug please try to upload
-                 a screenshot to help us track it down. Providing your name and
-                 email address will allow us to contact you if we need further
-                 information to resolve your issues. You can also email feedback
-                  to <a href="mailto:jira@slidewiki.atlassian.net?Subject=SlideWiki%20Feedback" target="_blank"> jira@slidewiki.atlassian.net</a>.
-                  Thank you!</p>
+                <p>If you wish to contact us, please complete the form below. If you wish to report an issue with a particular deck, please use the Reporting button on the deck.</p>
 
                   <Divider hidden />
                   <Segment attached="bottom" textAlign="left" >
@@ -69,21 +81,35 @@ class ContactUs extends React.Component {
                     <Form>
 
                       <Form.Field>
-                      <Label as='label' style={labelStyle} ribbon color='blue' htmlFor="typeContact">Type of report:</Label>
-                      <Dropdown selection id='typeContact' name='typeContact' ref={(type) => {this.type = type;}}  placeholder='Select type of report' options={typeOptions} role="listbox"  onChange={(e, {value}) => {this.handleTypeChange(value);}}/>
+                      <Label as='label' style={labelStyle} ribbon color='blue' htmlFor="typeContact">Type of report*:</Label>
+                      <Dropdown selection id='typeContact' name='typeContact' ref={(type) => {this.typeContact = type;}}  placeholder='Select type of report' options={typeOptions} aria-required="true" />
                       </Form.Field>
                       <Form.Field>
                         <Label as='label' style={labelStyle} ribbon color='blue'  htmlFor="firstNameContact">First Name:</Label>
-                        <Input type='text' id='firstNameContact' name="firstNameContact" placeholder='Your first name'  onChange={(e, {value}) => {this.handleFisrtNameChange(value);}}/>
+                        <Input type='text' id='firstNameContact'  ref={(input) => {this.firstNameContact = input;}} name="firstNameContact" placeholder='Your first name' value={this.state.firstName}/>
                       </Form.Field>
                       <Form.Field>
                         <Label as='label' style={labelStyle} ribbon color='blue'  htmlFor="lastNameContact">Last Name:</Label>
-                        <Input type='text' id='firstNameContact' name="lastNameContact" placeholder='Your last name'  onChange={(e, {value}) => {this.handleLastNameChange(value);}}/>
+                        <Input type='text' id='firstNameContact' name="lastNameContact" placeholder='Your last name' value={this.state.lastName}/>
                       </Form.Field>
                       <Form.Field>
-                        <Label as='label' style={labelStyle} ribbon color='blue'  htmlFor="emailContact">Email:</Label>
-                        <Input type='email' id='emailContact' name="emailContact" placeholder='user@server.com'  onChange={(e, {value}) => {this.handleEmailChange(value);}}/>
+                        <Label as='label' style={labelStyle} ribbon color='blue'  htmlFor="emailContact">Email*:</Label>
+                        <Input type='email' id='emailContact' name="emailContact" placeholder='user@server.com'  aria-required="true"  value={this.state.email}/>
                       </Form.Field>
+                      <Form.Field>
+                        <Label as='label' style={labelStyle} ribbon color='blue'  htmlFor="summaryContact">Summary*:</Label>
+                        <Input type='text' id='summaryContact' name="summaryContact" placeholder='Please, write us a one-sentence summary' aria-required="true" />
+                      </Form.Field>
+                      <Form.Field>
+                        <Label as='label' style={labelStyle} ribbon color='blue'  htmlFor="descriptionContact">Context:</Label>
+                         <TextArea id='descriptionContact' name="descriptionContact" autoHeight placeholder='Please, give us more information about' />
+                      </Form.Field>
+                      <Form.Field>
+                        <input type="hidden" id="recaptchaContact" name="recaptchaContact"></input>
+                        <ReCAPTCHA style={recaptchaStyle} sitekey={publicRecaptchaKey}  onChange={this.onRecaptchaChange.bind(this)}  aria-required="true"/>
+                      </Form.Field>
+                      <Form.Button color='blue'>Send Feedback</Form.Button>
+
 
 
 
@@ -95,9 +121,16 @@ class ContactUs extends React.Component {
         );
     }
 }
+
+ContactUs.contextTypes = {
+    executeAction: React.PropTypes.func.isRequired,
+    intl: React.PropTypes.object.isRequired
+};
+
 ContactUs = connectToStores(ContactUs,[UserProfileStore],(context,props) => {
     return {
         UserProfileStore: context.getStore(UserProfileStore).getState()
     };
 });
+
 export default ContactUs;
