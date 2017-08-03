bb5275e4d3e3eb90f55e9e452e7a23c4353179c7
Huw Fryer <huwfryer@gmail.com>
2017-08-03 15:37:35 +0100

    SWIK-1584 Adding subdeck to store

--- stores/ContentStore.js
+++ stores/ContentStore.js
@@ -3,7 +3,7 @@ import {BaseStore} from 'fluxible/addons';
 class ContentStore extends BaseStore {
     constructor(dispatcher) {
         super(dispatcher);
-        this.selector = {'id': 0, 'spath': '', 'sid': 0, 'stype': '', page: 'content', 'theme': 'white'};
+        this.selector = {'id': 0, 'subdeck': 0, 'spath': '', 'sid': 0, 'stype': '', page: 'content', 'theme': 'white'};
         this.mode = 'view';
     }
     updateContent(payload) {
@@ -13,7 +13,8 @@ class ContentStore extends BaseStore {
             'sid': payload.params.sid,
             'stype': payload.params.stype,
             'page': payload.page,
-            'theme': payload.params.theme
+            'theme': payload.params.theme,
+            'subdeck': this.getCurrentSubdeck()
         };
         this.mode = payload.params.mode;
         this.emitChange();
@@ -24,6 +25,7 @@ class ContentStore extends BaseStore {
         this.selector.sid = selector.sid;
         this.selector.stype = selector.stype;
         this.selector.theme = selector.theme;
+        this.selector.currentSubDeck = selector.currentSubDeck;
         this.emitChange();
     }
     getState() {
@@ -39,6 +41,29 @@ class ContentStore extends BaseStore {
         this.selector = state.selector;
         this.mode = state.mode;
     }
+    getCurrentSubdeck(){
+        let selector = this.selector;
+        console.log('selector', selector);
+        let currentSubDeck;
+        let splitSpath = selector.spath.split(';');
+        console.log('stype: ', selector.stype);
+        if(selector.spath === ''){
+            return selector.id;
+        }
+        else if(selector.stype === 'deck' && selector.spath){
+            console.log('spath ', selector.spath);
+            currentSubDeck = splitSpath[splitSpath.length - 1].split(':')[0];
+            // console.log('length: ', currentSubDeck.length);
+            console.log('currentSubDeck: ', currentSubDeck);
+        }
+        else if(selector.stype === 'slide' && selector.spath){
+            // Since the last one in the list will be the slide ID, we use -2
+            currentSubDeck = splitSpath[splitSpath.length - 2].split(':')[0];
+        }
+        return currentSubDeck;
+
+    }
+
 }
 
 ContentStore.storeName = 'ContentStore';
