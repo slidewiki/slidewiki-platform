c3e327d98a279dbf5848a49fdb1b155b193ba97f
Huw Fryer <huwfryer@gmail.com>
2017-08-03 21:14:09 +0100

    SWIK-1584 Presentation now only plays subdecks

--- services/presentation.js
+++ services/presentation.js
@@ -6,29 +6,92 @@ export default {
     name: 'presentation',
     // At least one of the CRUD methods is Required
     read: (req, resource, params, config, callback) => {
+        // console.log('req:', req, '\n\n');
+        // console.log('resource': resource, '\n\n');
+        // console.log('params', params);
+
         req.reqId = req.reqId ? req.reqId : -1;
         log.info({Id: req.reqId, Service: __filename.split('/').pop(), Resource: resource, Operation: 'read', Method: req.method});
         let args = params.params? params.params : params;
-        let selector= {'id': args.id, 'spath': args.spath, 'sid': args.sid, 'stype': args.stype, 'mode': args.mode};
+        // console.log('args', args);
+        let selector= {'id': args.id, 'subdeck': args.subdeck, 'spath': args.spath, 'sid': args.sid, 'stype': args.stype, 'mode': args.mode};
         //Load the whole presentation
         let presentation = [];
         if(resource === 'presentation.content'){
             /*********connect to microservices*************/
             let returnErr = false;
-            let slideServiceRes;
-            //let theme = get_sample_theme();
-            rp.get({uri: Microservices.deck.uri + '/deck/' + String(args.id) + '/slides'}).then((res) => {
-                slideServiceRes = JSON.parse(res);
-                callback(null, {content: slideServiceRes.children, theme: slideServiceRes.theme, selector: selector});
 
+            let id = args.subdeck ? args.subdeck : args.id;
+            let isSubdeck = (id === args.subdeck);
+            // let subdeck = args.subdeck;
+            let slideServiceRes;
+            let nextDeck = '';
+            // Get the full structure of the deck first
+
+            let uri = Microservices.deck.uri + '/deck/' + String(id) + '/slides';
+            rp.get({uri: uri}).then((res) => {
+                let slideServiceRes = JSON.parse(res);
+                if(isSubdeck){
+                    // Find the next one in the list (if it exists)
+                    rp.get(Microservices.deck.uri + '/deck/' + String(args.id)).then((res) => {
+                        // slideServiceRes = JSON.parse(res);
+                        // let next = false;
+                        // for(s in slideServiceRes.revisions[0].contentItems){
+                        //     if(next){
+                        //
+                        //     }
+                        //     if(s.id === id){
+                        //
+                        //     }
+                        // }
+                        callback(null, {content: slideServiceRes.children, theme: slideServiceRes.theme, selector: selector});
+                    }).catch((err) => {
+                        console.log('There was an error!', err);
+                        returnErr = true;
+                        callback(null, {content: slideServiceRes, theme: theme, selector: selector});
+
+                    });
+
+                }
+                else{
+                    callback(null, {content: slideServiceRes.children, theme: slideServiceRes.theme, selector: selector});
+                }
             }).catch((err) => {
                 console.log('There was an error!', err);
                 returnErr = true;
                 callback(null, {content: slideServiceRes, theme: theme, selector: selector});
+
             });
+
+
+            // return rp.get().then((res) => {
+            //     slideServiceRes = JSON.parse(res);
+            //     // console.log('slideServiceRes', res);
+            //
+            //
+            // }).catch((err) => {
+            // });
+
         }//If presentation.content
+    },
+
+    getSlides: (id, isSubdeck) => {
+
     }
 };
+
+function getSlides(args){
+  // This function will check which set of slides we want
+  //Algorithm will work as follows:
+  /*
+  1. If there is a slide ID, check if the parent deck is the same as root deck
+  2. If there is a deck ID, iterate through contentItems to see if there is a deck
+    2a. If yes, load presentation from start until the end of the deck
+    2b. If no, load the whole deck
+  */
+    // if(args.stype === 'slide'){
+
+}
 
 function get_sample_theme(){
     let themes = ['beige', 'black', 'blood', 'league', 'moon', 'night', 'serif', 'simple', 'sky', 'solarized', 'white'];
