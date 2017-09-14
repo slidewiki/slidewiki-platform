6e893156a711ab7cf762d1d69bc290240787ea08
Vinay Modi <vinay.modi@gmail.com>
2017-09-14 13:50:55 +0200

    Updated deleteQuestion.js to show question panel (thus remove edit question text boxes) after deleting a question

--- actions/questions/deleteQuestion.js
+++ actions/questions/deleteQuestion.js
@@ -6,10 +6,10 @@ export default function deleteQuestion(context, payload, done) {
     context.service.delete('questions.delete', payload, {timeout: 20 * 1000}, (err, res) => {
         if (err) {
             log.error(context, {filepath: __filename});
-            context.executeAction(serviceUnavailable, payload, done);
-            context.dispatch('DELETE_QUESTION_FAILED');
+            context.dispatch('LOAD_CONTENT_MODULES_SUCCESS', {moduleType: 'question', selector:payload.question.selector});
         } else {
-            context.dispatch('DELETE_QUESTION', res);
+            context.dispatch('LOAD_CONTENT_MODULES_SUCCESS', {moduleType: 'question', selector:payload.question.selector});
+            context.dispatch('UPDATE_MODULE_TYPE_SUCCESS', {moduleType: 'questions'});
         }
         done();
     });
