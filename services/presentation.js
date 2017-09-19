c0f3b9d29a63cb09591e27f73b9a50d18cf42ebb
Huw Fryer <huwfryer@gmail.com>
2017-09-19 15:25:31 +0100

    SWIK-1584 removed logging and unused function

--- services/presentation.js
+++ services/presentation.js
@@ -21,25 +21,15 @@ export default {
             //let theme = get_sample_theme();
             let isSubdeck = selector.id !== selector.subdeck;
             let id = isSubdeck ? selector.subdeck : selector.id;
-            console.log('selector: ', selector, '\nargs: ', args, '\nid: ', id, 'isSubdeck: ', isSubdeck, '\n\n');
 
             rp.get({uri: Microservices.deck.uri + '/deck/' + String(id) + '/slides'}).then((res) => {
                 slideServiceRes = JSON.parse(res);
                 callback(null, {content: slideServiceRes.children, theme: slideServiceRes.theme, selector: selector});
 
             }).catch((err) => {
-                console.log('There was an error!', err);
                 returnErr = true;
                 callback(null, {content: slideServiceRes, theme: theme, selector: selector});
             });
         }//If presentation.content
     }
 };
-
-function get_sample_theme(){
-    let themes = ['beige', 'black', 'blood', 'league', 'moon', 'night', 'serif', 'simple', 'sky', 'solarized', 'white'];
-    let index = Math.floor(Math.random() * (themes.length - 1));
-    // Just hardcode this for now.  Change to themes[index] if we really want it random.
-    //return themes[0];
-    return themes[12];
-}
