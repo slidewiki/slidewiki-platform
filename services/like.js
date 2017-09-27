4281bea3ea46bc63aaeb1f32c3ab4323745b2cc5
dpaun <dpaunovic@gmail.com>
2017-09-27 10:25:20 +0200

    SWIK-1704 adjusted fetching of likes

--- services/like.js
+++ services/like.js
@@ -32,7 +32,7 @@ export default {
                 //console.log("LOAD Deck id: " + targetDeckID);
                   /*********connect to microservices*************/
                 rp.get({uri: Microservices.activities.uri + '/activities/deck/' + targetDeckID + '?metaonly=false&activity_type=react&all_revisions=true'}).then((res) => {
-                    let activities = JSON.parse(res);
+                    let activities = JSON.parse(res).items;
                     let listOfUserIDs = [];
 
                     activities.forEach((activity) => listOfUserIDs.push(activity.user_id));
