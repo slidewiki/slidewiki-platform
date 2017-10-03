2aa0c3e470d0342b96426ed36c2468d53036d60f
svalero <svalero@dsic.upv.es>
2017-10-03 14:23:51 +0200

    - Form added with radio buttons to select the format - download button downloads the selected option. Default option PDF - Accesibility labels added:   -- Tab to access/leave the radioGroup   -- Arrow keys to move over the options.

--- components/Deck/ContentPanel/ContentActions/DownloadModal.js
+++ components/Deck/ContentPanel/ContentActions/DownloadModal.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import FocusTrap from 'focus-trap-react';
-import { Button, Container, Form, Modal, TextArea, Icon, Segment } from 'semantic-ui-react';
-const headerStyle = {
-    'textAlign': 'center'
-};
+import { Button, Container, Form, Modal, Radio, Icon, Segment, Grid } from 'semantic-ui-react';
+import {connectToStores} from 'fluxible-addons-react';
+import ContentStore from '../../../../stores/ContentStore';
+import UserProfileStore from '../../../../stores/UserProfileStore';
+import {Microservices} from '../../../../configs/microservices';
+import addActivity from '../../../../actions/activityfeed/addActivity';
+import incrementDeckViewCounter from '../../../../actions/activityfeed/incrementDeckViewCounter';
+import {FormattedMessage, defineMessages} from 'react-intl';
+
 
 class DownloadModal extends React.Component{
     constructor(props) {
@@ -12,6 +17,7 @@ class DownloadModal extends React.Component{
         this.state = {
             modalOpen: false,
             activeTrap: false,
+            radioValue: 'PDF'
 
         };
 
@@ -19,6 +25,27 @@ class DownloadModal extends React.Component{
         this.handleClose = this.handleClose.bind(this);
         this.unmountTrap = this.unmountTrap.bind(this);
         this.handleDownload = this.handleDownload.bind(this);
+        this.handleRadioChange = this.handleRadioChange.bind(this);
+
+
+        this.messages = defineMessages({
+            downloadModal_header:{
+                id: 'downloadModal.downloadModal_header',
+                defaultMessage:'Download this deck'
+            },
+            downloadModal_description:{
+                id: 'downloadModal.downloadModal_description',
+                defaultMessage:'Select the download file format:'
+            },
+            downloadModal_downloadButton:{
+                id:'downloadModal.downloadModal_downloadButton',
+                defaultMessage: 'Download'
+            },
+            downloadModal_cancelButton:{
+                id:'downloadModal.downloadModal_cancelButton',
+                defaultMessage: 'Cancel'
+            }
+        });
     }
 
     handleOpen(){
@@ -45,9 +72,69 @@ class DownloadModal extends React.Component{
             $('#app').attr('aria-hidden','false');
         }
     }
-    handleDownload(){
+    handleRadioChange(event,data){
+        this.setState({
+            radioValue:data.value
+        });
 
     }
+    getExportHref(type){
+        let splittedId;
+        if (this.props.ContentStore.selector.id !== undefined && this.props.ContentStore.selector.id !== '' && this.props.ContentStore.selector.id !== 0){
+
+            splittedId =  this.props.ContentStore.selector.id.split('-'); //separates deckId and revision
+        } else {
+            return ''; //no deckId
+        }
+
+        switch (type) {
+            case 'PDF':
+                return Microservices.pdf.uri + '/exportPDF/' + splittedId[0];
+                break;
+            case 'ePub':
+                return Microservices.pdf.uri + '/exportEPub/' + splittedId[0];
+                break;
+            case 'SCORMv1.2':
+            case 'SCORMv2':
+            case 'SCORMv3':
+            case 'SCORMv4':
+                let version = type.split('v'); //separates format from version. In second position we have the version
+                return Microservices.pdf.uri + '/exportSCORM/' + splittedId[0]+ '?version='+version[1];
+                break;
+            default:
+                return '';
+
+        }
+
+
+    }
+    createDownloadActivity() {
+        //create new activity
+        let splittedId =  this.props.ContentStore.selector.id.split('-'); //separates deckId and revision
+        let userId = String(this.props.UserProfileStore.userid);
+        if (userId === '') {
+            userId = '0';//Unknown - not logged in
+        }
+        let activity = {
+            activity_type: 'download',
+            user_id: userId,
+            content_id: splittedId[0],
+            content_kind: 'deck'
+        };
+        this.context.executeAction(addActivity, {activity: activity});
+        context.executeAction(incrementDeckViewCounter, {type: 'download'});
+    }
+    handleDownload(event,data){
+        if(process.env.BROWSER){
+            event.preventDefault();
+            window.open(this.getExportHref(this.state.radioValue));
+        }
+
+        this.createDownloadActivity();
+
+
+    }
+
     render() {
         return(
 
@@ -59,35 +146,126 @@ class DownloadModal extends React.Component{
 
                       }
                   open={this.state.modalOpen}
-                  onOpen={this.handleOpen}
                   onClose={this.handleClose}
                   id="downloadModal"
                   aria-labelledby="downloadModalHeader"
                   aria-describedby="downloadModalDescription"
+                  aria-hidden = {!this.state.modalOpen}
                   tabIndex="0"
               >
                   <FocusTrap
                       id="focus-trap-downloadModal"
                       className = "header"
                       active={this.state.activeTrap}
-                      onDeactivate={this.unmountTrap}
-                      clickOutsideDeactivates={true}
-                      initialFocus=""
+                      focusTrapOptions={{
+                          onDeactivate: this.unmountTrap,
+                          clickOutsideDeactivates: true ,
+                          initialFocus: '#downloadModalDescription'
+                      }}
                   >
                       <Modal.Header className="ui center aligned" id="downloadModalHeader">
-                          <h1 style={headerStyle}>Download this deck</h1>
+                          <h1 style={{'textAlign': 'center'}}> {this.context.intl.formatMessage(this.messages.downloadModal_header)}</h1>
                       </Modal.Header>
                       <Modal.Content>
                           <Container>
                               <Segment color="blue" textAlign="center" padded>
-                                 <Segment>
-                                     <div className="sr-only" id="downloadModalDescription">Select one target format to download this deck</div>
-                                  <Form id="downloadForm">
+                                <Segment>
+                                  <Form id="downloadForm" >
+                                   <Grid >
+                                   <Grid.Row>
 
+                                   <Grid.Column width={3}>
+
+                                   </Grid.Column>
+
+                                   <Grid.Column textAlign='left' width={13} role="radiogroup">
+                                    <label  id="downloadModalDescription" >{this.context.intl.formatMessage(this.messages.downloadModal_description)}</label>
+                                    <Form.Field >
+                                          <Radio
+                                            label='PDF'
+                                            name='downloadRadioGroup'
+                                            value='PDF'
+                                            checked={this.state.radioValue === 'PDF'}
+                                            onChange={this.handleRadioChange}
+                                            role="radio"
+                                            aria-checked={this.state.radioValue === 'PDF'}
+                                            tabIndex="0"
+                                            />
+                                        </Form.Field>
+                                        <Form.Field>
+                                          <Radio
+                                              label='ePub'
+                                              name='downloadRadioGroup'
+                                              value='ePub'
+                                              checked={this.state.radioValue === 'ePub'}
+                                              onChange={this.handleRadioChange}
+                                              role="radio"
+                                              aria-checked={this.state.radioValue === 'ePub'}
+                                              tabIndex="-1"
+
+                                          />
+                                         </Form.Field>
+                                         <Form.Field>
+                                           <Radio
+                                               label='SCORM 1.2'
+                                               name='downloadRadioGroup'
+                                               value='SCORMv1.2'
+                                               checked={this.state.radioValue === 'SCORMv1.2'}
+                                               onChange={this.handleRadioChange}
+                                               role="radio"
+                                               aria-checked={this.state.radioValue === 'SCORMv1.2'}
+                                               tabIndex="-1"
+
+                                           />
+                                          </Form.Field>
+                                          <Form.Field>
+                                            <Radio
+                                                label='SCORM 2004 (3rd edition)'
+                                                name='downloadRadioGroup'
+                                                value='SCORMv2'
+                                                checked={this.state.radioValue === 'SCORMv2'}
+                                                onChange={this.handleRadioChange}
+                                                role="radio"
+                                                aria-checked={this.state.radioValue === 'SCORMv2'}
+                                                tabIndex="-1"
+
+                                            />
+                                          </Form.Field>
+                                          <Form.Field>
+                                            <Radio
+                                                label='SCORM 2004 (4th edition)'
+                                                name='downloadRadioGroup'
+                                                value='SCORMv3'
+                                                checked={this.state.radioValue === 'SCORMv3'}
+                                                onChange={this.handleRadioChange}
+                                                role="radio"
+                                                tabIndex="-1"
+                                            />
+                                          </Form.Field>
+                                          <Form.Field>
+                                            <Radio
+                                                label='SCORM 2004 (5th edition)'
+                                                name='downloadRadioGroup'
+                                                value='SCORMv4'
+                                                checked={this.state.radioValue === 'SCORMv4'}
+                                                onChange={this.handleRadioChange}
+                                                role="radio"
+                                                aria-checked={this.state.radioValue === 'SCORMv4'}
+                                                tabIndex="-1"
+                                            />
+                                          </Form.Field>
+
+                                      </Grid.Column>
+                                      <Grid.Column>
+                                      </Grid.Column>
+                                      </Grid.Row>
+                                      <Grid.Row>
+
+                                      <Grid.Column width={16} textAlign='center'>
                                       <Button
                                           color="blue"
                                           type="submit"
-                                          content="Download"
+                                          content={this.context.intl.formatMessage(this.messages.downloadModal_downloadButton)}
                                           icon='download'
                                           onClick={this.handleDownload}
                                       />
@@ -96,11 +274,17 @@ class DownloadModal extends React.Component{
                                           color="red"
                                           type="button"
                                           onClick={this.handleClose}
-                                          content="Cancel"
+                                          content={this.context.intl.formatMessage(this.messages.downloadModal_downloadButton)}
                                       />
+                                      </Grid.Column>
+                                      </Grid.Row>
+
+                                      </Grid>
 
                                   </Form>
-                                  </Segment>
+
+
+                                </Segment>
                               </Segment>
                           </Container>
 
@@ -115,8 +299,16 @@ class DownloadModal extends React.Component{
 
 
 }
+
 DownloadModal.contextTypes = {
-    executeAction: React.PropTypes.func.isRequired
+    executeAction: React.PropTypes.func.isRequired,
+    intl: React.PropTypes.object.isRequired
 };
+DownloadModal = connectToStores(DownloadModal,[ContentStore,UserProfileStore],(context,props) => {
+    return{
+        ContentStore : context.getStore(ContentStore).getState(),
+        UserProfileStore : context.getStore(UserProfileStore).getState()
+    };
+});
 
 export default DownloadModal;
