1cd22d32e8eafb292b21a7024a00b46ed895b25c
svalero <svalero@dsic.upv.es>
2017-10-19 17:29:20 +0200

    SWIK-1796-fixDeckReport added an action which calls the send email service for sening the report

--- actions/report/sendReport.js
+++ actions/report/sendReport.js
@@ -1,4 +1,45 @@
-/**
- * Created by lfernandes on 12.03.17.
- */
+import log from '../log/clog';
+//import {navigateAction} from 'fluxible-router';
 
+export default function sendReport(context,payload,done){
+    log.info(context);
+
+    context.service.create('email', {subject: payload.subject, message: payload.text}, { timeout: 20 * 1000 }, (err, res) => {
+        // console.log('action got:', err, res);
+        if (err) {
+            swal({
+                title: payload.swal_messages.title,
+                text: payload.swal_messages.error_text,
+                type: 'error',
+                confirmButtonText: payload.swal_messages.error_confirmButtonText,
+                confirmButtonClass: 'positive ui button',
+                allowEscapeKey: false,
+                allowOutsideClick: false,
+                buttonsStyling: false
+            })
+            .then(() => {
+                //nothing
+            });
+        } else {
+            swal({
+                title: payload.swal_messages.title,
+                text:payload.swal_messages.text,
+                type: 'success',
+                confirmButtonText: payload.swal_messages.confirmButtonText,
+                confirmButtonClass: 'positive ui button',
+                allowEscapeKey: false,
+                allowOutsideClick: false,
+                buttonsStyling: false
+            })
+            .then(() => {
+                //go to homepage
+                //context.executeAction(navigateAction, {
+                  //go to home page after
+                  //  url: '/'
+                //});
+
+            });
+        }
+        done();
+    });
+}
