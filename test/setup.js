63e7b52677334ec2292baf20de76180ef49d2521
Todor Tsankov <tsankov.todya@gmail.com>
2017-10-21 09:27:27 +0200

    qa-setup migrate to enzyme v3 config

--- test/setup.js
+++ test/setup.js
@@ -1,20 +1,6 @@
 require('babel-register')();
+// setup file
+var enzyme = require('enzyme');
+var Adapter = require('enzyme-adapter-react-15');
 
-const jsdom = require('jsdom').jsdom;
-
-let exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
-        exposedProperties.push(property);
-        global[property] = document.defaultView[property];
-    }
-});
-
-global.navigator = {
-    userAgent: 'node.js'
-};
-
-documentRef = document;
+enzyme.configure({ adapter: new Adapter() });
