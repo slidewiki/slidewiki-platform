825f41655ca2ce38876da84a180087c83374def3
svalero <svalero@dsic.upv.es>
2017-11-03 14:31:22 +0100

    SWIK-1500-fixUserMenu User menu uses unordered list

--- components/Login/HeaderDropdown.js
+++ components/Login/HeaderDropdown.js
@@ -9,6 +9,13 @@ import UserNotificationsStore from '../../stores/UserNotificationsStore';
 import fetchUser from '../../actions/user/userprofile/fetchUser';
 
 class HeaderDropdown extends React.Component {
+    constructor(props){
+      super(props);
+      this.state ={
+          userMenuButtonExpanded : false
+      };
+
+    }
     componentDidMount(){
         $(this.refs.userDropDown).dropdown({action: this.onEnterAndClick.bind(this), selectOnKeydown: false});
         if(this.props.UserProfileStore.userpicture === undefined)
@@ -37,34 +44,34 @@ class HeaderDropdown extends React.Component {
         const alarmClassName = (this.props.UserNotificationsStore.newNotificationsCount > 0) ? 'alarm red icon' : 'alarm outline icon';
         const alarmIcon = (this.props.UserNotificationsStore.newNotificationsCount > 0) ? (<i className="ui small outline alarm icon" />) : '';
         return(
-            <div id="userButtonMenu" className="ui top right dropdown larger blue button" ref="userDropDown" role="button" aria-haspopup="true" aria-controls="userHeaderMenu" aria-label="User management">
+            <div id="userButtonMenu" className="ui top right dropdown larger blue button" ref="userDropDown" role="button" aria-haspopup="true" aria-controls="userHeaderMenu" aria-label="User management" aria-expanded={this.state.userMenuButtonExpanded} onClick={this.setState({userMenuButtonExpanded:true})}>
                 <div className="text">
                     <UserPicture picture={ pic } username={ this.props.UserProfileStore.username } avatar={ true } width= { 30 } />
                 </div>
                 <i className="ui big left floated aligned dropdown icon"></i>{alarmIcon}
-                <div id="userHeaderMenu" className="ui menu vertical" style={{width:'160px'}} role="menu" aria-labelledby="userButtonMenu">
+                <ul id="userHeaderMenu" className="ui menu vertical" style={{width:'160px'}} role="menu" aria-labelledby="userButtonMenu" tabIndex="-1">
                     <div className="header" role="separator" tabIndex="-1">
                         {this.props.UserProfileStore.username}
                     </div>
                     <div className="divider"  role="separator" ></div>
 
-                    <div className="item" data-value={'/user/' + this.props.UserProfileStore.username} role="menuitem" aria-label="My Decks" tabIndex="-1" >
+                    <li className="item" data-value={'/user/' + this.props.UserProfileStore.username} role="menuitem" aria-label="My Decks" tabIndex="-1" >
                         <i className="user icon link"  /> My Decks
-                    </div>
-                    <div className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/groups/overview'} role="menuitem" aria-label="My Groups" tabIndex="-1" >
+                    </li>
+                    <li className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/groups/overview'} role="menuitem" aria-label="My Groups" tabIndex="-1" >
                         <i className="icon users" /> My Groups
-                    </div>
-                    <div className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/settings/profile' } role="menuitem" aria-label="My Settings" tabIndex="-1" >
+                    </li>
+                    <li className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/settings/profile' } role="menuitem" aria-label="My Settings" tabIndex="-1" >
                         <i className="setting icon" /> My Settings
-                    </div>
-                    <div className="item" data-value={'/notifications'} role="menuitem" aria-label="My Notifications" tabIndex="-1" >
+                    </li>
+                    <li className="item" data-value={'/notifications'} role="menuitem" aria-label="My Notifications" tabIndex="-1" >
                         <i className={alarmClassName} /> My Notifications
-                    </div>
-                    <div className="item" data-value={'logout'} role="menuitem" aria-label="Sign Out" tabIndex="-1" >
+                    </li>
+                    <li className="item" data-value={'logout'} role="menuitem" aria-label="Sign Out" tabIndex="-1" >
                         <i className="sign out icon"/> Sign Out
-                    </div>
+                    </li>
 
-                </div>
+                </ul>
             </div>
         );
     }
