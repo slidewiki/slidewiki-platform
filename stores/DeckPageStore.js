787ce63f567836c318a4f0a7ce20962445014631
kadevgraaf <kadegraaf@gmail.com>
2017-11-15 21:05:05 +0100

    Revert "SWIK 1861   integrate and implement new Slide editor design "

--- stores/DeckPageStore.js
+++ stores/DeckPageStore.js
@@ -9,7 +9,6 @@ class DeckPageStore extends BaseStore {
         this.componentsStatus = {
             'NavigationPanel': {visible: 1, columnSize: 16},
             'TreePanel': {visible: 1, columnSize: 3},
-            'SlideEditPanel': {visible: 1, columnSize: 3},
             'ActivityFeedPanel': {visible: 1, columnSize: 3},
             'ContentPanel': {visible: 1, columnSize: 10},
             'ContentModulesPanel': {visible: 1, columnSize: 10}};
@@ -21,7 +20,7 @@ class DeckPageStore extends BaseStore {
         this.emitChange();
     }
     restoreAll() {
-        this.componentsStatus = {'NavigationPanel': {visible: 1, columnSize: 16}, 'TreePanel': {visible: 1, columnSize: 3}, 'SlideEditPanel': {visible: 1, columnSize: 3}, 'ActivityFeedPanel': {visible: 1, columnSize: 3}, 'ContentPanel': {visible: 1, columnSize: 10}, 'ContentModulesPanel': {visible: 1, columnSize: 10}};
+        this.componentsStatus = {'NavigationPanel': {visible: 1, columnSize: 16}, 'TreePanel': {visible: 1, columnSize: 3}, 'ActivityFeedPanel': {visible: 1, columnSize: 3}, 'ContentPanel': {visible: 1, columnSize: 10}, 'ContentModulesPanel': {visible: 1, columnSize: 10}};
         this.emitChange();
     }
     expandContentPanel() {
@@ -34,30 +33,6 @@ class DeckPageStore extends BaseStore {
                 this.componentsStatus[c].visible=0;
             }
         }
-        this.emitChange();
-    }
-    showSlideEditPanel() {
-        //hide all others than Navigation and Content
-        for(let c in this.componentsStatus){
-            if(c=== 'NavigationPanel') {
-                //this.componentsStatus[c].visible=0;
-                this.componentsStatus[c].visible=1;
-                this.componentsStatus[c].columnSize=16;
-            }else if(c=== 'SlideEditPanel'){
-                this.componentsStatus[c].visible=1;
-                this.componentsStatus[c].columnSize=3;
-            }else if(c=== 'ContentPanel'){
-                this.componentsStatus[c].visible=1;
-                this.componentsStatus[c].columnSize=10;
-            }else if(c=== 'ActivityFeedPanel'){
-                this.componentsStatus[c].visible=0;
-                this.componentsStatus[c].visible=1;
-                this.componentsStatus[c].columnSize=3;
-            }else{
-                this.componentsStatus[c].visible=0;
-            }
-        }
-
         this.emitChange();
     }
     hideLeftColumn() {
@@ -96,8 +71,7 @@ DeckPageStore.handlers = {
     'UPDATE_DECK_PAGE_CONTENT': 'updateContent',
     'EXPAND_CONTENET_PANEL': 'expandContentPanel',
     'HIDE_LEFT_COLUMN': 'hideLeftColumn',
-    'RESTORE_DECK_PAGE_LAYOUT': 'restoreAll',
-    'SHOW_SLIDE_EDIT_PANEL': 'showSlideEditPanel'
+    'RESTORE_DECK_PAGE_LAYOUT': 'restoreAll'
 };
 
 export default DeckPageStore;
