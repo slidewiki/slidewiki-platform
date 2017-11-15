d9440a45657e5d271fd39fffe149cb936397ef99
Abi James <a.james@soton.ac.uk>
2017-11-15 10:29:30 +0000

    add terms page

---
+++ components/Home/Terms.js
@@ -0,0 +1,97 @@
+import React from 'react';
+
+class license extends React.Component {
+    render() {
+        return (
+            <div className="ui text container" ref="license">
+                <div className="hidden divider"></div>
+                <h1 className="ui header">Terms of use of SlideWiki</h1>
+                <p>This is a human-readable <b>summary</b> of the Terms of Use. Disclaimer: This summary is not a part of the Terms of Use and is not a legal document. It is simply a handy reference for understanding the full terms. Think of it as the user-friendly interface to the legal language of our Terms of Use.</p>
+                <p><b>Part of our mission is to:</b></p>
+                <ul class="ui list">
+                    <li> <b>Empower and Engage</b> people around the world to collect and develop educational content and either publish it under a free license or dedicate it to the public domain.
+                    </li>
+                    <li><b>Disseminate</b>  this content effectively and globally, free of charge</li>
+                    <li> <b>Disseminate</b> this content effectively and globally, free of charge.</li>
+                </ul>
+                <p><br />
+                    <b>('You are free to'); ?></b>:
+                </p>
+                <ul class="ui list">
+                    <li> <b>Read and Print</b> our presentations and other media free of charge.'); ?>
+                    </li>
+                    <li> <b>Share and Reuse</b> our presentations and other media under free and open licenses.'); ?>
+                    </li>
+                    <li> <b>Contribute To and Edit</b> our various sites or Projects.'); ?>
+                    </li>
+                </ul>
+                <p><br />
+                    <b>'Under the following conditions'; ?></b>:
+                </p>
+                <ul class="ui list">
+                    <li> <b>Responsibility</b> &#8211; You take responsibility for your edits (since we only <i>host</i> your content)'); ?>.
+                    </li>
+                    <li> <b>Civility</b> &#8211; You support a civil environment and do not harass other users.'); ?>
+                    </li>
+                    <li> <b>Lawful Behavior</b> &#8211; You do not violate copyright or other laws.'); ?>
+                    </li>
+                    <li> <b>No Harm</b> &#8211; You do not harm our technology infrastructure.'); ?>
+                    </li>
+                    <li> <b>Terms of Use and Policies</b> &#8211; You adhere to the below Terms of Use and to the applicable community policies when you visit our sites or participate in our communities.'); ?>
+                    </li>
+                </ul>
+                <p><br />
+                    <b>'With the understanding that'); ?></b>:
+                </p>
+                <ul class="ui list">
+                    <li>
+                        'This service may contain <b>translations</b> powered by Google. 
+                        Google disclaims all warranties related to the translations, expressed or implied, including any warranties of accuracy, reliability, and any implied warranties of merchantability, fitness for a particular purpose and noninfringement.'; ?>
+                    </li>
+                    <li> '<b>You License Freely Your Contributions</b> &#8211; you generally must license your contributions and edits to our sites or Projects under a free and open license (unless your contribution is in the public domain).'); ?>
+                    </li>
+                    <li><b>No Professional Advice</b> &#8211; the content of presentations and other projects is for informational purposes only and does not constitute professional advice.
+                    </li>
+                </ul>
+    
+           <p>Text is adopted from <a href="http://wikimediafoundation.org/wiki/Terms_of_Use">Wikipedia terms of use</a> and is available under the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution/Share-Alike</a> License.'); ?>
+                </p>       
+
+            <h2 className="ui header">Creative Commons Attribution ShareAlike 4.0 CC BY-SA</h2>
+            <div className="ui right floated medium image">
+                <img alt="Creative Commons BY-SA License logo"  src="http://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-sa.png" />
+            </div>
+            <p>Decks and slides in SlideWiki are published under the Creative Common Attribution ShareAlike licence. This license lets others reuse, alter and build on slides and decks as long as the user credit the orginial author and license their new creations under identical terms. Any external works that is is based on SlideWiki content must carry the same license. This means that any derivatives will also allow for commercial use, but still need to attribute the orginal author and publish under the same license.</p>
+            <p>Find out more about the CC BY-SA and access the full license text by viewing the <a href="https://creativecommons.org/licenses/by-sa/4.0/">Human readable summary of the CC-BY-SA license from Creative Commons</a>.</p>
+            <h3 className="ui header">Why do we use the CC BY-SA license for SlideWiki decks and slides?</h3>             
+            <p>The CC BY-SA license is used by Wikipedia and similar resources, which means you can include material from Wikipedia in your decks as long as you include the original source. It also ensures that material created by SlideWiki users is always attributed to the orginal author, which we believe enhances the quality and probity of educational resources. </p>
+            <p>By using this license, everyone will be free to copy and redistribute the materials from any SlideWiki deck in any medium or format as long as they are published under the same license. SlideWiki material can also be adapted, including remixing, transforming and building on the material for any purpose, including commercial activities. </p>
+            <h3 className="ui header">Can I add any materials to my slides and decks under the CC BY-SA licence?</h3>      
+            <p>When adding content to your slides, it is your responsibility to ensure that it can be shared under a Creative Commons Attribution ShareAlike license. This means that the content you create must also have a similar license so that it can be adapted and used by anyone (including for commercial activities). Materials that are in the public domain (sometimes referred to as CC0) or licensed under Creative Commons Attribution (CC BY) can be included in your slides as well as those already publised under Creative Commons Attribution ShareAlike license (CC BY-SA).</p>
+            <h3 className="ui header">Where can I find material that I can use under this license in my decks and slides?</h3>
+            <p>As all decks and slides are published under a license that allows content to be adapted and reused, you can reuse content from any decks in your own slides. The Append Deck and Append Slides function allows you to find and attach  decks and slide as you are creating your own work.</p>
+            <p><a href="https://search.creativecommons.org">Creative Commons Search</a> lists sources of materials published under creative commons licenses. Some media services such as Flickr, YouTube and Vimeo publish some content under creative commons licenses. Content marked “All rights reserved” cannot be included in SlideWiki.</p>
+            <p>If you add content to your slides that is published under a Creative Common Attribution ShareAlike licence then you must ensure you add the attribution information into the Sources for the slide. [You can find out more about adding Sources to slides in our Guide]. Images published under a Creative Commons Attribution ShareAlike licence can be uploaded and incorporated into SlideWiki slides. However, it is your responsibility to ensure that you provide credit or attribution to the creator within the image upload dialog.</p>
+            <p> If a video or content has an Embed function provided by the video’s owner, then it is possible to add the code to embed the video into your slides. However, caution should be taken with videos that could have been posted without the copyright owner’s permission. [You can find out more about embedding videos in slides in our Guide.]</p>
+            <p>If you would like to find out more about copyright issues then you may find this article helpful: <a href="https://www.theedublogger.com/2017/01/20/copyright-fair-use-and-creative-commons/">“The Educator’s Guide To Copyright, Fair Use, And Creative Commons”</a>.
+            </p>
+            <h2 className="ui header">Creative Commons Attribution 4.0 CC BY</h2> 
+            <div className="ui right floated medium image">
+                <img alt="Creative Commons CC BY License logo"  src="http://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png" />
+            </div>
+            <p>This license allows materials to be copied and redistributed in any medium or format as long as they are attributed to the original creator and copyright notices. Images published under a Creative Commons Attribution licence can be uploaded and incorporated into SlideWiki slides. However, it is your responsibility to ensure that you provide credit or attribution to the creator within the image upload dialog.</p>
+            <p>Find out more about the CC BY and access the full license text by viewing the <a href="https://creativecommons.org/licenses/by/4.0/">Human readable summary of the CC BY license from Creative Commons</a>.</p>
+            <h2 className="ui header">Work in the Public Domain, free of copyright or CC0</h2> 
+            <div className="ui right floated medium image">
+                <img alt="Creative Commons CC 0 public domain License logo"  src="http://mirrors.creativecommons.org/presskit/buttons/88x31/png/cc-zero.png" />
+            </div>
+            <p>Work in the Public domain is free of copyright restrictions. This is usually for works that are free from copyright restrictions. This includes works where the copyright has expired or the creator has assigned their work to the public domain. While it is not always necessary to attribute the source of public domain work. However, it is good practice to attribute works to the creator or source to add provienance to your decks and slides. CCO is the Creative Commons license that enables authors and creators to waive their rights to their work and place it in the public domain. </p>
+            <h2 className="ui header">Notices</h2>
+            <p>The SlideWiki website and its content are provided "as is". We offer no warranties, explicit or implied regarding any content, the webiste or the accuracy of any information.  These license may not give you all of the permissions necessary for your intended use. For example, other rights such as publicity, privacy, or moral rights may limit how you use the material. We reserve the right to remove materials and content that we believe to infringe copyright and license requirements.</p>
+            </div>
+
+        );
+    }
+}
+
+export default license;
