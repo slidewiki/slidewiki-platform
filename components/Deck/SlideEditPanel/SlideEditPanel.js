76412bf1a58bf0c0e3fe7f39e872415d0e99be6a
kadevgraaf <kadegraaf@gmail.com>
2017-11-15 20:38:37 +0100

    comment out title edit button

--- components/Deck/SlideEditPanel/SlideEditPanel.js
+++ components/Deck/SlideEditPanel/SlideEditPanel.js
@@ -16,9 +16,12 @@ class SlideEditPanel extends React.Component {
         return (
             <div className="ui container" ref="treePanel" role="navigation" onFocus={this.handleFocus} onBlur={this.handleBlur}>
                 <NavigationPanel />
-                <div className="four wide column">
+                {/*
+                    give mode='edit' to navigationpane
+                    <div className="four wide column">
                     <button className="ui small basic right floated icon button"><i className="edit icon"></i></button>
                 </div>
+                */}
                 <div className="ui grey inverted segment bottom attached active tab">
                   <div className="ui center aligned grid">
                       <div className="ui vertical labeled icon grey inverted massive  menu">
