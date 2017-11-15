b896c7b05706d5ac16bd515f8a14ac43843ad37f
kadevgraaf <kadegraaf@gmail.com>
2017-11-15 20:27:09 +0100

    remove unnecessary call

--- actions/deckpagelayout/showSlideEditPanel.js
+++
@@ -1,7 +0,0 @@
-const log = require('../log/clog');
-
-export default function showSlideEditPanel(context, payload, done) {
-    log.info(context);
-    context.dispatch('SHOW_SLIDE_EDIT_PANEL', payload);
-    done();
-}
