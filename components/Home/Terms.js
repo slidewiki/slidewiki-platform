25a0c0ee304e71930f2a2f47cb1cde7fc4b36de8
Abi James <a.james@soton.ac.uk>
2017-11-20 18:38:22 +0000

    edit terms

--- components/Home/Terms.js
+++ components/Home/Terms.js
@@ -6,14 +6,14 @@ class terms extends React.Component {
             <div className="ui text container" ref="terms">
                 <div className="hidden divider"></div>
                 <h1 className="ui header">Terms of use of SlideWiki</h1>
-                <p>This is a human-readable <b>summary</b> of the Terms of Use.</p>
+                <p>This is a human-readable <b>summary</b> of the Terms of Use for SlideWiki (the project).</p>
                 <p>Disclaimer: This summary is not a part of the Terms of Use and is not a legal document. It is simply a handy reference for understanding the full terms. Think of it as the user-friendly interface to the legal language of our Terms of Use.</p>
                 <h2 className="ui header">Part of our mission is to:</h2>
                 <ul className="ui list">
-                    <li> <b>Empower and Engage</b> people around the world to collect and develop educational content and either publish it under a free license or dedicate it to the public domain.
+                    <li> <b>Empower and engage</b> people around the world to collect and develop educational content and either publish it under a free license or dedicate it to the public domain.
                     </li>
                     <li><b>Disseminate</b>  this content effectively and globally, free of charge</li>
-                    <li> <b>Disseminate</b> this content effectively and globally, free of charge.</li>
+    
                 </ul>
                 <h2 className="ui header">You are free to:</h2>
                 <ul className="ui list">
@@ -21,7 +21,7 @@ class terms extends React.Component {
                     </li>
                     <li> <b>Share and Reuse</b> our presentations and other media under free and open licenses.
                     </li>
-                    <li> <b>Contribute To and Edit</b> our various sites or Projects.
+                    <li> <b>Contribute To and Edit</b> our various sites or projects.
                     </li>
                 </ul>
                 <h2 className="ui header">Under the following conditions</h2>
@@ -30,21 +30,21 @@ class terms extends React.Component {
                     </li>
                     <li> <b>Civility</b> &#8211; You support a civil environment and do not harass other users.
                     </li>
-                    <li> <b>Lawful Behavior</b> &#8211; You do not violate copyright or other laws.
+                    <li> <b>Lawful behaviour</b> &#8211; You do not violate copyright or other laws.
                     </li>
                     <li> <b>No Harm</b> &#8211; You do not harm our technology infrastructure.
                     </li>
-                    <li> <b>Terms of Use and Policies</b> &#8211; You adhere to the below Terms of Use and to the applicable community policies when you visit our sites or participate in our communities.
+                    <li> <b>Terms of Use and Policies</b> &#8211; You adhere to the Terms of Use and to the applicable community policies when you visit our sites or participate in our communities.
                     </li>
                 </ul>
                 <h2 className="ui header">With the understanding that</h2>
 
                 <ul className="ui list">
-                    <li>This service may contain <b>translations</b> powered by Microsoft translate. We disclaims all warranties related to the translations, expressed or implied, including any warranties of accuracy, reliability, and any implied warranties of merchantability, fitness for a particular purpose and noninfringement.
+                    <li>This service may contain <b>translations</b> powered by thir party sercies. Selecting to use the translate service will result in data being sent to third-party services. We disclaims all warranties related to the translations, expressed or implied, including any warranties of accuracy, reliability, and any implied warranties of merchantability, fitness for a particular purpose and noninfringement.
                     </li>
-                    <li> <b>You License Freely Your Contributions</b> &#8211; you generally must license your contributions and edits to our sites or Projects under a free and open license (unless your contribution is in the public domain).
+                    <li> <b>You license freely your contributions</b> &#8211; you generally must license your contributions and edits to our sites or projects under a free and open license (unless your contribution is in the public domain).
                     </li>
-                    <li><b>No Professional Advice</b> &#8211; the content of presentations and other projects is for informational purposes only and does not constitute professional advice.
+                    <li><b>No professional advice</b> &#8211; the content of presentations and other projects is for informational purposes only and does not constitute professional advice.
                     </li>
                 </ul>
 
@@ -60,7 +60,7 @@ class terms extends React.Component {
                 <h3 className="ui header">Violating the Privacy of Others</h3>
                 <ul className="ui list">
                     <li>Infringing the privacy rights of others under the laws of Germany or other applicable laws (which may include the laws where you live or where you view or edit content);</li>
-                    <li>Soliciting personally identifiable information for purposes of harassment, exploitation, violation of privacy, or any promotional or commercial purpose not explicitly approved by the Project; and</li>
+                    <li>Soliciting personally identifiable information for purposes of harassment, exploitation, violation of privacy, or any promotional or commercial purpose not explicitly approved by the project; and</li>
                     <li>Soliciting personally identifiable information from anyone under the age of 18 for an illegal purpose or violating any applicable law regarding the health or well-being of minors.</li>
                 </ul>
                 <h3 className="ui header">Engaging in False Statements, Impersonation, or Fraud</h3>
@@ -84,21 +84,21 @@ class terms extends React.Component {
                 <h3 className="ui header">Engaging in Disruptive and Illegal Misuse of Facilities</h3>  
                 <ul className="ui list">
                     <li>Posting or distributing content that contains any viruses, malware, worms, Trojan horses, malicious code, or other device that could harm our technical infrastructure or system or that of our users;</li>
-                    <li>Engaging in automated uses of the site that are abusive or disruptive of the services and have not been approved by the Wikimedia community;</li>
-                    <li>Disrupting the services by placing an undue burden on a Project website or the networks or servers connected with a Project website;</li>
-                    <li>Disrupting the services by inundating any of the Project websites with communications or other traffic that suggests no serious intent to use the Project website for its stated purpose;</li>
+                    <li>Engaging in automated uses of the site that are abusive or disruptive of the services and have not been approved by the project community;</li>
+                    <li>Disrupting the services by placing an undue burden on a project website or the networks or servers connected with a project website;</li>
+                    <li>Disrupting the services by inundating any of the project websites with communications or other traffic that suggests no serious intent to use the project website for its stated purpose;</li>
                     <li>Knowingly accessing, tampering with, or using any of our non-public areas in our computer systems without authorization; and</li>
                     <li>Probing, scanning, or testing the vulnerability of any of our technical systems or networks unless all the following conditions are met:</li>
                     <ul className="ui list">
                         <li>such actions do not unduly abuse or disrupt our technical systems or networks;</li>
                         <li>such actions are not for personal gain (except for credit for your work);</li>
-                        <li>you report any vulnerabilities to MediaWiki developers (or fix it yourself); and</li>
+                        <li>you report any vulnerabilities to project's developers (or fix it yourself); and</li>
                         <li>you do not undertake such actions with malicious or destructive intent.</li>
                     </ul>
                 </ul>
 
-                <h3 className="ui header">Compatibility with Wikipedia terms of use</h3>  
-                <p>Our terms are adopted from <a href="http://wikimediafoundation.org/wiki/Terms_of_Use">Wikipedia terms of use</a> which is available under the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution/Share-Alike</a> License.
+                <h3 className="ui header">Compatibility with Wikipedia's terms of use</h3>  
+                <p>Our terms are adapted from the <a href="http://wikimediafoundation.org/wiki/Terms_of_Use">Wikipedia terms of use</a>, which are available under the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution/Share-Alike</a> License.
                 </p>
                 <p>For full terms and conditions please read our Imprint page.</p>
             </div>
