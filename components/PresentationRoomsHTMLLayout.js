bd502007c4672cd8508f0d7e4c85d54a3bce658a
Roy Meissner <meissner@informatik.uni-leipzig.de>
2017-11-21 11:23:02 +0100

    Readded feedback button, added a warning to the welcome modals

--- components/PresentationRoomsHTMLLayout.js
+++ components/PresentationRoomsHTMLLayout.js
@@ -44,6 +44,7 @@ class PresentationRoomsHTMLLayout extends React.Component {
                 {/* Main app bundle */}
                 <script src={'/public/js/' + this.props.clientFile} defer></script>
                 <script src="/sweetalert2/dist/sweetalert2.min.js" defer></script>
+                <script type="text/javascript" src="https://slidewiki.atlassian.net/s/5e2fc7b2a8ba40bc00a09a4f81a301c8-T/rfg5q6/100012/c/1000.0.9/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-UK&collectorId=241c9e18" defer></script>
                 <script dangerouslySetInnerHTML={ {__html: ga} } />
             </body>
             </html>
