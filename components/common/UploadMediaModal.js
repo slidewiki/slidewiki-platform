d16f278ebaf80147b76cde985ab30012e06fc29a
kadevgraaf <kadegraaf@gmail.com>
2017-11-23 14:23:10 +0100

    Add labels, button for keyboard access, rename input box to text box

--- components/common/UploadMediaModal.js
+++ components/common/UploadMediaModal.js
@@ -143,7 +143,7 @@ class UploadMediaModal extends React.Component {
             dropzone = <div className="dropzone">
               <Dropzone onDrop={this.onDrop.bind(this)} accept="image/*" multiple={false} className="ui grey inverted center aligned padded raised segment">
                 <Icon name="cloud upload" size="massive"/>
-                <p>Try drop a file here, or click to select a file to upload.</p>
+                <p>Drop a file directly from your filebrowser here to upload it.</p><p>Alternatively, click <button id="upload" className="ui button" aria-label='select file to upload'><i className="outline upload icon large black"></i><label htmlFor="upload">choose file</label></button> or anywhere around this text to select a file to upload.</p>
               </Dropzone>
             </div>;
         } else { //TODO Implement a switch-case statement for other media files. Currently only works for images.
@@ -166,28 +166,28 @@ class UploadMediaModal extends React.Component {
         let submitButtonIcon = 'arrow right';
         if(this.state.license){
             heading = 'License information';
-            licenseBoxes = (this.state.licenseValue !== 'CC0') ? <div className="required field"><label>Image created by/ attributed to:</label><Input ref="copyrightHolder" name="copyrightHolder" onChange={this.handleChange.bind(this)} required defaultValue={this.props.userFullName}/></div> : '';
+            licenseBoxes = (this.state.licenseValue !== 'CC0') ? <div className="required field"><label htmlFor="copyrightHolder">Image created by/ attributed to:</label><Input id="copyrightHolder" aria-required="true" ref="copyrightHolder" name="copyrightHolder" onChange={this.handleChange.bind(this)} required defaultValue={this.props.userFullName}/></div> : '';
             content = <div>
               <Image src={this.state.files[0].preview} size="large" centered={true}/>
               <Divider/>
               <form className="ui form" onSubmit={this.submitPressed.bind(this)}>
                 <div className="required field">
-                  <label>Title:</label>
-                  <Input defaultValue={this.state.files[0].name} ref="mediaTitle" name="title" onChange={this.handleChange.bind(this)} required/>
+                  <label htmlFor="title">Title:</label>
+                  <Input aria-required="true" id="title" defaultValue={this.state.files[0].name} ref="mediaTitle" name="title" onChange={this.handleChange.bind(this)} required/>
                 </div>
                 <div className="required field">
-                  <label>Description/Alt Text:</label>
-                  <Popup trigger={<input ref="mediaAltText" id="UploadMediaModal_input_mediaAltText" name="alt" onChange={this.handleChange.bind(this)} required/>} content='What does the picture mean?' position='top center'/>
+                  <label htmlFor="UploadMediaModal_input_mediaAltText">Description/Alt Text:</label>
+                  <Popup trigger={<input ref="mediaAltText" aria-required="true" id="UploadMediaModal_input_mediaAltText" name="alt" onChange={this.handleChange.bind(this)} required/>} content='What does the picture mean?' position='top center'/>
                 </div>
                 <div className="required field">
-                  <label>Choose a license:</label>
-                  <Dropdown selection options={[{text: 'CC0 Public Domain', value: 'CC0'},{text: 'CC-BY Creative Commons Attribution 4.0', value: 'CC BY 4.0'},{text: 'CC-BY-SA Creative Common Attribution Share-Alike 4.0', value: 'CC BY SA 4.0'}]} defaultValue='CC0' onChange={this.changeLicense.bind(this)} ref="mediaLicense" required/>
+                  <label htmlFor="medialicense">Choose a license:</label>
+                  <Dropdown aria-required="true" id="medialicense" selection options={[{text: 'CC0 Public Domain', value: 'CC0'},{text: 'CC-BY Creative Commons Attribution 4.0', value: 'CC BY 4.0'},{text: 'CC-BY-SA Creative Common Attribution Share-Alike 4.0', value: 'CC BY SA 4.0'}]} defaultValue='CC0' onChange={this.changeLicense.bind(this)} ref="mediaLicense" required/>
                 </div>
                 {licenseBoxes}
                 <div className="required field">
                   <div className="ui checkbox">
-                    <input type="checkbox" required/>
-                    <label>I confirm that I have the rights to upload this image as per the SlideWiki <a href="/imprint">terms and conditions</a> and that the <a href="/license">license information</a> I have provided is correct.</label>{/*TODO Add a link to the slidewiki terms/cond site, currently not exising*/}
+                    <input type="checkbox" aria-required="true" htmlFor="confirm" required/>
+                    <label htmlFor="confirm">I confirm that I have the rights to upload this image as per the SlideWiki <a href="/imprint">terms and conditions</a> and that the <a href="/license">license information</a> I have provided is correct.</label>{/*TODO Add a link to the slidewiki terms/cond site, currently not exising*/}
                   </div>
                 </div>
                 <Button type='submit' id="UploadFormSubmitButton" style={{display: 'none'}}>Submit</Button> {/*black magic hack to trigger the form from the outside*/}
@@ -205,7 +205,7 @@ class UploadMediaModal extends React.Component {
         return (
           <Modal trigger={
                   <Button className="ui orange button" tabIndex='0' id="ChangePictureModalOpenButton" aria-hidden={this.state.modalOpen} onClick={this.handleOpen} value="">
-                    <i className="outline upload icon black"></i>
+                    <i className="outline upload icon large black"></i>
                     <a style={buttonColorBlack}>Add Image</a>
                   </Button>
                  }
