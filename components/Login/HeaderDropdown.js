0fbdeb62310738b8c8f144e5a7b30dc6712ed20a
svalero <svalero@dsic.upv.es>
2017-12-01 15:19:45 +0100

    SWIK-1500-fixUserMenu tabindex=0 in the menu items from user menu.

--- components/Login/HeaderDropdown.js
+++ components/Login/HeaderDropdown.js
@@ -70,9 +70,9 @@ class HeaderDropdown extends React.Component {
         const alarmIcon = (this.props.UserNotificationsStore.newNotificationsCount > 0) ? (<i className="ui small outline alarm icon" />) : '';
         return(
               <div id="userButtonMenu" className="ui top right dropdown larger blue button" ref={(drop) => {this.userDropDown=drop;}}  aria-haspopup="true" aria-controls="userHeaderMenu" aria-label="User management" aria-expanded={this.state.userMenuButtonExpanded}>
-                <div className="text">
+                {/*<div className="text">*/}
                     <UserPicture picture={ pic } username={ this.props.UserProfileStore.username } avatar={ true } width= { 30 } />
-                </div>
+                {/*</div>*/}
 
                 {/*<i className="ui big left floated aligned dropdown icon"></i>{alarmIcon}*/}
                 <ul id="userHeaderMenu" className="ui menu vertical" style={{width:'160px'}} role="menu" aria-labelledby="userButtonMenu" tabIndex="0">
@@ -82,19 +82,19 @@ class HeaderDropdown extends React.Component {
                     </li>
                     <li className="divider"  role="separator" tabIndex="-1" ></li>
                      */}
-                    <li id="myDecksMenuItem" className="item" data-value={'/user/' + this.props.UserProfileStore.username} role="menuitem" aria-label="My Decks" tabIndex="-1" >
+                    <li id="myDecksMenuItem" className="item" data-value={'/user/' + this.props.UserProfileStore.username} role="menuitem" aria-label="My Decks" tabIndex="0" >
                         <i className="user icon link"  /> My Decks
                     </li>
-                    <li className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/groups/overview'} role="menuitem" aria-label="My Groups" tabIndex="-1" >
+                    <li className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/groups/overview'} role="menuitem" aria-label="My Groups" tabIndex="0" >
                         <i className="icon users" /> My Groups
                     </li>
-                    <li className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/settings/profile' } role="menuitem" aria-label="My Settings" tabIndex="-1" >
+                    <li className="item" data-value={'/user/' + this.props.UserProfileStore.username + '/settings/profile' } role="menuitem" aria-label="My Settings" tabIndex="0" >
                         <i className="setting icon" /> My Settings
                     </li>
-                    <li className="item" data-value={'/notifications'} role="menuitem" aria-label="My Notifications" tabIndex="-1" >
+                    <li className="item" data-value={'/notifications'} role="menuitem" aria-label="My Notifications" tabIndex="0" >
                         <i className={alarmClassName} /> My Notifications
                     </li>
-                    <li className="item" data-value={'logout'} role="menuitem" aria-label="Sign Out" tabIndex="-1" >
+                    <li className="item" data-value={'logout'} role="menuitem" aria-label="Sign Out" tabIndex="0" >
                         <i className="sign out icon"/> Sign Out
                     </li>
 
