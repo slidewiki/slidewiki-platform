8a1bcd5047677053e1a452adb41800b4bf040ec3
Kostis Pristouris <kprist@gmail.com>
2017-12-20 16:45:11 +0200

    Temporarily override default microservices for testing

--- configs/microservices.sample.js
+++ configs/microservices.sample.js
@@ -35,7 +35,9 @@ STEP 3: Verify that in STEP 2, you have correct operation mentioned. If you see 
 export default {
     Microservices: {
         'deck': {
-            uri: 'https://deckservice.experimental.slidewiki.org'
+            // NOTE remove before merging
+            uri: 'http://slidewiki.imis.athena-innovation.gr/deckservice'
+            // uri: 'https://deckservice.experimental.slidewiki.org'
         },
         'discussion': {
             uri: 'https://discussionservice.experimental.slidewiki.org'
