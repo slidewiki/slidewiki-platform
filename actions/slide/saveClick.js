2dd382da99f7df855f94e1f968424e96dc9182a6
svalero <svalero@dsic.upv.es>
2017-12-26 13:55:36 +0100

    SWIK-1133 debug comment on saveClick action

--- actions/slide/saveClick.js
+++ actions/slide/saveClick.js
@@ -3,5 +3,6 @@ import serviceUnavailable from '../error/serviceUnavailable';
 const log = require('../log/clog');
 
 export default function saveClick(context, payload, done) {
+    console.log('SAVE_SLIDE_CLICK')
     context.dispatch('SAVE_SLIDE_CLICK', {});
 }
