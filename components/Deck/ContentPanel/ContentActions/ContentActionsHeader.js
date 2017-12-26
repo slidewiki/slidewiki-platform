a68447c6146f1bfa2638ae99c40184b686351d9a
svalero <svalero@dsic.upv.es>
2017-12-26 12:46:51 +0100

    SWIK-1133: component uses intl for button texts

--- components/Deck/ContentPanel/ContentActions/ContentActionsHeader.js
+++ components/Deck/ContentPanel/ContentActions/ContentActionsHeader.js
@@ -16,12 +16,50 @@ import saveClick from '../../../../actions/slide/saveClick';
 import cancelClick from '../../../../actions/slide/cancelClick';
 import undoClick from '../../../../actions/slide/undoClick';
 import redoClick from '../../../../actions/slide/redoClick';
+import {defineMessages} from 'react-intl';
 
 class ContentActionsHeader extends React.Component {
+    constructor(props){
+        super(props);
+        this.messages = defineMessages({
+            viewButtonText:{
+                id: 'ContentActionsHeader.viewButtonText',
+                defaultMessage:'View'
+            },
+            viewButtonAriaText:{
+                id: 'ContentActionsHeader.viewButtonAriaText',
+                defaultMessage:'View Mode'
+            },
+            editButtonText:{
+                id: 'ContentActionsHeader.editButtonText',
+                defaultMessage:'Edit'
+            },
+            editButtonAriaText:{
+                id: 'ContentActionsHeader.editButtonAriaText',
+                defaultMessage:'Edit Mode'
+            },
+            addSlideButtonAriaText:{
+                id: 'ContentActionsHeader.addSlideButtonAriaText',
+                defaultMessage:'Add Slide'
+            },
+            addDeckButtonAriaText:{
+                id: 'ContentActionsHeader.addDeckButtonAriaText',
+                defaultMessage:'Add Deck'
+            },
+            duplicateAriaText:{
+                id: 'ContentActionsHeader.duplicateAriaText',
+                defaultMessage:'Duplicate'
+            },
+            deleteAriaText:{
+                id: 'ContentActionsHeader.deleteAriaText',
+                defaultMessage:'Delete'
+            },
 
-    componentDidUpdate(){
+        });
 
     }
+
+
     handleAddNode(selector, nodeSpec) {
         //selector: Object {id: "56", stype: "deck", sid: 67, spath: "67:2"}
         //nodeSec: Object {type: "slide", id: "0"}
@@ -147,25 +185,25 @@ class ContentActionsHeader extends React.Component {
 
 
         }
-      
+
         return (
             <div className="ui top attached menu" role="menu">
                 <button className={viewClass} onClick={this.handleViewButton.bind(this,selector)}
                   type="button"
-                  aria-label="View Mode"
-                  data-tooltip="View Mode"
+                  aria-label={this.context.intl.formatMessage(this.messages.viewButtonAriaText)}
+                  data-tooltip={this.context.intl.formatMessage(this.messages.viewButtonAriaText)}
                   tabIndex = {contentDetails.mode ==='edit'?0:-1}
                 >
-                  <i className="blue large unhide icon"></i>View
+                  <i className="blue large unhide icon"></i>{this.context.intl.formatMessage(this.messages.viewButtonText)}
                 </button>
                 {this.props.UserProfileStore.username === '' ? '' : /* Edit button only visible if logged user*/
                 <button className={editClass} onClick={this.handleEditButton.bind(this,selector)}
                   type="button"
-                  aria-label="Edit Mode"
-                  data-tooltip="Edit Mode"
+                  aria-label={this.context.intl.formatMessage(this.messages.editButtonAriaText)}
+                  data-tooltip={this.context.intl.formatMessage(this.messages.editButtonAriaText)}
                   tabIndex = {contentDetails.mode ==='edit'?-1:0}
                 >
-                  <i className="ui large blue edit icon "></i>Edit
+                  <i className="ui large blue edit icon "></i>{this.context.intl.formatMessage(this.messages.editButtonText)}
                 </button>
                 }
                 {saveButton}
@@ -177,8 +215,8 @@ class ContentActionsHeader extends React.Component {
                     <div className="right menu">
                         <button className={addSlideClass} onClick={this.handleAddNode.bind(this, selector, {type: 'slide', id: '0'}) }
                           type="button"
-                          aria-label="Add Slide"
-                          data-tooltip="Add Slide"
+                          aria-label={this.context.intl.formatMessage(this.messages.addSlideButtonAriaText)}
+                          data-tooltip={this.context.intl.formatMessage(this.messages.addSlideButtonAriaText)}
                           tabIndex={this.props.PermissionsStore.permissions.readOnly || !this.props.PermissionsStore.permissions.edit || contentDetails.mode ==='edit'?-1:0}>
                             <i className="icons">
                               <i className="grey file large text icon"></i>
@@ -187,32 +225,32 @@ class ContentActionsHeader extends React.Component {
 
                         </button>
                         <AttachSlides buttonStyle={buttonStyle} selector={selector} />
-                          <button className={addDeckClass} onClick={this.handleAddNode.bind(this, selector, {type: 'deck', id: '0'})}
+                        <button className={addDeckClass} onClick={this.handleAddNode.bind(this, selector, {type: 'deck', id: '0'})}
                            type="button"
-                           aria-label="Add Deck"
-                           data-tooltip="Add Deck"
+                           aria-label={this.context.intl.formatMessage(this.messages.addDeckButtonAriaText)}
+                           data-tooltip={this.context.intl.formatMessage(this.messages.addDeckButtonAriaText)}
                            tabIndex={this.props.PermissionsStore.permissions.readOnly || !this.props.PermissionsStore.permissions.edit || contentDetails.mode ==='edit'?-1:0}>
                               <i className="medium icons">
                                 <i className="yellow large folder icon"></i>
                                 <i className="inverted corner plus icon"></i>
                               </i>
-                          </button>
-                          <AttachSubdeck buttonStyle={buttonStyle} selector={selector} />
-                          <button className={duplicateItemClass} onClick={this.handleAddNode.bind(this, selector, {type: selector.stype, id: selector.sid})}
+                        </button>
+                        <AttachSubdeck buttonStyle={buttonStyle} selector={selector} />
+                        <button className={duplicateItemClass} onClick={this.handleAddNode.bind(this, selector, {type: selector.stype, id: selector.sid})}
                            type="button"
-                           aria-label="Duplicate"
-                           data-tooltip="Duplicate"
+                           aria-label={this.context.intl.formatMessage(this.messages.duplicateAriaText)}
+                           data-tooltip={this.context.intl.formatMessage(this.messages.duplicateAriaText)}
                            tabIndex={contentDetails.selector.id === contentDetails.selector.sid || contentDetails.selector.stype==='deck' || this.props.PermissionsStore.permissions.readOnly || !this.props.PermissionsStore.permissions.edit || contentDetails.mode ==='edit'?-1:0}>
                               <i className="grey large copy icon"></i>
 
-                          </button>
-                          <button className={deleteItemClass} onClick={this.handleDeleteNode.bind(this, selector)}
+                        </button>
+                        <button className={deleteItemClass} onClick={this.handleDeleteNode.bind(this, selector)}
                             type="button"
-                            aria-label="Delete"
-                            data-tooltip="Delete"
+                            aria-label={this.context.intl.formatMessage(this.messages.deleteAriaText)}
+                            data-tooltip={this.context.intl.formatMessage(this.messages.deleteAriaText)}
                             tabIndex={this.props.PermissionsStore.permissions.readOnly || !this.props.PermissionsStore.permissions.edit || contentDetails.mode ==='edit'?-1:0}>
                               <i className="red large trash icon"></i>
-                          </button>
+                        </button>
                           {/*
                           <button className="item ui small basic right attached disabled button">
                               <a className="" title="Settings">
@@ -227,7 +265,8 @@ class ContentActionsHeader extends React.Component {
     }
   }
 ContentActionsHeader.contextTypes = {
-    executeAction: React.PropTypes.func.isRequired
+    executeAction: React.PropTypes.func.isRequired,
+    intl: React.PropTypes.object.isRequired
 };
 //it should listen to decktree store in order to handle adding slides/decks
 ContentActionsHeader = connectToStores(ContentActionsHeader, [DeckTreeStore, UserProfileStore, PermissionsStore, ContentStore], (context, props) => {
