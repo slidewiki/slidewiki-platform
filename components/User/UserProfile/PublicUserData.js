f33ae1a492b16221c2343dab1748b591256d2446
Abi James <a.james@soton.ac.uk>
2018-01-04 16:33:42 +0000

    improved styling

--- components/User/UserProfile/PublicUserData.js
+++ components/User/UserProfile/PublicUserData.js
@@ -10,22 +10,49 @@ class PublicUserData extends React.Component {
             <div>
                 <UserPicture picture={ this.props.user.picture } username={ this.props.user.uname } link={ false } private={ false } width={ 150 } centered={ false } size={ 'small' }/>
                 <h2>{ this.props.user.fname } { this.props.user.lname }</h2>
-                <div className="ui medium header">
-                    <i className="icon user" aira-label="user name"></i>
-                    { this.props.user.uname }
-                    <strong>     </strong>
-                    {(this.props.user.uname === this.props.loggedinuser) ? (
-                      <NavLink className="basic ui icon button" href={ '/user/' + this.props.user.uname + '/settings/profile' } role="button" aria-label="open my settings">
-                        <i className="setting icon" aria-hidden="true"/>
-                      </NavLink>
-                      ) : ''}
+                <div className="ui item">
+                    <div className="item">
+                        <div className="content">
+                            <div className="header">
+                                <i className="icon user" aira-label="user name"></i>
+                                { this.props.user.uname }
+                            </div>
+                            <div className="description">
+                                { !isEmpty(this.props.user.description) ? <p>{ this.props.user.description }</p> : '' }
+                            </div>
+                            <div className="extra">
+                                {(this.props.user.uname === this.props.loggedinuser) ? (
+                                    <div className="ui right floated basic setting icon button">
+                                        <i className="setting icon" aria-hidden="true"/>
+                                        <NavLink href={ '/user/' + this.props.user.uname + '/settings/profile' } role="button" aria-label="open my settings">
+                                        </NavLink>
+                                    </div>
+                                ) : ''}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                { !isEmpty(this.props.user.description) ? <p>{ this.props.user.description }</p> : '' }
                 <div className = "ui divider" />
-                { !isEmpty(this.props.user.organization) ? <div><i className="user circle outline icon" aria-label="organisation"/> { this.props.user.organization }<br/><br/></div> : '' }
-                { !isEmpty(this.props.user.country) ? <div><i className="marker icon" aria-label="country"/> { this.props.user.country }<br/><br/></div> : '' }
-                { !isEmpty(this.props.user.website) ? <div><i className="globe icon" aria-label="website"/> { this.props.user.website }<br/><br/></div> : '' }
-                { !isEmpty(this.props.user.joined) ? <div><i className="clock icon" aria-label="user since"/> { this.props.user.joined }<br/><br/></div> : '' }
+                <div className="ui list">
+                    { !isEmpty(this.props.user.organization) ? 
+                        <div className="item">
+                            <i className="user circle outline icon" aria-label="organisation"/> { this.props.user.organization }</div> 
+                        : '' }
+                    { !isEmpty(this.props.user.country) ? 
+                        <div classname="item">
+                            <i className="marker icon" aria-label="country"/> { this.props.user.country }
+                        </div> 
+                        : '' }
+                    { !isEmpty(this.props.user.website) ? 
+                        <div className="item">
+                            <i className="globe icon" aria-label="website"/> { this.props.user.website }
+                        </div> 
+                        : '' }
+                    { !isEmpty(this.props.user.joined) ? <div className="item">
+                        <i className="clock icon" aria-label="user since"/> { this.props.user.joined }
+                    </div> 
+                    : '' }
+                </div>
                 <div className = "ui divider" />
             </div>
         );
