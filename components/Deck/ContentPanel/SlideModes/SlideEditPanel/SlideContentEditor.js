66b033ba0df80a8d7b338c764612dc036e33574c
kadevgraaf <kadegraaf@gmail.com>
2018-01-09 19:57:08 +0100

    fix all resize

--- components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideContentEditor.js
+++ components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideContentEditor.js
@@ -855,18 +855,18 @@ class SlideContentEditor extends React.Component {
             {
                 if($(this).width() < $(this).find('img:first').width())
                 { //check if box width is smaller than iframe/image width/height
-                    $(this).width($(this).find('img:first').width());
+                    $(this).width($(this).find('img:first').width() + 8);
                 //    console.log('adjust iframe width');
                 } else if ($(this).width() < $(this).find('img:first').attr('width'))
                 {
-                    $(this).width($(this).find('img:first').attr('width'));
+                    $(this).width($(this).find('img:first').attr('width') + 8);
                 }
                 if($(this).height() < $(this).find('img:first').height())
                 { //check if box height is smaller than iframe/image width/height
-                    $(this).height($(this).find('img:first').height());
+                    $(this).height($(this).find('img:first').height() + 8);
                 } else if ($(this).height() < $(this).find('img:first').attr('height'))
                 {
-                    $(this).height($(this).find('img:first').attr('height'));
+                    $(this).height($(this).find('img:first').attr('height') + 8);
                 }
             }
         });
