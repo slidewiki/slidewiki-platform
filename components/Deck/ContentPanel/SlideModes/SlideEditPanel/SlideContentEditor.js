2c99019076962c6b6a056f0b56fb791aa3974908
kadevgraaf <kadegraaf@gmail.com>
2018-01-18 14:33:58 +0100

    Fixes to to prevent Firefox caret from resetting after adding new div elelements (input box, template, table)

--- components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideContentEditor.js
+++ components/Deck/ContentPanel/SlideModes/SlideEditPanel/SlideContentEditor.js
@@ -414,6 +414,10 @@ class SlideContentEditor extends React.Component {
                 '</div>';
                 break;
         }
+        //fix to prevent Firefox caret from resetting
+        $('.pptx2html [style*="absolute"]').on('mouseup', (evt) => {
+            CKEDITOR.instances.inlineContent.getSelection().unlock();
+        });
         this.emitChange(); //confirm non-save on-leave
         //this.addBorders();
         this.uniqueIDAllElements();
@@ -458,6 +462,10 @@ class SlideContentEditor extends React.Component {
                     }, 500);
                 });
             }
+            //fix to prevent Firefox caret from resetting
+            $('.pptx2html [style*="absolute"]').on('mouseup', (evt) => {
+                CKEDITOR.instances.inlineContent.getSelection().unlock();
+            });
             //ugly fix for SWIK-1348- Image dialog not appearing once image added to slide
             $('.cke_button__image_icon').mousedown((evt) => { //detect click on image dialog button
                 console.log('====ckeditor image dialog onclick====');
@@ -638,6 +646,10 @@ class SlideContentEditor extends React.Component {
             $('.pptx2html').append(this.getAbsoluteDiv(this.getHighestZIndex() + 10));
             //.css({'borderStyle': 'dashed dashed dashed dashed', 'borderColor': '#33cc33'});
             this.emitChange(); //confirm non-save on-leave
+            //fix to prevent Firefox caret from resetting
+            $('.pptx2html [style*="absolute"]').on('mouseup', (evt) => {
+                CKEDITOR.instances.inlineContent.getSelection().unlock();
+            });
             //this.uniqueIDAllElements();
             this.resizeDrag();
             //this.forceUpdate();
@@ -1580,6 +1592,10 @@ class SlideContentEditor extends React.Component {
             {
                 let uniqueID = this.getuniqueID();
                 $('.pptx2html').append('<div id="'+uniqueID+'" style="position: absolute; width: 400px; height:300px; top: 150px; left: 200px; z-index: '+(this.getHighestZIndex() + 10)+';"><span>&nbsp;</span></div>');
+                //fix to prevent Firefox caret from resetting
+                $('.pptx2html [style*="absolute"]').on('mouseup', (evt) => {
+                    CKEDITOR.instances.inlineContent.getSelection().unlock();
+                });
                 this.resizeDrag();
                 this.placeCaretAtStart(uniqueID);
                 $('#'+uniqueID).focus();
