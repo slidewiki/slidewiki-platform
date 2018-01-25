d9457289c9898d3b65af13c35ed32e572e878305
kovtun <kovtun@l3s.de>
2018-01-25 13:43:21 +0100

    Made the contents of the text area update when the dialog is open or when one of the size edit boxes is changed.

--- components/Deck/ContentPanel/ContentActions/EmbedModal.js
+++ components/Deck/ContentPanel/ContentActions/EmbedModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FocusTrap from 'focus-trap-react';
-import {Button, Container, Form, Modal, Radio, Icon, Segment, Grid, TextArea} from 'semantic-ui-react';
+import {Button, Container, Form, Modal, Icon, Segment, Grid, TextArea, Input, Label} from 'semantic-ui-react';
 import {FormattedMessage, defineMessages} from 'react-intl';
 import {connectToStores} from 'fluxible-addons-react';
 import ContentStore from '../../../../stores/ContentStore';
@@ -11,13 +11,19 @@ class EmbedModal extends React.Component {
     constructor(props) {
         super(props);
 
+        this.defaultWidthValue = 800;
+        this.defaultHeightValue = 400;
+
         this.state = {
             modalOpen: false,
             activeTrap: false,
+            width: this.defaultWidthValue,
+            height: this.defaultHeightValue,
+            href: null
         };
 
         this.handleEmbed = this.handleEmbed.bind(this);
-        this.handleOpen = this.handleOpen.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.unmountTrap = this.unmountTrap.bind(this);
 
@@ -33,27 +39,30 @@ class EmbedModal extends React.Component {
             embedModal_cancelButton:{
                 id:'embedModal.embedModal_cancelButton',
                 defaultMessage: 'Cancel'
+            },
+            embedModal_sizeLabel:{
+                id:'embedModal.embedModal_sizeLabel',
+                defaultMessage: 'Size'
             }
         });
     }
 
     handleEmbed() {
-//       this.props.onClose.call();
-//       this.state.modalOpen.call();
-
-//       $('#app').attr('aria-hidden', 'true');
-       this.setState({
-           modalOpen:true,
-           activeTrap:true
-       });
-       // TODO: implement.
-    }
-
-    handleOpen(){
-        $('#app').attr('aria-hidden', 'true');
         this.setState({
             modalOpen:true,
             activeTrap:true
+        });
+        // TODO: implement.
+    }
+
+    handleChange() {
+        var widthInput = $('#embedModalWidth');
+        var heightInput = $('#embedModalHeight');
+
+        this.setState({
+            width: widthInput.val() || this.defaultWidthValue,
+            height: heightInput.val() || this.defaultHeightValue,
+            href: window.location.href
         });
     }
 
@@ -76,15 +85,14 @@ class EmbedModal extends React.Component {
         return(
             <Modal
                     trigger={
-                        <Button
-                                icon="code"
+                        <Button icon="code"
                                 color="grey"
                                 type="button"
                                 onClick={this.handleEmbed}
-                                content={this.context.intl.formatMessage(this.messages.embedModal_embedButton)}
-                        />
+                                content={this.context.intl.formatMessage(this.messages.embedModal_embedButton)}/>
                     }
                     open={this.state.modalOpen}
+                    onOpen={this.handleChange}
                     onClose={this.handleClose}
                     id="embedModal"
                     aria-labelledby="embedModalHeader"
@@ -100,26 +108,43 @@ class EmbedModal extends React.Component {
                             onDeactivate: this.unmountTrap,
                             clickOutsideDeactivates: true ,
                             initialFocus: '#embedModalDescription'
-                        }}
-                >
+                        }}>
                     <Modal.Header className="ui center aligned" id="embedModalHeader">
-                        <h1 style={{'textAlign': 'center'}}> {this.context.intl.formatMessage(this.messages.embedModal_header)}</h1>
+                        <h1 style={{'textAlign': 'center'}}>
+                            {this.context.intl.formatMessage(this.messages.embedModal_header)}
+                        </h1>
                     </Modal.Header>
                     <Modal.Content>
                         <Container>
                             <Segment color="blue" textAlign="center" padded>
                                 <Segment>
-                                    <Form.Field >
-                                        <TextArea id="embedModalDescription"
-                                                label="Test" value="Hello there, this is some text in a text area."/>
+                                    <Form.Field>
+                                        <Label>
+                                            {this.context.intl.formatMessage(this.messages.embedModal_sizeLabel)}
+                                        </Label>
+                                        <Input id="embedModalWidth"
+                                                placeholder={this.defaultWidthValue}
+                                                onChange={this.handleChange}/>
+                                        <Label>x</Label>
+                                        <Input id="embedModalHeight"
+                                                placeholder={this.defaultHeightValue}
+                                                onChange={this.handleChange}/>
                                     </Form.Field>
-                                    <Button
-                                            icon="remove"
-                                            color="red"
-                                            type="button"
-                                            onClick={this.handleClose}
-                                            content={this.context.intl.formatMessage(this.messages.embedModal_cancelButton)}
-                                    />
+                                    <Form.Field>
+                                        <TextArea id="embedModalDescription"
+                                                label="description"
+                                                autoHeight={true}
+                                                value={'<iframe src="' + this.state.href + '" width="' +
+                                                        this.state.width + '" height="' + this.state.height +
+                                                        '"></iframe>'}/>
+                                    </Form.Field>
+                                    <Modal.Actions>
+                                        <Button icon="remove"
+                                                color="red"
+                                                type="button"
+                                                onClick={this.handleClose}
+                                                content={this.context.intl.formatMessage(this.messages.embedModal_cancelButton)}/>
+                                    </Modal.Actions>
                                 </Segment>
                             </Segment>
                         </Container>
