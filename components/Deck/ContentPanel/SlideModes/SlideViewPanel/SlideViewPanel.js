a1750773be0417b876755c2338a85c480841f32f
kovtun <kovtun@l3s.de>
2018-01-28 15:47:13 +0100

    Improved the efficiency when loading ContentPanel, but not completely.

--- components/Deck/ContentPanel/SlideModes/SlideViewPanel/SlideViewPanel.js
+++ components/Deck/ContentPanel/SlideModes/SlideViewPanel/SlideViewPanel.js
@@ -4,7 +4,32 @@ import SlideContentView from './SlideContentView';
 import SlideViewStore from '../../../../../stores/SlideViewStore';
 import DeckTreeStore from '../../../../../stores/DeckTreeStore';
 
-class SlideViewPanel extends React.Component {
+class SlideViewPanel extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.isLoading = true;
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        let samePropsState = this.props.SlideViewStore.content === nextProps.SlideViewStore.content;
+        let undefinedContent = nextProps.SlideViewStore.content === undefined ||
+                nextProps.SlideViewStore.content === '';
+        this.isLoading = undefinedContent;
+
+        // Content should be updated only when new content is ready or component properties/state have changed.
+        return !undefinedContent || !samePropsState;
+    }
+
+    componentWillUnmount() {
+        this.props.SlideViewStore.content = '';
+        this.isLoading = true;
+    }
+
+    componentDidUpdate() {
+        console.log('Updated SlideViewPanel.');
+    }
+
     render() {
         let deckTheme = this.props.selector && this.props.selector.theme;
         if (!deckTheme) {
@@ -18,14 +43,30 @@ class SlideViewPanel extends React.Component {
                 // pick theme from deck root as a last resort
                 deckTheme = this.props.DeckTreeStore.theme;
             }
-        } 
+        }
+
+        const compStyle = {
+            minHeight: 600,
+            overflowY: 'auto',
+            overflowX: 'auto',
+            position: 'relative'
+        };
+
+        if (this.isLoading) {
+            return (
+                <div className="ui bottom attached segment">
+                    <div className="ui active dimmer"><div className="ui text loader">Loading...</div></div>
+                    <div className="ui" style={compStyle}/>
+                </div>
+            );
+        }
+
         return (
             <div className="ui bottom attached segment">
-            {(this.props.SlideViewStore.content === undefined) ? <div className="ui active dimmer"><div className="ui text loader">Loading</div></div> : ''}
                 <SlideContentView content={this.props.SlideViewStore.content}
-                                  speakernotes={this.props.SlideViewStore.speakernotes}
-                                  loadingIndicator={this.props.SlideViewStore.loadingIndicator}
-                                  theme={deckTheme} />
+                        speakernotes={this.props.SlideViewStore.speakernotes}
+                        loadingIndicator={this.props.SlideViewStore.loadingIndicator}
+                        theme={deckTheme}/>
             </div>
         );
     }
