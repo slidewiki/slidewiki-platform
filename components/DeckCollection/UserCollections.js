72614558a199f7d3f410f246b4e181660ec6a668
Roy Meissner <meissner@informatik.uni-leipzig.de>
2018-03-09 11:17:38 +0100

    Hide deck collection creation on mobile

--- components/DeckCollection/UserCollections.js
+++ components/DeckCollection/UserCollections.js
@@ -8,6 +8,8 @@ import NewCollectionModal from './Modals/NewCollectionModal';
 import UpdateCollectionModal from './Modals/UpdateCollectionModal';
 import {FormattedMessage, defineMessages} from 'react-intl';
 
+import MobileDetect from 'mobile-detect';
+
 class UserCollections extends React.Component {
     constructor(props){
         super(props);
@@ -15,12 +17,19 @@ class UserCollections extends React.Component {
         this.styles = {'backgroundColor': '#2185D0', 'color': 'white'};
 
         this.state = {
-            showNewCollectionModal: false, 
+            showNewCollectionModal: false,
             showUpdateCollectionModal: false,
-            updateCollectionDetails: {}
+            updateCollectionDetails: {},
+            isMobile: false
         };
 
         this.messages = this.getIntlMessages();
+    }
+
+    componentDidMount() {
+        let userAgent = window.navigator.userAgent;
+        let mobile = new MobileDetect(userAgent);
+        this.setState({isMobile: (mobile.phone) ? true : false});
     }
 
     showNewCollectionModal(event){
@@ -86,19 +95,19 @@ class UserCollections extends React.Component {
             deleteError: {
                 id: 'UserCollections.error.delete',
                 defaultMessage: 'An error occurred while deleting collection...'
-            }, 
-            createError: { 
+            },
+            createError: {
                 id: 'UserCollections.error.create',
                 defaultMessage: 'An error occurred while creating collection....'
-            }, 
+            },
             updateError: {
                 id: 'UserCollections.error.update',
                 defaultMessage: 'An error occured while updating collection...'
-            }, 
+            },
             noCollectionsFound: {
                 id: 'UserCollections.collections.empty',
                 defaultMessage: 'No deck collections available'
-            }, 
+            },
             collectionCreate: {
                 id: 'UserCollections.collections.create',
                 defaultMessage: 'Create new collection'
@@ -106,44 +115,44 @@ class UserCollections extends React.Component {
             collectionDelete: {
                 id: 'UserCollections.collections.delete',
                 defaultMessage: 'Delete Collection'
-            }, 
+            },
             collectionSettings: {
                 id: 'UserCollections.collections.settings',
                 defaultMessage: 'Collection Settings'
-            }, 
+            },
             myCollectionsTitle: {
                 id: 'UserCollections.collections.mycollections',
                 defaultMessage: 'My Deck Collections'
-            }, 
+            },
             ownedCollectionsTitle: {
                 id: 'UserCollections.collections.owned',
                 defaultMessage: 'Owned Deck Collections'
-            }, 
+            },
             deckText: {
-                id: 'UserCollections.deck', 
+                id: 'UserCollections.deck',
                 defaultMessage: 'deck'
-            }, 
+            },
             decksText: {
-                id: 'UserCollections.decks', 
+                id: 'UserCollections.decks',
                 defaultMessage: 'decks'
-            }, 
+            },
             shareCollectionText: {
-                id: 'UserCollections.collections.shared', 
+                id: 'UserCollections.collections.shared',
                 defaultMessage: 'Shared Collection'
-            }, 
+            },
             deleteCollectionConfirmationTitle:{
-                id: 'UserCollections.collections.delete.title', 
+                id: 'UserCollections.collections.delete.title',
                 defaultMessage: 'Delete Collection'
-            }, 
+            },
             deleteCollectionConfirmationText:{
-                id: 'UserCollections.collections.delete.text', 
+                id: 'UserCollections.collections.delete.text',
                 defaultMessage: 'Are you sure you want to delete this collection?'
             }
         });
     }
 
     render() {
-        
+
 
         let content = '';
         let loadingDiv = '';
@@ -162,7 +171,7 @@ class UserCollections extends React.Component {
         } else if(this.props.DeckCollectionStore.updateCollectionMetadataError){
             this.showErrorPopup(this.context.intl.formatMessage(this.messages.updateError));
         }
-        
+
         // just show loading indicator
         if (collections === undefined){
             loadingDiv = (this.props.DeckCollectionStore.loading) ? <div className="ui active dimmer"><div className="ui text loader">Loading</div></div> : '';
@@ -209,7 +218,7 @@ class UserCollections extends React.Component {
                 {loadingDiv}
                 <div className="ui secondary clearing segment">
                     <h2 className="ui left floated header">{this.context.intl.formatMessage((this.props.loggedinuser === this.props.user.uname) ? this.messages.myCollectionsTitle :this.messages.ownedCollectionsTitle)}</h2>
-                    {(this.props.loggedinuser === this.props.user.uname) &&
+                    {(this.props.loggedinuser === this.props.user.uname && !this.state.isMobile) &&
                         <button className="ui right floated button" role="button" tabIndex="0" onClick={this.showNewCollectionModal.bind(this)}>
                           <p><FormattedMessage {...this.messages.collectionCreate} /></p>
                         </button>
@@ -226,7 +235,7 @@ class UserCollections extends React.Component {
 }
 
 UserCollections.contextTypes = {
-    executeAction: React.PropTypes.func.isRequired, 
+    executeAction: React.PropTypes.func.isRequired,
     intl: React.PropTypes.object.isRequired
 };
 
