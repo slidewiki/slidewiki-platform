a6a31e6bbe3de3135f0da5be85b0508abfd4fec2
dpaun <dpaunovic@gmail.com>
2018-03-20 12:37:04 +0100

    SWIK-1945 adding popular decks if there are not enough recommended ones

--- services/recommendations.js
+++ services/recommendations.js
@@ -14,9 +14,9 @@ export default {
             uid = 0;
         }
         if (resource === 'recommendations.list'){
+            const minNoOfRecommendations = 6;
 
-
-            const recommendationServiceUri = 'http://localhost:8084';
+            const recommendationServiceUri = 'http://localhost';
 
             rp.get({uri: recommendationServiceUri + '/analytics/webresources/recommender?userid=' + uid, proxy: '' }).then((res) => {
 
@@ -61,7 +61,65 @@ export default {
                         }
                     }
 
-                    callback(null, {recommendations: recommendationDecks});
+                    const noOfRecommendations = recommendationDecks.length;
+                    if (noOfRecommendations >= minNoOfRecommendations) {
+                        callback(null, {recommendations: recommendationDecks});
+                    } else {
+                        //GET SOME POPULAR decks
+                        const numberOfDecks = minNoOfRecommendations - noOfRecommendations;
+
+                        rp.get({uri: Microservices.activities.uri + '/activities/maxCount' +
+                            '?activity_type=react&activity_type=download' + // GET DECKS WITH MAX NUMBER OF LIKES AND DOWNLOADS
+                            '&all_revisions=true&limit=' + numberOfDecks + '&content_kind=deck' }).then((res) => {
+                                let items = JSON.parse(res);
+
+                                //GET DATA FOR DECKS FROM DECK SERVICE
+                                let deckPromises2 = [];
+                                let likesPromises2 = [];//get the number of deck likes
+
+                                // get details for the decks in the collection
+                                for(let item of items){
+                                    let deckId = item._id.content_id;
+
+                                    deckPromises2.push(
+                                        rp.get({
+                                            uri: `${Microservices.deck.uri}/deck/${deckId}`,
+                                            json: true
+                                        })
+                                    );
+
+                                    likesPromises2.push(
+                                        rp.get({
+                                            uri: Microservices.activities.uri + '/activities/deck/' + deckId + '?metaonly=true&activity_type=react&all_revisions=true'
+                                        })
+                                    );
+                                }
+                                let deckPromise2 = Promise.all(deckPromises2);
+                                let likesPromise2 = Promise.all(likesPromises2);
+
+                                Promise.all([deckPromise2, likesPromise2]).then( (data) => {
+                                    let decks = data[0];
+
+                                    for (let i = 0; i < data[1].length; i++) {
+                                        let recommendationDeck = decks[i];
+                                        if (recommendationDeck.user !== uid) {// do not recommend decks owned by the user
+                                            recommendationDeck.noOfLikes = data[1][i];
+                                            recommendationDeck.recommendationWeight = items[i].count / 1000;
+                                            recommendationDecks.push(recommendationDeck);
+                                        }
+                                    }
+
+                                    callback(null, {recommendations: recommendationDecks});
+                                }).catch((err) => {
+                                    console.log(err);
+                                    callback(null, {recommendations: []});
+                                });
+
+                            }).catch((err) => {
+                                console.log(err);
+                                callback(null, {recommendations: []});
+                            });
+                    }
                 }).catch( (err) => {
                     console.log(err);
                     callback(null, {recommendations: []});
