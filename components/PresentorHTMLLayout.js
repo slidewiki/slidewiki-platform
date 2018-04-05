a948b4b90f3b5649f4e30034adac5ebb497aabd3
Luis Fernandes <sednanref2004@gmail.com>
2018-04-05 15:44:14 +0200

    Added chart rendering libraries in PresentorHTMLLayout

--- components/PresentorHTMLLayout.js
+++ components/PresentorHTMLLayout.js
@@ -64,6 +64,8 @@ class PresentorHTMLLayout extends React.Component {
                 <script src="/custom_modules/custom-semantic-ui/dist/components/search.min.js" defer></script>
                 <script src="/custom_modules/custom-semantic-ui/dist/components/api.min.js" defer></script>
                 <script src="/custom_modules/custom-semantic-ui/dist/components/sidebar.min.js" defer></script>
+                <script src="/custom_modules/nvd3/d3.v3.min.js" defer></script>
+                <script src="/custom_modules/nvd3/nv.d3.min.js" defer></script>
                 {/* All external vendors bundle*/
                     this.props.addAssets ? <script src={'/public/js/vendor.bundle.js'} defer></script> : ''
                 }
