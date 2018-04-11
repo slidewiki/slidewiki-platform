3ae3225d6022a381a7d53c028a94515c9b76bebf
ali1k <hyperir@gmail.com>
2018-04-11 15:39:00 +0200

    added export to Neo4J browser

---
+++ components/BasicHTMLLayout.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ApplicationStore from '../stores/ApplicationStore';
+import ga from '../plugins/googleAnalytics/ga';
+
+let hook = require('css-modules-require-hook');
+
+hook({
+    generateScopedName: '[hash:base64:5]',
+});
+
+class BasicHTMLLayout extends React.Component {
+    render() {
+        let user = this.props.context.getUser();
+        return (
+            <html lang={ this.props.lang }>
+            <head>
+                <meta charSet="utf-8" />
+                <title>{this.props.context.getStore(ApplicationStore).getPageTitle()}</title>
+                <meta name="viewport" content="width=device-width, user-scalable=no" />
+                <link href="/assets/css/custom.css" rel="stylesheet" type="text/css" />
+                {/* Vendors css bundle */
+                    this.props.addAssets ? <link href="/public/css/vendor.bundle.css" rel="stylesheet" type="text/css" />: <style></style>
+                }
+            </head>
+            <body>
+                <div id="app" aria-hidden="false" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
+
+                {/* All external vendors bundle*/
+                    this.props.addAssets ? <script src={'/public/js/vendor.bundle.js'} defer></script> : ''
+                }
+                {/* Main app bundle */}
+                <script src={'/public/js/' + this.props.clientFile} defer></script>
+                <script dangerouslySetInnerHTML={ {__html: ga} } />
+            </body>
+            </html>
+        );
+    }
+}
+
+export default BasicHTMLLayout;
