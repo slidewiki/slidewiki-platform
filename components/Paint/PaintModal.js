7bcd58ed9099a44d4566604a42926cb8ac550846
Luis Fernandes <sednanref2004@gmail.com>
2018-06-04 16:12:23 +0200

    New arrow.

--- components/Paint/PaintModal.js
+++ components/Paint/PaintModal.js
@@ -281,21 +281,23 @@ class PaintModal extends React.Component {
     }
 
     addArrow() {
-        let path = new fabric.Path('M121.32,0L44.58,0C36.67,0,29.5,3.22,24.31,8.41\
-            c-5.19,5.19-8.41,12.37-8.41,20.28c0,15.82,12.87,28.69,28.69,28.69c0,0,4.4,\
-            0,7.48,0C36.66,72.78,8.4,101.04,8.4,101.04C2.98,106.45,0,113.66,0,121.32\
-            c0,7.66,2.98,14.87,8.4,20.29l0,0c5.42,5.42,12.62,8.4,20.28,8.4c7.66,0,14.87\
-            -2.98,20.29-8.4c0,0,28.26-28.25,43.66-43.66c0,3.08,0,7.48,0,7.48c0,15.82,\
-            12.87,28.69,28.69,28.69c7.66,0,14.87-2.99,20.29-8.4c5.42-5.42,8.4-12.62,8.4\
-            -20.28l0-76.74c0-7.66-2.98-14.87-8.4-20.29C136.19,2.98,128.98,0,121.32,0z');
+        let points = [
+            {x: 0, y: 20},
+            {x: 60, y: 20},
+            {x: 60, y: 10},
+            {x: 80, y: 40},
+            {x: 60, y: 70},
+            {x: 60, y: 60},
+            {x: 0, y: 60},
+            {x: 0, y: 20}
+        ];
 
-        this.canvas.add(path.set({ left: 30,
+        this.canvas.add(new fabric.Polygon(points, {
+            left: 30,
             top: 30,
             fill: this.primaryColor,
-            width: 50,
-            height: 50,
-            opacity: 1,
             stroke: this.secondaryColor,
+            opacity: 1,
             strokeWidth: this.canvas.freeDrawingBrush.width
         }));
     }
