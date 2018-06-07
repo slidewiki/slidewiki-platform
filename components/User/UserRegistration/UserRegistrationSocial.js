8068f07d112016fea12f8a61aad642f090435730
Roy Meissner <meissner@informatik.uni-leipzig.de>
2018-06-07 11:02:15 +0200

    Optimized code style and LOC

--- components/User/UserRegistration/UserRegistrationSocial.js
+++ components/User/UserRegistration/UserRegistrationSocial.js
@@ -135,39 +135,31 @@ class UserRegistrationSocial extends React.Component {
         if (nextProps.UserRegistrationStore.socialuserdata && localStorage.getItem(MODI) === 'register') {
             if ((nextProps.UserRegistrationStore.socialuserdata.username && !(this.refs.username.value)) && (nextProps.UserRegistrationStore.socialuserdata.email && !(this.refs.email.value)))
                 this.setUserdata(nextProps.UserRegistrationStore.socialuserdata);
-        }
-        else if (nextProps.UserRegistrationStore.socialError && !this.props.UserRegistrationStore.socialError) {
+        } else if (nextProps.UserRegistrationStore.socialError && !this.props.UserRegistrationStore.socialError) {
             const messages = defineMessages({
                 genericError: {
                     id: 'UserRegistrationSocial.genericError',
-                    defaultMessage: 'An error occured. Please try your action at a later time.',
+                    defaultMessage: 'An error occured. Please try again later.',
                 },
                 error: {
                     id: 'UserRegistrationSocial.error',
-                    defaultMessage: 'Error',
+                    defaultMessage: 'Social Login Error',
                 },
                 confirm: {
                     id: 'UserRegistrationSocial.confirm',
-                    defaultMessage: 'Confirm',
+                    defaultMessage: 'OK',
                 }
             });
 
             swal({
-                title: this.context.intl.formatMessage(messages.error),
+                titleText: this.context.intl.formatMessage(messages.error),
                 text: this.context.intl.formatMessage(messages.genericError),
                 type: 'error',
-                showCloseButton: true,
                 showCancelButton: false,
                 confirmButtonText: this.context.intl.formatMessage(messages.confirm),
                 confirmButtonClass: 'ui button',
                 buttonsStyling: false
-            })
-                .then((dismiss) => {
-                    return true;
-                })
-                .catch(() => {
-                    return true;
-                });
+            });
         }
     }
 
