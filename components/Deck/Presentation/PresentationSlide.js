60fe840aac584e5adf8c1701ef4baf142ac6edad
kadevgraaf <kadegraaf@gmail.com>
2018-06-29 12:43:42 +0200

    replace src with data-src

--- components/Deck/Presentation/PresentationSlide.js
+++ components/Deck/Presentation/PresentationSlide.js
@@ -8,9 +8,19 @@ import SlideViewStore from '../../../stores/SlideViewStore';
 let sectionStyle = { 'top': 'unset !important'};
 
 class PresentationSlide extends React.Component {
+    constructor(props) {
+        super(props);
+        this.replaced = false;
+        this.i = 0;
+    }
     render(){
+        let slideContent = '';
+        if (this.replaced === false && this.props.content !== ''){
+            slideContent = this.props.content.replace(/ src/g, ' data-src');
+            this.replaced = true;
+        }
         return (
-            <section dangerouslySetInnerHTML={{__html:this.props.content}} id={this.props.id} />
+            <section dangerouslySetInnerHTML={{__html:slideContent}} id={this.props.id} />
         );
     }
 
