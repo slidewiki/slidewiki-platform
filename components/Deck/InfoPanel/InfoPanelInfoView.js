9bf946dea0f7d1de475433e3425d9e690cc74db8
kadevgraaf <kadegraaf@gmail.com>
2018-07-11 11:34:20 +0200

    remove "current"

--- components/Deck/InfoPanel/InfoPanelInfoView.js
+++ components/Deck/InfoPanel/InfoPanelInfoView.js
@@ -259,7 +259,7 @@ class InfoPanelInfoView extends React.Component {
                 <div className="ui top attached icon buttons menu">
                     <Dropdown pointing="top left" disabled={languageOptions.length < 2 && !canEdit}
                         button basic className="attached" style={{textAlign: 'center'}}
-                        trigger={<h5 className='ui small header'>{this.context.intl.formatMessage(this.messages.selectLanguage)}:  <i className={currentLangIconName + ' flag'} style={{marginRight: 0, verticalAlign: 'middle'}}></i> ({this.context.intl.formatMessage(this.messages.current)}: {getLanguageName(this.props.TranslationStore.currentLang)}) </h5>} icon={null}
+                        trigger={<h5 className='ui small header'>{this.context.intl.formatMessage(this.messages.selectLanguage)}:  <i className={currentLangIconName + ' flag'} style={{marginRight: 0, verticalAlign: 'middle'}}></i> ({getLanguageName(this.props.TranslationStore.currentLang)}) </h5>} icon={null}
                         aria-label="Select language" data-tooltip="Select language"
                         defaultValue={activeLanguage} options={languageOptions} onChange={this.changeCurrentLanguage.bind(this)} />
                         {/*
