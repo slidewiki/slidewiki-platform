dfbf0ae757cff904f265f7a82710f5f3916aaf66
Luis Fernandes <sednanref2004@gmail.com>
2018-07-31 18:13:08 +0200

    Removing consolelog from handleDrop action

--- actions/media/handleDroppedFile.js
+++ actions/media/handleDroppedFile.js
@@ -2,8 +2,6 @@ const log = require('../log/clog');
 
 export default function uploadMediaFile(context, payload, done) {
     log.info(context);
-    console.log('//////////////////action');
-    console.log(payload);
     context.dispatch('HANDLE_DROPPED', payload);
     done();
 }
