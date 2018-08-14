eb5e3c806a751dcf21fb687bbd1cd07aeffe9904
Luis Fernandes <sednanref2004@gmail.com>
2018-08-14 23:15:29 +0200

    Added altText in dif containging svg image uploaded.

--- actions/paint/editImageWithSrc.js
+++ actions/paint/editImageWithSrc.js
@@ -6,23 +6,23 @@ export default function editImageWithSrc(context, src, done) {
     let url = src.split('?')[0];
     let extension = url.split('.')[src.split('.').length - 1];
     context.service.read('media.readMetadata', {url: url}, {timeout: 20 * 1000}, (err, res) => {
-       if (err) {
-           log.error(context, {filepath: __filename});
-           done();
-       } else {
-           res = JSON.parse(res);
-           let fileType = res.type;
-           let altText = res.altText;
-           let title = res.title;
-           if (fileType === 'image/svg+xml') {
-               context.service.read('media.readCSV', {url: url}, { timeout: 20 * 1000 }, (err, res) => {
-                   context.dispatch('OPEN_WITH_SRC', {url: url, svg: res, title: title, altText: altText});
-                   done();
-               });
-           } else {
-               context.dispatch('OPEN_PICTURE', {url: url, title: title, altText: altText});
-               done();
-           }
-       }
+        if (err) {
+            log.error(context, {filepath: __filename});
+            done();
+        } else {
+            res = JSON.parse(res);
+            let fileType = res.type;
+            let altText = res.altText;
+            let title = res.title;
+            if (fileType === 'image/svg+xml') {
+                context.service.read('media.readCSV', {url: url}, { timeout: 20 * 1000 }, (err, res) => {
+                    context.dispatch('OPEN_WITH_SRC', {url: url, svg: res, title: title, altText: altText});
+                    done();
+                });
+            } else {
+                context.dispatch('OPEN_PICTURE', {url: url, title: title, altText: altText});
+                done();
+            }
+        }
     });
 }
