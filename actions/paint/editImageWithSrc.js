8a8fb6bf4f63d578cfeee05069212c92cb3348cf
Luis Fernandes <sednanref2004@gmail.com>
2018-08-14 23:08:53 +0200

    Adapting editImageWithSrc action and PaintModalStore to extract altText and Title from images during edition.

--- actions/paint/editImageWithSrc.js
+++ actions/paint/editImageWithSrc.js
@@ -5,13 +5,24 @@ export default function editImageWithSrc(context, src, done) {
     // Split just in case it has the time trick to update cache.
     let url = src.split('?')[0];
     let extension = url.split('.')[src.split('.').length - 1];
-    if (extension === 'svg') {
-        context.service.read('media.readCSV', {url: url}, { timeout: 20 * 1000 }, (err, res) => {
-            context.dispatch('OPEN_WITH_SRC', {url: url, svg: res});
-            done();
-        });
-    } else {
-        context.dispatch('OPEN_PICTURE', {url: url});
-        done();
-    }
+    context.service.read('media.readMetadata', {url: url}, {timeout: 20 * 1000}, (err, res) => {
+       if (err) {
+           log.error(context, {filepath: __filename});
+           done();
+       } else {
+           res = JSON.parse(res);
+           let fileType = res.type;
+           let altText = res.altText;
+           let title = res.title;
+           if (fileType === 'image/svg+xml') {
+               context.service.read('media.readCSV', {url: url}, { timeout: 20 * 1000 }, (err, res) => {
+                   context.dispatch('OPEN_WITH_SRC', {url: url, svg: res, title: title, altText: altText});
+                   done();
+               });
+           } else {
+               context.dispatch('OPEN_PICTURE', {url: url, title: title, altText: altText});
+               done();
+           }
+       }
+    });
 }
