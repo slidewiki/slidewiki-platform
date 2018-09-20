d32cdb069a1e8c36378c39b55533bead036093ee
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-09-20 14:23:24 +0200

    Made slide resolution changes reflected on the resolution change button when changing the template.

--- actions/slide/changeSlideSizeText.js
+++ actions/slide/changeSlideSizeText.js
@@ -1,5 +1,4 @@
 import {shortTitle} from '../../configs/general';
-import serviceUnavailable from '../error/serviceUnavailable';
 const log = require('../log/clog');
 
 export default function changeSlideSizeText(context, payload, done) {
