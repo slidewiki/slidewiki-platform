ee542ef22180fcacd7d4b1b56ae6b8c3de33be91
Stavros Maroulis <stavrosmrl@gmail.com>
2018-10-04 14:59:01 +0300

    Add titles to user stats visualizations

--- components/User/UserProfile/PrivatePublicUserProfile/UserStats.js
+++ components/User/UserProfile/PrivatePublicUserProfile/UserStats.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
-import {Dropdown, Grid} from 'semantic-ui-react';
+import {Dropdown, Grid, Message, Segment} from 'semantic-ui-react';
 import moment from 'moment';
 import updateUserStatsPeriod from '../../../../actions/stats/updateUserStatsPeriod';
 import updateUserStatsActivityType from '../../../../actions/stats/updateUserStatsActivityType';
@@ -31,49 +31,66 @@ class UserStats extends React.Component {
             {value: 'LAST_1_YEAR', text: 'Last 1 year'},
             {value: 'LAST_2_YEARS', text: 'Last 2 years'},];
 
-        const typeOptions = [{value: 'edit', text: 'Edits'}, {value: 'like', text: 'Likes'}, {value: 'view', text: 'Views'}];
+        const typeOptions = [{value: 'edit', text: 'Edits'}, {value: 'like', text: 'Likes'}, {
+            value: 'view',
+            text: 'Views'
+        }];
         return (
-          <Grid relaxed padded >
-              <Grid.Row columns={2}>
-                  <Grid.Column textAlign='left'>
-                       <span>Activity type: {' '}
-                           <Dropdown inline placeholder='Activity Type' options={typeOptions}
-                                     value={this.props.userStats.activityType}
-                                     onChange={this.handleActivityTypeChange.bind(this)}/>
-                  </span>
-                  </Grid.Column>
-                  <Grid.Column textAlign='right'>
-                      <span>Period: {' '}
-                          <Dropdown inline placeholder='Period' options={periodOptions}
-                                    value={this.props.userStats.datePeriod}
-                                    onChange={this.handleDatePeriodChange.bind(this)}/>
-                  </span>
-                  </Grid.Column>
-              </Grid.Row>
+          <Grid relaxed padded>
+              {this.props.userStats.statsByTime && this.props.userStats.statsByTime.length > 0 &&
               <Grid.Row columns={1}>
                   <Grid.Column>
-                      {this.props.userStats.statsByTime && this.props.userStats.statsByTime.length > 0 &&
-                      <ResponsiveContainer height={300}>
-                          <LineChart data={this.props.userStats.statsByTime}
-                                     margin={{top: 5, right: 30, left: 30, bottom: 5}}>
-                              <YAxis type="number" width={10} allowDecimals={false}/>
-                              <XAxis dataKey='date'
-                                     name='Date'
-                                     type='category'
-                                     tickFormatter={(unixTime) => moment(unixTime).format('Y-M-D')} /*domain={['dataMin', 'dataMax']} scale='time'  interval='preserveStartEnd'*/
-                              />
-                              <Tooltip labelFormatter={(unixTime) => moment(unixTime).format('Y-M-D')}/>
-                              <Line dataKey="count" dot={false} type="monotone"/>
-                          </LineChart>
-                      </ResponsiveContainer>}
+                      <Message
+                        attached
+                        header='Activity Timeline'
+                      />
+                      <Segment attached padded>
+                          <span>
+                              <Grid>
+                                  <Grid.Row columns={2}>
+                                      <Grid.Column textAlign='left'>
+                                          <Dropdown inline placeholder='Activity Type'
+                                                    options={typeOptions}
+                                                    value={this.props.userStats.activityType}
+                                                    onChange={this.handleActivityTypeChange.bind(this)}/>
+                                      </Grid.Column>
+                                      <Grid.Column textAlign='right'>
+                                          <Dropdown
+                                            inline placeholder='Period' options={periodOptions}
+                                            value={this.props.userStats.datePeriod}
+                                            onChange={this.handleDatePeriodChange.bind(this)}/>
+                                      </Grid.Column>
+                                  </Grid.Row>
+                                  <Grid.Row columns={1}>
+                                      <ResponsiveContainer height={300}>
+                                          <LineChart data={this.props.userStats.statsByTime}
+                                                     margin={{top: 5, right: 30, left: 30, bottom: 5}}>
+                                              <YAxis type="number" width={10} allowDecimals={false}/>
+                                              <XAxis dataKey='date' name='Date'
+                                                     type='category'
+                                                     tickFormatter={(unixTime) => moment(unixTime).format('Y-M-D')}/>
+                                              <Tooltip labelFormatter={(unixTime) => moment(unixTime).format('Y-M-D')}/>
+                                              <Line dataKey="count" dot={false} type="monotone"/>
+                                          </LineChart>
+                                      </ResponsiveContainer>
+                                  </Grid.Row>
+                              </Grid>
+                          </span>
+                      </Segment>
                   </Grid.Column>
-              </Grid.Row>
+              </Grid.Row>}
+              {this.props.userStats.statsByTag && this.props.userStats.statsByTag.length > 0 &&
               <Grid.Row centered columns={1}>
-                  <Grid.Column textAlign='center'>
-                      {this.props.userStats.statsByTag && this.props.userStats.statsByTag.length > 0 &&
-                          <TagCloud minSize={16} maxSize={40} tags={this.props.userStats.statsByTag}/>}
+                  <Grid.Column>
+                      <Message
+                        attached
+                        header='Popular Tags'
+                      />
+                      <Segment attached textAlign='center' padded='very'>
+                          <TagCloud minSize={16} maxSize={40} tags={this.props.userStats.statsByTag}/>
+                      </Segment>
                   </Grid.Column>
-              </Grid.Row>
+              </Grid.Row>}
           </Grid>
         );
     }
