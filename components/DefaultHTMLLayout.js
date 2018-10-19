bbafdba5469e977487bc9d31b3ff27ac1871cc76
Abi James <abijames@users.noreply.github.com>
2018-10-19 13:45:17 +0100

    removed JIRA feedback button

--- components/DefaultHTMLLayout.js
+++ components/DefaultHTMLLayout.js
@@ -91,7 +91,7 @@ class DefaultHTMLLayout extends React.Component {
                 <script src="/public/js/settings.js" defer></script>
                 {/* Main app bundle */}
                 <script src={'/public/js/' + this.props.clientFile} defer></script>
-                <script type="text/javascript" src="https://slidewiki.atlassian.net/s/5e2fc7b2a8ba40bc00a09a4f81a301c8-T/rfg5q6/100012/c/1000.0.9/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-UK&collectorId=241c9e18" defer></script>
+{}              {/*  <script type="text/javascript" src="https://slidewiki.atlassian.net/s/5e2fc7b2a8ba40bc00a09a4f81a301c8-T/rfg5q6/100012/c/1000.0.9/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-UK&collectorId=241c9e18" defer></script> */}
                 <script src="/sweetalert2/dist/sweetalert2.min.js" defer></script>
                 { user ?
                   <script src="/jquery-contextmenu/dist/jquery.contextMenu.min.js"  defer></script>
