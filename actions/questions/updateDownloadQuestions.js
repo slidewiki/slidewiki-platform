f2090e2a2cf14046a479563df001071d6d55ca1e
Nicola Knight <nk1g09@soton.ac.uk>
2018-10-23 11:46:09 +0100

    created action for selecting questions

---
+++ actions/questions/updateDownloadQuestions.js
@@ -0,0 +1,8 @@
+const log = require('../log/clog');
+//import serviceUnavailable from '../error/serviceUnavailable';
+
+export default function updateDownloadQuestions(context, payload, done) {
+    log.info(context);
+    context.dispatch('UPDATE_DOWNLOAD_QUESTIONS', payload);
+    done(); 
+}
