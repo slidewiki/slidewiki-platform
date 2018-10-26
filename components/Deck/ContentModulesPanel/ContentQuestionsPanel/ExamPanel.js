99a0cbed855c259c8f3850bfc9e5e5a883bfcf80
dpaun <dpaunovic@gmail.com>
2018-10-26 11:01:19 +0200

    SWIK-2488 show back button when there are no exam questions

--- components/Deck/ContentModulesPanel/ContentQuestionsPanel/ExamPanel.js
+++ components/Deck/ContentModulesPanel/ContentQuestionsPanel/ExamPanel.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import {connectToStores} from 'fluxible-addons-react';
+import {navigateAction} from 'fluxible-router';
 import ContentQuestionsStore from '../../../../stores/ContentQuestionsStore';
 import DeckViewStore from '../../../../stores/DeckViewStore';
+import DeckTreeStore from '../../../../stores/DeckTreeStore';
+import Util from '../../../common/Util';
 import ExamList from './ExamList';
+
 class ExamPanel extends React.Component {
+    getPath(){
+        const selector = this.props.ContentQuestionsStore.selector;
+        if (selector.id === undefined || selector.id === 'undefined') {//this can happen when exam has not been opened from the deck view
+            return '/' + selector.stype + '/' + selector.sid;
+        } else {
+            const flatTree = this.props.DeckTreeStore.flatTree;
+            let path = '';
+            for (let i=0; i < flatTree.size; i++) {
+                if (flatTree.get(i).get('type') === selector.stype && flatTree.get(i).get('id').split('-')[0] === selector.sid.split('-')[0]) {
+                    path = flatTree.get(i).get('path');
+                    let nodeSelector = {id: selector.id, stype: selector.stype, sid: selector.sid.split('-')[0], spath: path};
+                    let nodeURL = Util.makeNodeURL(nodeSelector, 'deck', 'view', undefined, undefined, true);
+                    return nodeURL;
+                }
+            }
+            return path;
+        }
+    }
+    cancelButtonClick(e) {
+        e.preventDefault();
+        this.context.executeAction(navigateAction, {
+            url: this.getPath()
+        });
+    }
     render() {
         const questions = this.props.ContentQuestionsStore.questions;
         let examQuestions = [];
@@ -19,17 +47,21 @@ class ExamPanel extends React.Component {
             title = revisions[revisions.length - 1].title + ' - ';
         }
         let questionsList = (<ExamList items={examQuestions} selector={selector} />);
-        let content = (
+        let noExamQuestionsNotification = (
             <div>
-                {examQuestions.length === 0 ? 'There are currently no exam questions for this ' + selector.stype + '.' : questionsList}
+                <h4>There are currently no exam questions for this {selector.stype}.</h4>
+                <button type="button" className="ui blue labeled close icon button" onClick={this.cancelButtonClick.bind(this)}>
+                    <i className="icon angle left" />Back
+                </button>
             </div>
         );
+        
         return (
             <div ref="examPanel" className="ui container segments">
                 <div className="ui secondary segment">
                     <h1 className="ui medium header">{title}Exam mode</h1>
                 </div>
-                { content }
+                {examQuestions.length !== 0 ? noExamQuestionsNotification : questionsList}
             </div>
         );
     }
@@ -37,9 +69,10 @@ class ExamPanel extends React.Component {
 ExamPanel.contextTypes = {
     executeAction: React.PropTypes.func.isRequired
 };
-ExamPanel = connectToStores(ExamPanel, [ContentQuestionsStore, DeckViewStore], (context, props) => {
+ExamPanel = connectToStores(ExamPanel, [ContentQuestionsStore, DeckTreeStore, DeckViewStore], (context, props) => {
     return {
         ContentQuestionsStore: context.getStore(ContentQuestionsStore).getState(),
+        DeckTreeStore: context.getStore(DeckTreeStore).getState(),
         DeckViewStore: context.getStore(DeckViewStore).getState()
     };
 });
