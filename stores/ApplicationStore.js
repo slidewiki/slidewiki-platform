9f98d20d608ffd21a4e3beac0fc30f3c2c028873
Ali <ali@Alis-MacBook-Pro-2.local>
2018-11-08 15:01:43 +0100

    adding metadata to dekc page layout

--- stores/ApplicationStore.js
+++ stores/ApplicationStore.js
@@ -5,6 +5,7 @@ class ApplicationStore extends BaseStore {
     constructor(dispatcher) {
         super(dispatcher);
         this.pageTitle = '';
+        this.pageThumbnail = '/assets/images/slideWiki-logo-linear.png'; //can add a default image here
         //this.frozen = false;
     }
     handlePageTitle(payload) {
@@ -16,22 +17,34 @@ class ApplicationStore extends BaseStore {
             this.emitChange();
         });
     }
+    updatePageMetadata(payload) {
+        this.dispatcher.waitFor(RouteStore, () => {
+            this.pageThumbnail = '/thumbnail/slide/' + payload.thumbnailID;
+            this.emitChange();
+        });
+    }
     getPageTitle() {
         return this.pageTitle;
     }
+    getPageThumbnail() {
+        return this.pageThumbnail;
+    }
     dehydrate() {
         return {
-            pageTitle: this.pageTitle
+            pageTitle: this.pageTitle,
+            pageThumbnail: this.pageThumbnail
         };
     }
     rehydrate(state) {
         this.pageTitle = state.pageTitle;
+        this.pageThumbnail = state.pageThumbnail;
     }
 }
 
 ApplicationStore.storeName = 'ApplicationStore';
 ApplicationStore.handlers = {
-    'UPDATE_PAGE_TITLE': 'handlePageTitle'
+    'UPDATE_PAGE_TITLE': 'handlePageTitle',
+    'LOAD_DECK_METADATA_SUCCESS': 'updatePageMetadata'
 };
 
 export default ApplicationStore;
