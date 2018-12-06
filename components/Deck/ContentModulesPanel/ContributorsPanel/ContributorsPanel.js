e9a5a58ac40b9354abc9880366c30461ed0c2252
Russell Newman <rn@russellnewman.co.uk>
2018-12-06 18:01:31 -0500

    Made ContributorsPanel accessible #SWIK-2616

--- components/Deck/ContentModulesPanel/ContributorsPanel/ContributorsPanel.js
+++ components/Deck/ContentModulesPanel/ContributorsPanel/ContributorsPanel.js
@@ -5,27 +5,25 @@ import classNames from 'classnames/bind';
 import ContributorsStore from '../../../../stores/ContributorsStore';
 import ContributorsList from './ContributorsList';
 import { FormattedMessage, defineMessages } from 'react-intl';
+import {Accordion, AccordionItem, AccordionItemBody, AccordionItemTitle} from 'react-accessible-accordion';
+import {Icon} from 'semantic-ui-react';
 
 class ContributorsPanel extends React.Component {
-    componentDidMount() {
-        this.enableAccordion();
-    }
+    constructor(props) {
+        super(props);
+        this.state = {advanced_options_visible: false};
+    };
 
-    componentDidUpdate() {
-        this.refreshAccordion();
-    }
-
-    enableAccordion(status) {
-        let accordionDIV = this.refs.contributorsPanel;
-        $(accordionDIV).find('.ui.accordion').accordion({
-            exclusive: false
-        });
-    }
-
-    refreshAccordion(status) {
-        let accordionDIV = this.refs.contributorsPanel;
-        $(accordionDIV).find('.ui.accordion').accordion('refresh');
-    }
+    /**
+     * Toggles the state of advanced_options_visible.
+     * Triggered by clicks on <AccordionItemTitle> elements.
+     *
+     * @returns {void}
+     */
+    handleAccordionChange = () => {
+        const { advanced_options_visible } = this.state;
+        this.setState({ advanced_options_visible: !advanced_options_visible });
+    };
 
     render() {
         const form_messages = defineMessages({
@@ -34,6 +32,9 @@ class ContributorsPanel extends React.Component {
                 defaultMessage: 'There are no contributors for this',
             }
         });
+
+        const { advanced_options_visible } = this.state;
+
         return (
             <div className="sw-contributors-panel" ref="contributorsPanel">
                 <div className="ui">
@@ -42,20 +43,22 @@ class ContributorsPanel extends React.Component {
                             id='ContributorsPanel.form.header'
                             defaultMessage='Creator' />
                     </h4>
-                    <ContributorsList items={this.props.ContributorsStore.creator  }></ContributorsList>
-                    <div className="ui accordion">
-                        <div className="title" tabIndex="0">
-                            <i className="dropdown icon"></i>
-                            <FormattedMessage
-                                id='ContributorsPanel.form.title'
-                                defaultMessage='Contributors' />
-                        </div>
-                        <div className="content">
-                            {this.props.ContributorsStore.contributors.length === 0 ?
-                                <div>{this.context.intl.formatMessage(form_messages.no_contributors) + ' ' + this.props.ContributorsStore.selector.stype}.</div> :
-                                <ContributorsList items={this.props.ContributorsStore.contributors}></ContributorsList>}
-                        </div>
-                    </div>
+                    <ContributorsList items={this.props.ContributorsStore.creator}/>
+                    <Accordion onChange={this.handleAccordionChange} accordion={false}>
+                        <AccordionItem>
+                            <AccordionItemTitle>
+                                <Icon name={advanced_options_visible ? 'down caret' : 'right caret'}/>
+                                <FormattedMessage
+                                    id='ContributorsPanel.form.title'
+                                    defaultMessage='Contributors' />
+                            </AccordionItemTitle>
+                            <AccordionItemBody hideBodyClassName='hidden'>
+                                {this.props.ContributorsStore.contributors.length === 0 ?
+                                    `${this.context.intl.formatMessage(form_messages.no_contributors) + ' ' + this.props.ContributorsStore.selector.stype}.` :
+                                    <ContributorsList items={this.props.ContributorsStore.contributors}/>}
+                            </AccordionItemBody>
+                        </AccordionItem>
+                    </Accordion>
                 </div>
             </div>
 
